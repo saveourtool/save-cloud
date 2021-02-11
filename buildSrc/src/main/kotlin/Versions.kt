d2fa17f45c84a6369716591bc15a1b31e554fabb
Peter Trifanov <peter.trifanov@mail.ru>
2021-02-11 12:09:26 +0300

    Experiments with docker API
    ### What's done:
    * Extracted orchestrator to separate subproject
    * Extracted versions to buildSrc

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+    val kotlin = "1.4.30"
+    val springBoot = "2.4.2"
+    val springSecurity = "5.4.2"
+    val hibernate = "5.4.2.Final"
+    val liquibase = "4.2.2"
+    val slf4j = "1.7.30"
+    val logback = "1.2.3"
+    val dockerJavaApi = "3.2.7"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 rootProject.name = "save"
 
 include("save-backend")
+include("save-orchestrator")
 include("save-core")
 include("save-frontend")
 include("save-plugins")
