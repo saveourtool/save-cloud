a5fc6b9d8dbabc6e800f6a0be07457ad39aedd47
aktsay6 <aktsay6@gmail.com>
2021-02-24 13:17:52 +0300

    feature/save-database
    ### What's done:
      * Fixed bugs

--- save-backend/src/main/kotlin/org/cqfn/save/backend/repository/ProjectRepository.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/repository/ProjectRepository.kt
@@ -1,6 +1,8 @@
 package org.cqfn.save.backend.repository
 
 import org.cqfn.save.backend.entities.Project
+import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.JpaRepository
 
+@Profile("dev")
 interface ProjectRepository : JpaRepository<Project, Int>
--- save-backend/src/test/kotlin/org/cqfn/save/backend/DownloadFilesTest.kt
+++ save-backend/src/test/kotlin/org/cqfn/save/backend/DownloadFilesTest.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.core.io.ByteArrayResource
 import org.springframework.http.client.MultipartBodyBuilder
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.reactive.function.BodyInserters
 import kotlin.io.path.ExperimentalPathApi
