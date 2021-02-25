c6883e1ee271646090ac9ba1a7fb10a8b6180401
aktsay6 <aktsay6@gmail.com>
2021-02-25 14:57:55 +0300

    feature/save-database
    ### What's done:
      * Fixed bugs

--- save-backend/src/main/kotlin/org/cqfn/save/backend/SaveApplication.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/SaveApplication.kt
@@ -2,6 +2,7 @@ package org.cqfn.save.backend
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.web.reactive.config.EnableWebFlux
 
@@ -11,6 +12,7 @@ import org.springframework.web.reactive.config.EnableWebFlux
 @SpringBootApplication
 @EnableWebFlux
 @EnableJpaRepositories(basePackages = ["org.cqfn.save.backend.repository"])
+@EntityScan("org.cqfn.save.entities")
 open class SaveApplication
 
 fun main(args: Array<String>) {
