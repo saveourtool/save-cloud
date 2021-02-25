a12c2dfa267c88a22236877a6a711c97b1929890
aktsay6 <aktsay6@gmail.com>
2021-02-25 14:13:08 +0300

    feature/save-database
    ### What's done:
      * Added OptIn

--- save-common/src/commonMain/kotlin/org/cqfn/save/entities/Annotations.kt
+++ save-common/src/commonMain/kotlin/org/cqfn/save/entities/Annotations.kt
@@ -1,13 +1,16 @@
 package org.cqfn.save.entities
 
+@OptIn
 @OptionalExpectation
 @ExperimentalMultiplatform
 expect annotation class Entity()
 
+@OptIn
 @OptionalExpectation
 @ExperimentalMultiplatform
 expect annotation class Id()
 
+@OptIn
 @OptionalExpectation
 @ExperimentalMultiplatform
 expect annotation class GeneratedValue()
