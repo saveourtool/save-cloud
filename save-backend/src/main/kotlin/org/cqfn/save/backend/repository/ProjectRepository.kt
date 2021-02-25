1d4d5d67fa651864de95deeb82f103897c8be9e6
aktsay6 <aktsay6@gmail.com>
2021-02-25 14:21:24 +0300

    feature/save-database
    ### What's done:
      * Removed Project

--- save-backend/src/main/kotlin/org/cqfn/save/backend/repository/ProjectRepository.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/repository/ProjectRepository.kt
@@ -1,6 +1,6 @@
 package org.cqfn.save.backend.repository
 
-import org.cqfn.save.backend.entities.Project
+import org.cqfn.save.entities.Project
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
