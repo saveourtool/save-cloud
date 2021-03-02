da5f7722c401cbad096abf8ac11c798bbaecc016
kentr0w <qbextted0@gmail.com>
2021-03-02 13:07:49 +0300

    Preprocessor ### What's done: Fixed after merge

--- save-preprocessor/build.gradle.kts
+++ save-preprocessor/build.gradle.kts
@@ -11,6 +11,5 @@ dependencies {
     implementation("org.eclipse.jgit:org.eclipse.jgit:${Versions.jgit}")
     implementation("org.hibernate.javax.persistence:hibernate-jpa-2.1-api:${Versions.jpa}")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:${Versions.springBoot}")
-    implementation("com.h2database:h2:${Versions.h2}")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.serialization}")
 }
