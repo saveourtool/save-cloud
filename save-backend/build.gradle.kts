166424ca45ba91a9f6da17f92ca9c5ed1de5eaa9
Peter Trifanov <peter.trifanov@mail.ru>
2021-03-02 11:52:46 +0300

    Use testcontainers instead of H2
    ### What's done:
    * Extracted common dependencies to buildSrc

--- save-backend/build.gradle.kts
+++ save-backend/build.gradle.kts
@@ -20,16 +20,6 @@ tasks.withType<Test> {
 
 dependencies {
     implementation(project(":save-common"))
-    implementation("org.liquibase:liquibase-core:${Versions.liquibase}")
-    implementation("org.hibernate:hibernate-core:${Versions.hibernate}")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:${Versions.springBoot}")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}")
-    implementation("org.slf4j:slf4j-api:${Versions.slf4j}")
-    implementation("ch.qos.logback:logback-core:${Versions.logback}")
-    implementation("mysql:mysql-connector-java:8.0.23")
-    testImplementation("org.testcontainers:testcontainers:1.15.2")
-    testImplementation("org.testcontainers:mysql:1.15.2")
-    testImplementation("org.testcontainers:junit-jupiter:1.15.2")
 }
 
 configureJacoco()
