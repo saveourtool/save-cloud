0f02fd5a8ba587f0a974cc589e567337bf1c00d9
kentr0w <qbextted0@gmail.com>
2021-03-02 16:18:51 +0300

    Preprocessor ### What's done: Fixed according to our code-style

--- save-common/src/commonMain/kotlin/org/cqfn/save/repository/GitRepository.kt
+++ save-common/src/commonMain/kotlin/org/cqfn/save/repository/GitRepository.kt
@@ -13,8 +13,8 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 data class GitRepository(
-        val url: String,
-        val username: String? = null,
-        val password: String? = null,
-        val branch: String? = null,
+    val url: String,
+    val username: String? = null,
+    val password: String? = null,
+    val branch: String? = null,
 )
