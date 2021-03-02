9c3666f171b22ece69afc00509199d1a7c449d91
kentr0w <qbextted0@gmail.com>
2021-03-02 16:29:55 +0300

    Preprocessor ### What's done: Fixed according to our code-style

--- save-preprocessor/src/main/kotlin/org/cqfn/save/preprocessor/controllers/DownloadProject.kt
+++ save-preprocessor/src/main/kotlin/org/cqfn/save/preprocessor/controllers/DownloadProject.kt
@@ -7,8 +7,9 @@ import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
 import java.io.File
 
 /**
