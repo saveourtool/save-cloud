312bfb3f2e91134be2bc1c22a5e35fdd03121a19
aktsay6 <aktsay6@gmail.com>
2021-03-04 11:26:00 +0300

    feature/liquibase-support
    ### What's done:
      * Move liquibase changesets to root

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,34 @@ import org.cqfn.save.buildutils.installGitHooks
 plugins {
     kotlin("jvm") version Versions.kotlin apply false
     id("com.github.ben-manes.versions") version "0.36.0"
+    id("org.liquibase.gradle") version Versions.liquibaseGradlePlugin
+}
+
+liquibase {
+    activities {
+        // Configuring luiquibase
+        register("main") {
+            arguments = mapOf(
+                    "changeLogFile" to "db/changelog/db.changelog-master.xml",
+                    "url" to "jdbc:mysql://172.17.0.2:3306/test",
+                    "username" to "root",
+                    "password" to "123",
+                    "logLevel" to "info"
+            )
+        }
+    }
+}
+
+dependencies {
+    liquibaseRuntime("org.liquibase:liquibase-core:${Versions.liquibase}")
+    liquibaseRuntime("mysql:mysql-connector-java:${Versions.mySql}")
+    liquibaseRuntime("org.liquibase.ext:liquibase-hibernate5:${Versions.liquibaseHibernate5}")
+}
+
+tasks.register<Copy>("copyLiquibase") {
+    from("db")
+    into("save-backend/build/resources/main/db")
+    into("save-orchestrator/build/resources/main/db")
 }
 
 allprojects {
--- save-backend/build.gradle.kts
+++ save-backend/build.gradle.kts
@@ -4,22 +4,6 @@ import  org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
-    id("org.liquibase.gradle") version Versions.liquibaseGradlePlugin
-}
-
-liquibase {
-    activities {
-        // Configuring luiquibase
-        register("main") {
-            arguments = mapOf(
-                    "changeLogFile" to "save-backend/src/main/resources/db/changelog/db.changelog-master.xml",
-                    "url" to "jdbc:mysql://172.17.0.2:3306/test",
-                    "username" to "root",
-                    "password" to "123",
-                    "logLevel" to "info"
-            )
-        }
-    }
 }
 
 configureSpringBoot(true)
@@ -35,10 +19,6 @@ tasks.withType<Test> {
 }
 
 dependencies {
-    liquibaseRuntime("org.liquibase:liquibase-core:${Versions.liquibase}")
-    liquibaseRuntime("mysql:mysql-connector-java:${Versions.mySql}")
-    liquibaseRuntime("org.liquibase.ext:liquibase-hibernate5:${Versions.liquibaseHibernate5}")
-    liquibaseRuntime(sourceSets.getByName("main").output)
     implementation(project(":save-common"))
     implementation("org.hibernate:hibernate-core:${Versions.hibernate}")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:${Versions.springBoot}")
