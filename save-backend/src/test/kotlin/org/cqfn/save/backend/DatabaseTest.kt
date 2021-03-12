b5e6293b1c2945bb2a9b3fbaf20007875016434a
aktsay6 <aktsay6@gmail.com>
2021-03-12 18:04:09 +0300

    feature/schema-test-agent(#19)
    ### What's done:
      * Fixed bugs

--- save-backend/src/test/kotlin/org/cqfn/save/backend/DatabaseTest.kt
+++ save-backend/src/test/kotlin/org/cqfn/save/backend/DatabaseTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(classes = [SaveApplication::class])
 class DatabaseTest : DatabaseTestBase() {
