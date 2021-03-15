434549a94caf1fb27458950c5112f61b5345d76d
Peter Trifanov <peter.trifanov@mail.ru>
2021-03-15 13:20:34 +0300

    Added configuration for local database
    ### What's done:
    * Added mysql with default password and default database to docker-compose.yaml.template in dev profile
    * Added startMysqlDb task
    * Updated save-deploy/README.md
    * Added run configuration for IDEA
    * Fixed application-dev.properties

--- buildSrc/src/main/kotlin/org/cqfn/save/buildutils/DockerStackConfiguration.kt
+++ buildSrc/src/main/kotlin/org/cqfn/save/buildutils/DockerStackConfiguration.kt
@@ -6,6 +6,7 @@ import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 import java.io.File
+import java.io.ByteArrayOutputStream
 
 fun Project.createStackDeployTask(profile: String) {
     tasks.register<Exec>("startLocalDockerRegistry") {
@@ -49,9 +50,19 @@ fun Project.createStackDeployTask(profile: String) {
 //        }
     }
 
+    tasks.register<Exec>("startMysqlDb") {
+        dependsOn("generateComposeFile")
+        commandLine("docker-compose", "--file", "$buildDir/docker-compose.yaml", "--profile", "dev", "up", "-d", "mysql")
+        errorOutput = ByteArrayOutputStream()
+        if (!errorOutput.toString().contains(" is up-to-date")) {
+            logger.info("Waitnig 5 sec for mysql to start")
+            Thread.sleep(5_000)  // wait 5 sec for mysql to start, can be manually increased when needed
+        }
+    }
+
     tasks.register<Exec>("deployLocal") {
         dependsOn(subprojects.flatMap { it.tasks.withType<BootBuildImage>() })
-        dependsOn("generateComposeFile")
+        dependsOn("startMysqlDb")
         commandLine("docker-compose", "--file", "$buildDir/docker-compose.yaml", "up", "-d", "orchestrator", "backend", "preprocessor")
     }
 }
