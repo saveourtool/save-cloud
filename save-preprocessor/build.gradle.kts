2b95a05379e9b34833e414e10ee63767c9b36259
Denis Kumar <qbextted0@gmail.com>
2021-03-16 14:03:20 +0300

    Preprocessor: add class for configuration properties (#45)
    * Add Configuration
    ### What's done:
    Added configuration for preprocessor

--- save-preprocessor/build.gradle.kts
+++ save-preprocessor/build.gradle.kts
@@ -9,6 +9,7 @@ configureSpringBoot()
 dependencies {
     implementation(project(":save-common"))
     implementation("org.eclipse.jgit:org.eclipse.jgit:${Versions.jgit}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}")
 }
 
 tasks.withType<Test> {
