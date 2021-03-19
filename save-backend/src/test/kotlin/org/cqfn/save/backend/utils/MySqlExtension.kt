a457209431c4ce228fc11cf1040793b0736b8409
Peter Trifanov <peter.trifanov@mail.ru>
2021-03-19 14:48:16 +0300

    Infrastructure enhancements
    ### What's done:
    * Added grafana.ini
    * Updated docker-compose.yaml.template
    * Extracted DatabaseTestBase to a junit5 extension

---
+++ save-backend/src/test/kotlin/org/cqfn/save/backend/utils/MySqlExtension.kt
@@ -0,0 +1,31 @@
+package org.cqfn.save.backend.utils
+
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.testcontainers.containers.JdbcDatabaseContainer
+import org.testcontainers.containers.MySQLContainerProvider
+import org.testcontainers.containers.wait.strategy.Wait
+import java.util.concurrent.atomic.AtomicBoolean
+
+class MySqlExtension : BeforeAllCallback {
+    override fun beforeAll(context: ExtensionContext?) {
+        if (!isDatabaseCreated.getAndSet(true)) {
+            val dbContainer: JdbcDatabaseContainer<*> = MySQLContainerProvider()
+                .newInstance("8.0.20")
+                .withExposedPorts(3306)
+                .withDatabaseName("save_db_test")
+                .waitingFor(Wait.forLogMessage("Container is started (JDBC URL: ", 1))
+                .apply {
+                    start()
+                }
+
+            System.setProperty("spring.datasource.url", dbContainer.jdbcUrl)
+            System.setProperty("spring.datasource.username", dbContainer.username)
+            System.setProperty("spring.datasource.password", dbContainer.password)
+        }
+    }
+
+    companion object {
+        private val isDatabaseCreated = AtomicBoolean(false)
+    }
+}
