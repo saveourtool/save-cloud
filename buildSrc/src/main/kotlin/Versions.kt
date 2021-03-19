fb6d8e674d61eb0bc0372a2feef63db7118a61e6
Peter Trifanov <peter.trifanov@mail.ru>
2021-03-19 18:32:07 +0300

    Infrastructure enhancements
    ### What's done:
    * Dependency updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val jdk = "11"  // jdk version that will be used as kotlin compiler target
     const val BP_JVM_VERSION = "11"  // jvm version for spring boot image build
     const val kotlin = "1.4.31"
-    const val springBoot = "2.4.3"
+    const val springBoot = "2.4.4"
     const val springSecurity = "5.4.5"
     const val hibernate = "5.4.2.Final"
     const val liquibase = "4.3.1"
@@ -12,7 +12,7 @@ object Versions {
     const val ktor = "1.5.2"
     const val coroutines = "1.4.3"
     const val serialization = "1.1.0"  // serialization is compiled by 1.4.30 since version 1.1.0 and for native ABI is different. We can update serialization only after we update kotlin.
-    const val micrometer = "1.6.4"
+    const val micrometer = "1.6.5"
     const val mySql = "8.0.20"
     const val jpa = "1.0.2.Final"
     const val liquibaseGradlePlugin = "2.0.4"
@@ -20,5 +20,5 @@ object Versions {
     const val react = "17.0.1"
     const val kotlinJsWrappersSuffix = "-pre.148-kotlin-1.4.30"
     const val kotlinReact = "$react$kotlinJsWrappersSuffix"
-    const val jgit = "5.10.0.202012080955-r"
+    const val jgit = "5.11.0.202103091610-r"
 }
