a02324395669f8adb19333d2f3174119d88304b5
aktsay6 <aktsay6@gmail.com>
2021-03-29 13:58:02 +0300

    feature/retrieve-batches(#51)
    ### What's done:
      * Fixed bugs

--- save-backend/src/main/kotlin/org/cqfn/save/backend/repository/TestRepository.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/repository/TestRepository.kt
@@ -8,6 +8,11 @@ import org.springframework.data.jpa.repository.Query
  * Repository of tests
  */
 interface TestRepository : JpaRepository<Test, String> {
+    /**
+     * Method to retrieve ready batches
+     *
+     * @return List of Tests
+     */
     @Query(value = "select * from test inner join test_execution on test.id = test_execution.test_id and test_execution.status = 'READY'", nativeQuery = true)
     fun retrieveBatches(): List<Test>
 }
