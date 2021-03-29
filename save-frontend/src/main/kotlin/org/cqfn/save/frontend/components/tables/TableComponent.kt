c4ae285bb583e246857bbaa881ddab281a7a85e7
Peter Trifanov <peter.trifanov@mail.ru>
2021-03-29 18:11:03 +0300

    Frontend: Retrieve projects from backend (#71)
    ### What's done:
    * Added endpoint to backend
    * Added RequestUtils.kt in frontend
    * Supported client-side pagination in TableComponent
    * Fetch data for CollectionView
    * Fixed liquibase contexts in dev profile
    * Added new tasks to manage docker: restartMysqlDb, stopDockerStack
    * Added charset to headers on backend
    * Fixed nunito font link
    * Fixed unicode symbols
    * Updated build_and_test.yml to upload distribution jar instead of zipping the entire folder

--- save-frontend/src/main/kotlin/org/cqfn/save/frontend/components/tables/TableComponent.kt
+++ save-frontend/src/main/kotlin/org/cqfn/save/frontend/components/tables/TableComponent.kt
@@ -9,7 +9,9 @@ package org.cqfn.save.frontend.components.tables
 import org.cqfn.save.frontend.utils.spread
 
 import react.RProps
+import react.dom.button
 import react.dom.div
+import react.dom.em
 import react.dom.h6
 import react.dom.span
 import react.dom.table
@@ -20,6 +22,8 @@ import react.dom.tr
 import react.functionalComponent
 import react.table.Column
 import react.table.TableInstance
+import react.table.TableState
+import react.table.usePagination
 import react.table.useSortBy
 import react.table.useTable
 import react.useEffect
@@ -27,6 +31,10 @@ import react.useMemo
 import react.useState
 
 import kotlin.js.json
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.html.ButtonType
+import kotlinx.html.js.onClickFunction
 
 /**
  * [RProps] of a data table
@@ -43,20 +51,39 @@ external interface TableProps : RProps {
  *
  * @param columns columns as an array of [Column]
  * @param getData a function to retrieve data for the table, returns an array of data of type [out D] that will be inserted into the table
+ * @param initialPageSize initial size of table page
+ * @param useServerPaging whether data is split into pages server-side or in browser
  * @return a functional react component
  */
 @Suppress("TOO_LONG_FUNCTION", "ForbiddenComment", "LongMethod")
-fun <D : Any> tableComponent(columns: Array<out Column<D, *>>, getData: () -> Array<out D>) = functionalComponent<TableProps> { props ->
+fun <D : Any> tableComponent(columns: Array<out Column<D, *>>,
+                             initialPageSize: Int = 10,
+                             useServerPaging: Boolean = false,
+                             getData: suspend () -> Array<out D>,
+) = functionalComponent<TableProps> { props ->
     val (data, setData) = useState<Array<out D>>(emptyArray())
 
-    // with `emptyList` effect will be executed only once on the initial render. TODO: how to handle dynamic data updates, e.g. when paging?
-    useEffect(emptyList()) {
-        setData(getData())
-    }
-
-    val tableInstance: TableInstance<D> = useTable(useSortBy) {
+    val tableInstance: TableInstance<D> = useTable(useSortBy, usePagination) {
         this.columns = useMemo { columns }
         this.data = data
+        this.initialState = json(
+            "pageSize" to initialPageSize,
+            "pageIndex" to 0,
+            "manualPagination" to useServerPaging
+        ).unsafeCast<TableState<D>>()
+    }
+
+    // list of entities, updates of which will cause update of the data retrieving effect
+    val dependencies = if (useServerPaging) {
+        listOf(tableInstance.state.pageIndex, tableInstance.state.pageSize)
+    } else {
+        // when all data is already available, we don't need to repeat `getData` calls
+        emptyList()
+    }
+    useEffect(dependencies) {
+        GlobalScope.launch {
+            setData(getData())
+        }
     }
 
     div("card shadow mb-4") {
@@ -94,7 +121,7 @@ fun <D : Any> tableComponent(columns: Array<out Column<D, *>>, getData: () -> Ar
                     }
                     tbody {
                         spread(tableInstance.getTableBodyProps())
-                        tableInstance.rows.map { row ->
+                        tableInstance.page.map { row ->
                             tableInstance.prepareRow(row)
                             tr {
                                 spread(row.getRowProps())
@@ -106,6 +133,31 @@ fun <D : Any> tableComponent(columns: Array<out Column<D, *>>, getData: () -> Ar
                                         }
                                     }))
                                 }
+                            }
+                        }
+                    }
+                }
+                if (tableInstance.pageCount > 1) {
+                    // block with paging controls
+                    div {
+                        button(type = ButtonType.button, classes = "btn btn-link") {
+                            attrs.onClickFunction = {
+                                tableInstance.previousPage()
+                            }
+                            attrs.disabled = !tableInstance.canPreviousPage
+                            +js("String.fromCharCode(8592)").unsafeCast<String>()
+                        }
+                        button(type = ButtonType.button, classes = "btn btn-link") {
+                            attrs.onClickFunction = {
+                                tableInstance.nextPage()
+                            }
+                            attrs.disabled = !tableInstance.canNextPage
+                            +js("String.fromCharCode(8594)").unsafeCast<String>()
+                        }
+                        div {
+                            +"Page "
+                            em {
+                                +"${tableInstance.state.pageIndex + 1} of ${tableInstance.pageCount}"
                             }
                         }
                     }
