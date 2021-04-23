9d332d599ac322d6c6b2c03713e172b7d2ee54d3
aktsay6 <aktsay6@gmail.com>
2021-04-23 15:54:14 +0300

    bugfix/offset-per-execution(#97)
    ### What's done:
      * Fixed bugs

--- save-backend/src/main/kotlin/org/cqfn/save/backend/repository/TestRepository.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/repository/TestRepository.kt
@@ -14,8 +14,10 @@ interface TestRepository : BaseEntityRepository<Test> {
      *
      * @param limit
      * @param offset
+     * @param executionId
      * @return List of Tests
      */
-    @Query(value = "select * from test inner join test_execution on test.id = test_execution.test_id and test_execution.status = 'READY' and test_execution.test_suite_execution_id = ?3 limit ?1 offset ?2", nativeQuery = true)
+    @Query(value = "select * from test inner join test_execution on test.id = test_execution.test_id and test_execution.status = 'READY' " +
+            "and test_execution.test_suite_execution_id = ?3 limit ?1 offset ?2", nativeQuery = true)
     fun retrieveBatches(limit: Int, offset: Int, executionId: Long): List<Test>
 }
