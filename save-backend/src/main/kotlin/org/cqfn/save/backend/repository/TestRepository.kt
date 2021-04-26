b524b4f6d459a98b931ce451aed8254a5273c9e4
aktsay6 <aktsay6@gmail.com>
2021-04-26 10:57:09 +0300

    bugfix/offset-per-execution(#92)
    ### What's done:
      * Fixed bugs

--- save-backend/src/main/kotlin/org/cqfn/save/backend/repository/TestRepository.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/repository/TestRepository.kt
@@ -1,7 +1,6 @@
 package org.cqfn.save.backend.repository
 
 import org.cqfn.save.entities.Test
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 /**
