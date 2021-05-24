cb2dc79dc73d1feca99b999805cb8ff6a96a6361
kentr0w <qbextted0@gmail.com>
2021-05-24 15:11:16 +0300

    Save frontend submit execution request
    ### What's done:
    Fixed according to our code-style

--- save-backend/src/main/kotlin/org/cqfn/save/backend/configs/CorsFilter.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/configs/CorsFilter.kt
@@ -8,23 +8,23 @@ import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
 
+/**
+ * Component to frontend
+ */
 @Component
+@Suppress("LONG_LINE")
 class CorsFilter : WebFilter {
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-        if (exchange != null) {
-            exchange.response.headers.add("Access-Control-Allow-Origin", "*")
-            exchange.response.headers.add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-            exchange.response.headers.add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range")
-            if (exchange.request.method == HttpMethod.OPTIONS) {
-                exchange.response.headers.add("Access-Control-Max-Age", "1728000")
-                exchange.response.statusCode = HttpStatus.NO_CONTENT
-                return Mono.empty()
-            } else {
-                exchange.response.headers.add("Access-Control-Expose-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range")
-                return chain?.filter(exchange) ?: Mono.empty()
-            }
+        exchange.response.headers.add("Access-Control-Allow-Origin", "*")
+        exchange.response.headers.add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+        exchange.response.headers.add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range")
+        return if (exchange.request.method == HttpMethod.OPTIONS) {
+            exchange.response.headers.add("Access-Control-Max-Age", "1728000")
+            exchange.response.statusCode = HttpStatus.NO_CONTENT
+            Mono.empty()
         } else {
-            return chain?.filter(exchange) ?: Mono.empty()
+            exchange.response.headers.add("Access-Control-Expose-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range")
+            chain.filter(exchange)
         }
     }
 }
