ee9c0eecb86c1fa8527f037d51e1945a5db0b59c
kentr0w <qbextted0@gmail.com>
2021-05-26 14:20:48 +0300

    Upload binary file
    ### What's done:
    Fixed according to our code style

--- save-backend/src/test/kotlin/org/cqfn/save/backend/controller/CloningRepositoryControllerTest.kt
+++ save-backend/src/test/kotlin/org/cqfn/save/backend/controller/CloningRepositoryControllerTest.kt
@@ -17,7 +17,6 @@ import org.cqfn.save.repository.GitRepository
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
