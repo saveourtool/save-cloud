94be0eb1d65bba562fd9322e68815b03c7f1b474
Peter Trifanov <peter.trifanov@mail.ru>
2021-06-03 16:25:25 +0300

    Update Kotlin to 1.5.0
    ### What's done:
    * Fixes

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,8 +5,8 @@ object Versions {
     const val kotlin = "1.5.0"
     const val ktor = "1.6.0"
     const val coroutines = "1.5.0"
-    const val serialization = "1.2.1"
-    const val kotlinxDatetime = "0.2.1"
+    const val serialization = "1.1.0"
+    const val kotlinxDatetime = "0.2.0"
     const val saveCore = "0.1.0-SNAPSHOT"
 
     // microservices
