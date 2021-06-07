b19948185c3336d875fb40a660959d361b1f92d4
kentr0w <qbextted0@gmail.com>
2021-06-07 17:46:11 +0300

    Upload binary file
    ### What's done:
    Fixed according detect code style

--- save-backend/src/main/kotlin/org/cqfn/save/backend/controllers/ProjectController.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/controllers/ProjectController.kt
@@ -5,7 +5,11 @@ import org.cqfn.save.backend.service.ProjectService
 import org.cqfn.save.entities.ExecutionRequest
 import org.cqfn.save.entities.Project
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 
 /**
  * Controller for working with projects.
