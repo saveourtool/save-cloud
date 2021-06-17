0588262eb457a690f8c149959fff13f46eb43ca6
kentr0w <qbextted0@gmail.com>
2021-06-18 01:12:32 +0300

    Show history in frontend
    ###What's done:
    Fixed bugs to build

--- save-orchestrator/src/test/kotlin/org/cqfn/save/orchestrator/service/DockerServiceTest.kt
+++ save-orchestrator/src/test/kotlin/org/cqfn/save/orchestrator/service/DockerServiceTest.kt
@@ -55,7 +55,8 @@ class DockerServiceTest {
     fun `should create a container with save agent and test resources and start it`() {
         // build base image
         val project = Project("Huawei", "huaweiName", "huaweiUrl", "description")
-        val testExecution = Execution(project, LocalDateTime.now(), LocalDateTime.now(), ExecutionStatus.PENDING, "1", "foo", 0, 20, ExecutionType.GIT, "0.0.1").apply {
+        val testExecution = Execution(project, LocalDateTime.now(), LocalDateTime.now(), ExecutionStatus.PENDING, "1",
+            "foo", 0, 20, ExecutionType.GIT, "0.0.1", 0, 0, 0).apply {
             id = 42L
         }
         testContainerId = dockerService.buildAndCreateContainers(testExecution).single()
