ed27acef0f7194a3eaf43b3c999f645ae1e32005
kentr0w <qbextted0@gmail.com>
2021-06-22 16:09:19 +0300

    History in front-end
    ###What's done:
    Fixed bug

--- save-frontend/src/main/kotlin/org/cqfn/save/frontend/components/views/HistoryView.kt
+++ save-frontend/src/main/kotlin/org/cqfn/save/frontend/components/views/HistoryView.kt
@@ -58,7 +58,7 @@ class HistoryView : RComponent<HistoryProps, RState>() {
                 column("date", "Date") {
                     td {
                         +(it.value.endTime?.let {
-                            Instant.fromEpochSeconds(it.toString().toLong())  // there is some bug with parse endTime to Instant - that why this crutch
+                            Instant.fromEpochSeconds(it, 0)
                                 .toString()
                                 .replace("[TZ]".toRegex(), " ")
                         } ?: "RUNNING")
