34c3f57c3dabd8523c3b698a7cfe1b1bd0f522f5
kentr0w <qbextted0@gmail.com>
2021-07-16 12:54:27 +0300

    Change execution in all cases
    ###What's done:
    Fixed to build

--- save-preprocessor/src/main/kotlin/org/cqfn/save/preprocessor/controllers/DownloadProjectController.kt
+++ save-preprocessor/src/main/kotlin/org/cqfn/save/preprocessor/controllers/DownloadProjectController.kt
@@ -113,7 +113,8 @@ class DownloadProjectController(private val configProperties: ConfigProperties) 
     @Suppress(
         "TooGenericExceptionCaught",
         "TOO_LONG_FUNCTION",
-        "TOO_MANY_LINES_IN_LAMBDA")
+        "TOO_MANY_LINES_IN_LAMBDA",
+        "UnsafeCallOnNullableType")
     private fun downLoadRepository(executionRequest: ExecutionRequest) {
         val gitDto = executionRequest.gitDto
         val project = executionRequest.project
