121288b8baec9a585922899f783c874ff40f11e1
Peter Trifanov <peter.trifanov@mail.ru>
2021-07-19 17:09:05 +0300

    Send execution data from agent to backend
    ### What's done:
    * Fixes logic for standard executions

--- save-backend/src/main/kotlin/org/cqfn/save/backend/utils/LongUtils.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/utils/LongUtils.kt
@@ -9,13 +9,6 @@ import java.time.LocalDateTime
 import java.time.ZoneOffset
 
 /**
- * Function to convert long number of milliseconds to LocalDateTime
- *
- * @return an instance of LocalDateTime
- */
-fun Long.millisToLocalDateTime(): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(this), ZoneOffset.UTC)
-
-/**
  * Function to convert long number of seconds to LocalDateTime
  *
  * @return an instance of LocalDateTime
