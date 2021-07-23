380cbfe7877eb95947dc9a16e7ee92843d9dee27
Peter Trifanov <peter.trifanov@mail.ru>
2021-07-23 12:42:49 +0300

    Dependency updates
    ### What's done:
    * agent

--- save-agent/src/linuxX64Main/kotlin/org/cqfn/save/agent/SaveAgent.kt
+++ save-agent/src/linuxX64Main/kotlin/org/cqfn/save/agent/SaveAgent.kt
@@ -50,12 +50,7 @@ import kotlinx.serialization.modules.SerializersModule
 class SaveAgent(private val config: AgentConfiguration,
                 private val httpClient: HttpClient = HttpClient {
                     install(JsonFeature) {
-                        serializer = KotlinxSerializer(Json {
-                            serializersModule = SerializersModule {
-                                // for some reason for K/N it's needed explicitly, at least for ktor 1.5.1, kotlin 1.4.21
-                                contextual(HeartbeatResponse::class, HeartbeatResponse.serializer())
-                            }
-                        })
+                        serializer = KotlinxSerializer()
                     }
                     install(HttpTimeout) {
                         requestTimeoutMillis = config.requestTimeoutMillis
