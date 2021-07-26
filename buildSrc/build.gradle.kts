931e2877bab0379f1bd2d3fe2cad19c29297a460
Peter Trifanov <peter.trifanov@mail.ru>
2021-07-26 19:01:47 +0300

    Fix InvalidMutabilityException from Ktor/kx.serialization in agent
    ### What's done:
    * Moved HttpClient creation outside of constructor
    * Bump gradle to 7.1.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,11 +7,12 @@ repositories {
     mavenCentral()
 }
 
+val kotlinVersion = "1.5.21"
 dependencies {
-    runtimeOnly(kotlin("gradle-plugin", "1.5.21"))
+    runtimeOnly(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.springframework.boot:spring-boot-gradle-plugin:2.4.5")
     implementation("org.cqfn.diktat:diktat-gradle-plugin:1.0.0-rc.2")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.17.1")
     implementation("org.ajoberstar.reckon:reckon-gradle:0.13.0")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.5.0")
+    implementation(kotlin("allopen", kotlinVersion))
 }
