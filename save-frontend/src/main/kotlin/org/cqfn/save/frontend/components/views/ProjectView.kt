05b39d3094786783027e6be2fee2822602605ca0
kentr0w <qbextted0@gmail.com>
2021-07-26 11:27:35 +0300

    Install jdk or python into agent
    ###What's done:
    Fixed to build

--- save-frontend/src/main/kotlin/org/cqfn/save/frontend/components/views/ProjectView.kt
+++ save-frontend/src/main/kotlin/org/cqfn/save/frontend/components/views/ProjectView.kt
@@ -35,6 +35,7 @@ import react.RProps
 import react.RState
 import react.child
 import react.dom.a
+import react.dom.attrs
 import react.dom.button
 import react.dom.defaultValue
 import react.dom.div
