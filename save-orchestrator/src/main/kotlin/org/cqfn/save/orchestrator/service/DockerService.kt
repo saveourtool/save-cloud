1903ee0ed59414f6dafdb8de593d7b31fe0c2210
Peter Trifanov <peter.trifanov@mail.ru>
2021-09-21 15:21:27 +0300

    Remove usages of `run.sh` from SAVE repo
    ### What's done:
    * Removed code
    * Minor refactoring
    
    Closes #255

--- save-orchestrator/src/main/kotlin/org/cqfn/save/orchestrator/service/DockerService.kt
+++ save-orchestrator/src/main/kotlin/org/cqfn/save/orchestrator/service/DockerService.kt
@@ -159,7 +159,7 @@ class DockerService(private val configProperties: ConfigProperties) {
         val saveCliExecFlags = if (testSuitesForDocker.isNotEmpty()) {
             // create stub toml config in aim to execute all test suites directories from `testSuitesDir`
             testSuitesDir.resolve("save.toml").apply { createNewFile() }.writeText("[general]")
-            " \"$standardTestSuiteDir\" --include-suites \"${testSuitesForDocker.map { it.name }.joinToString(" ") }\""
+            " \"$standardTestSuiteDir\" --include-suites \"${testSuitesForDocker.joinToString(" ") { it.name }}\""
         } else {
             ""
         }
@@ -246,20 +246,18 @@ class DockerService(private val configProperties: ConfigProperties) {
             ClassPathResource("agent.properties").inputStream,
             agentPropertiesFile
         )
-        val resourcesPath = File(
-            configProperties.testResources.basePath,
-            execution.resourcesRootPath,
-        )
+        val cliCommand = "./$SAVE_CLI_EXECUTABLE_NAME$saveCliExecFlags"
         agentPropertiesFile.writeText(
             agentPropertiesFile.readLines().joinToString(System.lineSeparator()) {
-                if (it.startsWith("id=")) "id=$containerId" else it
+                if (it.startsWith("id=")) {
+                    "id=$containerId"
+                } else if (it.startsWith("cliCommand=")) {
+                    "cliCommand=$cliCommand"
+                } else {
+                    it
+                }
             }
         )
-        // todo: un-hardcode script
-        if (File(resourcesPath, "examples/kotlin-diktat/run.sh").exists()) {
-            val cliCommand = "bash ./examples/kotlin-diktat/run.sh || ./$SAVE_CLI_EXECUTABLE_NAME$saveCliExecFlags"
-            agentPropertiesFile.appendText("\ncliCommand=$cliCommand\n")
-        }
         containerManager.copyResourcesIntoContainer(
             containerId, executionDir,
             listOf(agentPropertiesFile)
