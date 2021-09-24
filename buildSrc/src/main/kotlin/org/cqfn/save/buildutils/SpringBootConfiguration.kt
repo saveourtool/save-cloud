9cc98ce0f7f5a95c710d832351a6dc16404dfd5e
Peter Trifanov <peter.trifanov@mail.ru>
2021-09-24 16:55:06 +0300

    Deploy images to ghcr.io
    ### What's done:
    * WIP

--- buildSrc/src/main/kotlin/org/cqfn/save/buildutils/SpringBootConfiguration.kt
+++ buildSrc/src/main/kotlin/org/cqfn/save/buildutils/SpringBootConfiguration.kt
@@ -72,6 +72,7 @@ fun Project.configureSpringBoot(withSpringDataJpa: Boolean = false) {
             "BPE_DELIM_JAVA_TOOL_OPTIONS" to " ",
             "BPE_APPEND_JAVA_TOOL_OPTIONS" to "-Dreactor.netty.pool.maxIdleTime=60000 -Dreactor.netty.pool.leasingStrategy=lifo"
         )
+        isVerboseLogging = true
         val registryPassword: String? = System.getenv("GHCR_PWD")
         isPublish = registryPassword != null
         if (isPublish) {
