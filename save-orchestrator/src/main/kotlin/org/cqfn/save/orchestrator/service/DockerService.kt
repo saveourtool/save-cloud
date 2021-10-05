c9922d84242afdeb114b9b098fe67411df632507
Cheshiriks <vlad-20-11-94@yandex.ru>
2021-10-05 16:09:00 +0300

    Fixed chmod
    What's done:
    * fixed chmod

--- save-orchestrator/src/main/kotlin/org/cqfn/save/orchestrator/service/DockerService.kt
+++ save-orchestrator/src/main/kotlin/org/cqfn/save/orchestrator/service/DockerService.kt
@@ -188,8 +188,12 @@ class DockerService(private val configProperties: ConfigProperties) {
                     |RUN ln -fs /usr/share/zoneinfo/UTC /etc/localtime
                     |RUN chmod +x $executionDir/$SAVE_AGENT_EXECUTABLE_NAME
                     |RUN chmod +x $executionDir/$SAVE_CLI_EXECUTABLE_NAME
-                    |RUN find . -name $executionDir/diktat-rules/src/test/resources/test/smoke/ktlint -type f -exec chmod +x {} \;
-                    |RUN find . -name $executionDir/clang-tools-extra/test/clang-tidy/clang-tidy-linux -type f -exec chmod +x {} \;
+                    |RUN if [ -d $executionDir/diktat-rules/src/test/resources/test/smoke ]; then 
+                    |   find $executionDir/diktat-rules/src/test/resources/test/smoke -type f -name "ktlint" -exec chmod +x {} \;
+                    |fi
+                    |RUN if [ -d $executionDir/clang-tools-extra/test/clang-tidy ]; 
+                    |   then find $executionDir/clang-tools-extra/test/clang-tidy -type f -name "clang-tidy-linux" -exec chmod +x {} \;
+                    |fi 
                 """
         )
         saveAgent.delete()
