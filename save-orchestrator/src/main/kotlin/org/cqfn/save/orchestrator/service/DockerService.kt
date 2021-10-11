8d31853278f5b130d9368a7960d81caaa21463f8
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-10-11 11:24:20 +0300

    Add more indo in logging ### What's done: * Add more indo in logging

--- save-orchestrator/src/main/kotlin/org/cqfn/save/orchestrator/service/DockerService.kt
+++ save-orchestrator/src/main/kotlin/org/cqfn/save/orchestrator/service/DockerService.kt
@@ -103,7 +103,8 @@ class DockerService(private val configProperties: ConfigProperties) {
                             containerManager.dockerClient.stopContainerCmd(it).exec()
                             log.info("Agent with id=$it has been stopped")
                         } else {
-                            log.warn("Agent with id=$it was requested to be stopped, but it actually doesn't running")
+                            log.warn("Agent with id=$it was requested to be stopped, " +
+                                    "but it actual state=${containerManager.dockerClient.inspectContainerCmd(it).exec().state}")
                         }
                     }
                     true
