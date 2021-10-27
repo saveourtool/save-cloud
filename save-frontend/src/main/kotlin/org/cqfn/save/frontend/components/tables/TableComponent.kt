a8702bc53d113084e0d83cc019996ad5c5264283
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-10-27 15:16:41 +0300

    Refactoring ### What's done: * Refactoring

--- save-frontend/src/main/kotlin/org/cqfn/save/frontend/components/tables/TableComponent.kt
+++ save-frontend/src/main/kotlin/org/cqfn/save/frontend/components/tables/TableComponent.kt
@@ -46,10 +46,14 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.html.ButtonType
+import kotlinx.html.I
 import kotlinx.html.InputType
+import kotlinx.html.Tag
 import kotlinx.html.hidden
 import kotlinx.html.js.onChangeFunction
 import kotlinx.html.js.onClickFunction
+import react.StateSetter
+import react.dom.RDOMBuilder
 
 /**
  * [RProps] of a data table
@@ -142,46 +146,8 @@ fun <D : Any> tableComponent(
         }
     }
 
-    div("row") {
-        div("col-0 pt-3 pl-3 pr-0") {
-            +"Show "
-        }
-        div("col-1 pr-0") {
-            div("input-group input-group-sm mb-3 mt-3") {
-                select(classes = "form-control") {
-                    option("list-group-item") {
-                        val entries = "10"
-                        attrs.value = entries
-                        +entries
-                    }
-                    option("list-group-item") {
-                        val entries = "25"
-                        attrs.value = entries
-                        +entries
-                    }
-                    option("list-group-item") {
-                        val entries = "50"
-                        attrs.value = entries
-                        +entries
-                    }
-                    option("list-group-item") {
-                        val entries = "100"
-                        attrs.value = entries
-                        +entries
-                    }
-                    attrs.onChangeFunction = {
-                        val tg = it.target as HTMLSelectElement
-                        val entries = tg.value
-                        setPageIndex(0)
-                        tableInstance.gotoPage(0)
-                        tableInstance.setPageSize(entries.toInt())
-                    }
-                }
-            }
-        }
-        div("col-0 pt-3 pl-2") {
-            +" entries"
-        }
+    div {
+        setEntries(tableInstance, setPageIndex)
     }
 
     div("card shadow mb-4") {
@@ -237,137 +203,7 @@ fun <D : Any> tableComponent(
                 }
                 if (tableInstance.pageCount > 1) {
                     // block with paging controls
-                    div("row") {
-                        // First page
-                        button(type = ButtonType.button, classes = "btn btn-link") {
-                            attrs.onClickFunction = {
-                                setPageIndex(0)
-                                tableInstance.gotoPage(0)
-                            }
-                            attrs.disabled = !tableInstance.canPreviousPage
-                            +js("String.fromCharCode(171)").unsafeCast<String>()
-                        }
-                        // Previous page icon <
-                        button(type = ButtonType.button, classes = "btn btn-link") {
-                            attrs.onClickFunction = {
-                                setPageIndex(pageIndex - 1)
-                                tableInstance.previousPage()
-                            }
-                            attrs.disabled = !tableInstance.canPreviousPage
-                            +js("String.fromCharCode(8249)").unsafeCast<String>()
-                        }
-                        // Previous before previous page
-                        button(type = ButtonType.button, classes = "btn btn-link") {
-                            val index = pageIndex - 2
-                            attrs.onClickFunction = {
-                                setPageIndex(index)
-                                tableInstance.gotoPage(index)
-                            }
-                            attrs.hidden = (index < 0)
-                            em {
-                                +"${index + 1}"
-                            }
-                        }
-                        // Previous page number
-                        button(type = ButtonType.button, classes = "btn btn-link") {
-                            attrs.onClickFunction = {
-                                setPageIndex(pageIndex - 1)
-                                tableInstance.previousPage()
-                            }
-                            attrs.hidden = !tableInstance.canPreviousPage
-                            em {
-                                +pageIndex
-                            }
-                        }
-                        // Current page number
-                        button(type = ButtonType.button, classes = "btn btn-link") {
-                            attrs.disabled = true
-                            em {
-                                +"${pageIndex + 1}"
-                            }
-                        }
-                        // Next page number
-                        button(type = ButtonType.button, classes = "btn btn-link") {
-                            attrs.onClickFunction = {
-                                setPageIndex(pageIndex + 1)
-                                tableInstance.nextPage()
-                            }
-                            attrs.hidden = !tableInstance.canNextPage
-                            em {
-                                +"${pageIndex + 2}"
-                            }
-                        }
-                        // Next after next page
-                        button(type = ButtonType.button, classes = "btn btn-link") {
-                            val index = pageIndex + 2
-                            attrs.onClickFunction = {
-                                setPageIndex(index)
-                                tableInstance.gotoPage(index)
-                            }
-                            attrs.hidden = (index > pageCount - 1)
-                            em {
-                                +"${index + 1}"
-                            }
-                        }
-                        // Next page icon >
-                        button(type = ButtonType.button, classes = "btn btn-link") {
-                            attrs.onClickFunction = {
-                                setPageIndex(pageIndex + 1)
-                                tableInstance.nextPage()
-                            }
-                            attrs.disabled = !tableInstance.canNextPage
-                            +js("String.fromCharCode(8250)").unsafeCast<String>()
-                        }
-                        // Last page
-                        button(type = ButtonType.button, classes = "btn btn-link") {
-                            attrs.onClickFunction = {
-                                setPageIndex(pageCount - 1)
-                                tableInstance.gotoPage(pageCount - 1)
-                            }
-                            attrs.disabled = !tableInstance.canNextPage
-                            +js("String.fromCharCode(187)").unsafeCast<String>()
-                        }
-                        var number = 0
-                        form {
-                            div("row") {
-                                div("col-8 pr-0") {
-                                    div("input-group input-group-sm mb-3 mt-3") {
-                                        input(type = InputType.text, classes = "form-control") {
-                                            attrs["aria-describedby"] = "basic-addon2"
-                                            attrs.placeholder = "Jump to the page"
-                                            attrs {
-                                                onChangeFunction = {
-                                                    // TODO: Provide validation of non int types
-                                                    val tg = it.target as HTMLInputElement
-                                                    number = tg.value.toInt() - 1
-                                                    if (number < 0) {
-                                                        number = 0
-                                                    }
-                                                    if (number > pageCount - 1) {
-                                                        number = pageCount - 1
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-
-                                div("col-sm-offset-10 mr-3 justify-content-start") {
-                                    div("input-group input-group-sm mb-6") {
-                                        div("input-group-append mt-3") {
-                                            button(type = ButtonType.submit, classes = "btn btn-outline-secondary") {
-                                                attrs.onClickFunction = {
-                                                    setPageIndex(number)
-                                                    tableInstance.gotoPage(number)
-                                                }
-                                                +js("String.fromCharCode(10143)").unsafeCast<String>()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    pagingControl(tableInstance, setPageIndex, pageIndex, pageCount)
                     div {
                         +"Page "
                         em {
@@ -385,5 +221,184 @@ fun <D : Any> tableComponent(
             attrs.isOpen = isModalOpen
         }) {
         setIsModalOpen(false)
+    }
+}
+
+fun <T : Tag, D:Any> RDOMBuilder<T>.setEntries(tableInstance: TableInstance<D>, setPageIndex: StateSetter<Int>) =     div("row") {
+    div("col-0 pt-3 pl-3 pr-0") {
+        +"Show "
+    }
+    div("col-1 pr-0") {
+        div("input-group input-group-sm mb-3 mt-3") {
+            select(classes = "form-control") {
+                option("list-group-item") {
+                    val entries = "10"
+                    attrs.value = entries
+                    +entries
+                }
+                option("list-group-item") {
+                    val entries = "25"
+                    attrs.value = entries
+                    +entries
+                }
+                option("list-group-item") {
+                    val entries = "50"
+                    attrs.value = entries
+                    +entries
+                }
+                option("list-group-item") {
+                    val entries = "100"
+                    attrs.value = entries
+                    +entries
+                }
+                attrs.onChangeFunction = {
+                    val tg = it.target as HTMLSelectElement
+                    val entries = tg.value
+                    setPageIndex(0)
+                    tableInstance.gotoPage(0)
+                    tableInstance.setPageSize(entries.toInt())
+                }
+            }
+        }
+    }
+    div("col-0 pt-3 pl-2") {
+        +" entries"
+    }
+}
+
+
+fun <T : Tag, D:Any> RDOMBuilder<T>.pagingControl(tableInstance: TableInstance<D>, setPageIndex: StateSetter<Int>, pageIndex: Int, pageCount: Int) =                     div("row") {
+    // First page
+    button(type = ButtonType.button, classes = "btn btn-link") {
+        attrs.onClickFunction = {
+            setPageIndex(0)
+            tableInstance.gotoPage(0)
+        }
+        attrs.disabled = !tableInstance.canPreviousPage
+        +js("String.fromCharCode(171)").unsafeCast<String>()
+    }
+    // Previous page icon <
+    button(type = ButtonType.button, classes = "btn btn-link") {
+        attrs.onClickFunction = {
+            setPageIndex(pageIndex - 1)
+            tableInstance.previousPage()
+        }
+        attrs.disabled = !tableInstance.canPreviousPage
+        +js("String.fromCharCode(8249)").unsafeCast<String>()
+    }
+    // Previous before previous page
+    button(type = ButtonType.button, classes = "btn btn-link") {
+        val index = pageIndex - 2
+        attrs.onClickFunction = {
+            setPageIndex(index)
+            tableInstance.gotoPage(index)
+        }
+        attrs.hidden = (index < 0)
+        em {
+            +"${index + 1}"
+        }
+    }
+    // Previous page number
+    button(type = ButtonType.button, classes = "btn btn-link") {
+        attrs.onClickFunction = {
+            setPageIndex(pageIndex - 1)
+            tableInstance.previousPage()
+        }
+        attrs.hidden = !tableInstance.canPreviousPage
+        em {
+            +pageIndex.toString()
+        }
+    }
+    // Current page number
+    button(type = ButtonType.button, classes = "btn btn-link") {
+        attrs.disabled = true
+        em {
+            +"${pageIndex + 1}"
+        }
+    }
+    // Next page number
+    button(type = ButtonType.button, classes = "btn btn-link") {
+        attrs.onClickFunction = {
+            setPageIndex(pageIndex + 1)
+            tableInstance.nextPage()
+        }
+        attrs.hidden = !tableInstance.canNextPage
+        em {
+            +"${pageIndex + 2}"
+        }
+    }
+    // Next after next page
+    button(type = ButtonType.button, classes = "btn btn-link") {
+        val index = pageIndex + 2
+        attrs.onClickFunction = {
+            setPageIndex(index)
+            tableInstance.gotoPage(index)
+        }
+        attrs.hidden = (index > pageCount - 1)
+        em {
+            +"${index + 1}"
+        }
+    }
+    // Next page icon >
+    button(type = ButtonType.button, classes = "btn btn-link") {
+        attrs.onClickFunction = {
+            setPageIndex(pageIndex + 1)
+            tableInstance.nextPage()
+        }
+        attrs.disabled = !tableInstance.canNextPage
+        +js("String.fromCharCode(8250)").unsafeCast<String>()
+    }
+    // Last page
+    button(type = ButtonType.button, classes = "btn btn-link") {
+        attrs.onClickFunction = {
+            setPageIndex(pageCount - 1)
+            tableInstance.gotoPage(pageCount - 1)
+        }
+        attrs.disabled = !tableInstance.canNextPage
+        +js("String.fromCharCode(187)").unsafeCast<String>()
+    }
+    // Jump to the concrete page
+    jumpToPage(tableInstance, setPageIndex, pageCount)
+}
+
+fun <T : Tag, D:Any> RDOMBuilder<T>.jumpToPage(tableInstance: TableInstance<D>, setPageIndex: StateSetter<Int>, pageCount: Int) =
+form {
+    var number = 0
+    div("row") {
+        div("col-8 pr-0") {
+            div("input-group input-group-sm mb-3 mt-3") {
+                input(type = InputType.text, classes = "form-control") {
+                    attrs["aria-describedby"] = "basic-addon2"
+                    attrs.placeholder = "Jump to the page"
+                    attrs {
+                        onChangeFunction = {
+                            // TODO: Provide validation of non int types
+                            val tg = it.target as HTMLInputElement
+                            number = tg.value.toInt() - 1
+                            if (number < 0) {
+                                number = 0
+                            }
+                            if (number > pageCount - 1) {
+                                number = pageCount - 1
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        div("col-sm-offset-10 mr-3 justify-content-start") {
+            div("input-group input-group-sm mb-6") {
+                div("input-group-append mt-3") {
+                    button(type = ButtonType.submit, classes = "btn btn-outline-secondary") {
+                        attrs.onClickFunction = {
+                            setPageIndex(number)
+                            tableInstance.gotoPage(number)
+                        }
+                        +js("String.fromCharCode(10143)").unsafeCast<String>()
+                    }
+                }
+            }
+        }
     }
 }
