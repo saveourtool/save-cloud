803c7304ba7cdc19e9726a7e65f43edd5bc380ea
Peter Trifanov <peter.trifanov@mail.ru>
2021-11-24 12:58:59 +0300

    Hotfix: disable redirect to /login unless `secure` profile is activated

--- save-backend/src/main/kotlin/org/cqfn/save/backend/configs/WebConfiguration.kt
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/configs/WebConfiguration.kt
@@ -3,6 +3,7 @@ package org.cqfn.save.backend.configs
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
 import org.springframework.http.HttpStatus
@@ -15,7 +16,7 @@ import reactor.kotlin.core.publisher.switchIfEmpty
 @Configuration
 class WebConfiguration {
     /**
-     * @return a rotuer bean
+     * @return a router bean
      */
     @Bean
     fun staticResourceRouter() = router {
@@ -26,8 +27,21 @@ class WebConfiguration {
      * @param html requested resource
      * @return router bean
      */
-    @Bean
-    fun indexRouter(@Value("classpath:/static/index.html") html: Resource) = router {
+    @Bean(name = ["indexRouter"])
+    @Profile("!secure")
+    fun indexRouterInsecure(@Value("classpath:/static/index.html") html: Resource) = router {
+        GET("/") {
+            ok().header("Content-Type", "text/html; charset=utf8").bodyValue(html)
+        }
+    }
+
+    /**
+     * @param html requested resource
+     * @return router bean
+     */
+    @Bean(name = ["indexRouter"])
+    @Profile("secure")
+    fun indexRouterSecure(@Value("classpath:/static/index.html") html: Resource) = router {
         GET("/") { request ->
             request.principal().flatMap {
                 // if user is logged in, show `index.html`
