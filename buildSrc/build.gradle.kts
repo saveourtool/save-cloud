ebddc9bf8bcf462a5faaf81b10e3bc44ab0621a4
Peter Trifanov <peter.trifanov@mail.ru>
2021-12-06 12:00:25 +0300

    Use Gradle JVM toolchains
    ### What's done:
    * Use gradle JVM toolchains

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 dependencies {
     // workaround https://github.com/gradle/gradle/issues/15383
     implementation(files(project.libs.javaClass.superclass.protectionDomain.codeSource.location))
-    runtimeOnly(libs.kotlin.gradle.plugin)
+    implementation(libs.kotlin.gradle.plugin)
     implementation(libs.spring.boot.gradle.plugin)
     implementation(libs.diktat.gradle.plugin)
     implementation(libs.detekt.gradle.plugin)
