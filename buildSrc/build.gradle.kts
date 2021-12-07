4b2b1c898ac739e5ac466061fa1d7b8de76131b7
Peter Trifanov <peter.trifanov@mail.ru>
2021-12-07 11:48:41 +0300

    Improvements for Gradle configuration (#450)
    ### What's done:
    * Use gradle JVM toolchains
    * Add gateway to `deploy_images.yaml`
    * Add `RequiresOptIn` for `buildSrc`
    * Enable `useOverride` by default and log a warning
    
    Closes #449

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,10 +10,16 @@ repositories {
 dependencies {
     // workaround https://github.com/gradle/gradle/issues/15383
     implementation(files(project.libs.javaClass.superclass.protectionDomain.codeSource.location))
-    runtimeOnly(libs.kotlin.gradle.plugin)
+    implementation(libs.kotlin.gradle.plugin)
     implementation(libs.spring.boot.gradle.plugin)
     implementation(libs.diktat.gradle.plugin)
     implementation(libs.detekt.gradle.plugin)
     implementation(libs.reckon.gradle.plugin)
     implementation(libs.kotlin.plugin.allopen)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+    }
 }
