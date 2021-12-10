bb12bad32f83b552fb0f73cf6adcbed576603984
Peter Trifanov <peter.trifanov@mail.ru>
2021-12-10 17:34:42 +0300

    Redirect unauthorized requests from FE to login page
    ### What's done:
    * Redirect

--- save-frontend/src/main/kotlin/org/cqfn/save/frontend/utils/RequestUtils.kt
+++ save-frontend/src/main/kotlin/org/cqfn/save/frontend/utils/RequestUtils.kt
@@ -81,7 +81,14 @@ suspend fun request(url: String,
         credentials = credentials,
     )
 )
-    .await()
+    .await().also {
+        if (it.status == 401.toShort()) {
+            // if 401 - change current URL to /login
+            // fixme: this should happen if there is no logged in user on FE
+            // otherwise 401 means that the user has insufficient permissions
+            window.location.href = "${window.location.origin}/login"
+        }
+    }
 
 /**
  * @param name
