edec0c43a8e8e218f72d7912ab368834e0e5f66e
Peter Trifanov <peter.trifanov@mail.ru>
2021-12-10 10:44:15 +0300

    WIP: Enable multithreading in save-agent
    ### What's done:
    * Experiment: start heartbeats in a separate thread

--- save-agent/src/linuxX64Main/kotlin/org/cqfn/save/agent/SaveAgent.kt
+++ save-agent/src/linuxX64Main/kotlin/org/cqfn/save/agent/SaveAgent.kt
@@ -81,7 +81,9 @@ class SaveAgent(internal val config: AgentConfiguration,
      */
     suspend fun start() = coroutineScope {
         logInfoCustom("Starting agent")
-        val heartbeatsJob = launch { startHeartbeats() }
+        val heartbeatsJob = launch(
+            newSingleThreadContext("save-execution")
+        ) { startHeartbeats() }
         heartbeatsJob.join()
     }
 
