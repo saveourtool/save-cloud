29ad14dad3b9d34d8b4a358076db356f8a947059
Peter Trifanov <peter.trifanov@mail.ru>
2021-12-13 12:05:48 +0300

    Update all dependencies to match Kotlin 1.6
    ### What's done:
    * Fixes

--- save-backend/src/test/kotlin/org/cqfn/save/backend/DownloadFilesTest.kt
+++ save-backend/src/test/kotlin/org/cqfn/save/backend/DownloadFilesTest.kt
@@ -61,7 +61,7 @@ import kotlin.io.path.writeLines
     WebConfig::class,
     NoopWebSecurityConfig::class,
     TimestampBasedFileSystemRepository::class,
-    TestDataFilesystemRepository::class
+    TestDataFilesystemRepository::class,
 )
 @AutoConfigureWebTestClient
 @EnableConfigurationProperties(ConfigProperties::class)
