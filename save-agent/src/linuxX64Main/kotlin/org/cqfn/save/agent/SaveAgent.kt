87adbc4cfdd513b436cfad7d7d4957518a2fb39f
Peter Trifanov <peter.trifanov@mail.ru>
2021-12-15 10:46:36 +0300

    Revert Enable multithreading in save-agent
    ### What's done:
    * Remove separate threading

--- save-agent/src/linuxX64Main/kotlin/org/cqfn/save/agent/SaveAgent.kt
+++ save-agent/src/linuxX64Main/kotlin/org/cqfn/save/agent/SaveAgent.kt
@@ -37,7 +37,6 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.datetime.Clock
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
@@ -82,7 +81,7 @@ class SaveAgent(internal val config: AgentConfiguration,
     @Suppress("WHEN_WITHOUT_ELSE")  // when with sealed class
     private suspend fun startHeartbeats() = coroutineScope {
         logInfoCustom("Scheduling heartbeats")
-        launch(newSingleThreadContext("background")) {
+        launch {
             sendDataToBackend { saveAdditionalData() }
         }
         while (true) {
@@ -112,7 +111,7 @@ class SaveAgent(internal val config: AgentConfiguration,
         if (saveProcessJob.value?.isCompleted == false) {
             logErrorCustom("Shouldn't start new process when there is the previous running")
         } else {
-            saveProcessJob.value = launch(newSingleThreadContext("save-process")) {
+            saveProcessJob.value = launch {
                 runCatching {
                     // new job received from Orchestrator, spawning SAVE CLI process
                     startSaveProcess(cliArgs)
