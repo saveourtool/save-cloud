bddf64cb3fc4f15e2362aad7aa41d6445509bbb6
Peter Trifanov <peter.trifanov@mail.ru>
2021-12-23 16:50:46 +0300

    [skip ci] Security
    ### What's done:
    * Experiment with rules on GW

--- api-gateway/src/main/kotlin/org/cqfn/save/gateway/security/WebSecurityConfig.kt
+++ api-gateway/src/main/kotlin/org/cqfn/save/gateway/security/WebSecurityConfig.kt
@@ -8,16 +8,25 @@ import org.cqfn.save.gateway.config.ConfigurationProperties
 import org.cqfn.save.gateway.utils.StoringServerAuthenticationSuccessHandler
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager
+import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.DelegatingServerAuthenticationSuccessHandler
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler
 import org.springframework.security.web.server.authentication.logout.HttpStatusReturningServerLogoutSuccessHandler
+import reactor.core.publisher.Mono
 
 @EnableWebFluxSecurity
 @Suppress("MISSING_KDOC_TOP_LEVEL", "MISSING_KDOC_CLASS_ELEMENTS", "MISSING_KDOC_ON_FUNCTION")
@@ -32,9 +41,18 @@ class WebSecurityConfig(
             // this is default data that is required by FE to operate properly
             .pathMatchers("/", "/login", "/logout", "/sec/oauth-providers")
             .permitAll()
-            // some endpoints from backend allow anonymous access; todo: should be managed only by backend
-            .pathMatchers("/api/projects/not-deleted")
-            .permitAll()
+            // all requests to backend are permitted on gateway, if user agent is authenticated in gateway or doesn't have
+            // any authentication data at all.
+            // backend returns 401 for those endpoints that require authentication
+            .pathMatchers("/api/**")
+            .access { authentication, authorizationContext ->
+                authentication.map {
+                    AuthorizationDecision(
+                        it.isAuthenticated ||
+                            authorizationContext.exchange.request.headers[HttpHeaders.AUTHORIZATION].isNullOrEmpty()
+                    )
+                }
+            }
             // resources for frontend
             .pathMatchers("/*.html", "/*.js*", "img/**", "*.gif", "*.svg")
             .permitAll()
