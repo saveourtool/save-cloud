b2ce4927be314e807677b190a08246280d42bfde
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2022-01-02 22:56:40 +0300

    Small updates to be able to build save-cloud on MAC

--- save-frontend/build.gradle.kts
+++ save-frontend/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
-    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "17.3.0"
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.13.1"
 }
 
 dependencies {
