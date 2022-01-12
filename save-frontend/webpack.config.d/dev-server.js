8e01bba7db5d2522a0d8f813fcb4fd83fc42876f
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-01-12 17:09:43 +0300

    Experiments ### What's done: * Experiments

--- save-frontend/webpack.config.d/dev-server.js
+++ save-frontend/webpack.config.d/dev-server.js
@@ -6,6 +6,18 @@ config.devServer = Object.assign(
         {
           context: ["/api/**"],
           target: 'http://localhost:5000',
+            logProvider: function logProvider(provider) {
+                // replace the default console log provider.
+                return require('winston');
+            },
+            logLevel: 'debug',
+            onProxyReq: function (proxyReq, req, res) {
+                const winston = require('winston');
+                const logger = winston.createLogger();
+              logger.debug(JSON.stringify(req.headers));
+              logger.debug(JSON.stringify(proxyReq.headers));
+              proxyReq.setHeader("Authorization", "Basic YWRtaW46");
+            }
         }
       ]
     }
