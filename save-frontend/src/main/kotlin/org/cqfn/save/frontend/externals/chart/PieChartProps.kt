39a05f12143fcaf26c76e99501afb59832b32279
Vfrolov <vlad-20-11-94@yandex.ru>
2022-01-17 16:25:44 +0300

    Pie Chart. Support the logic for aggregation of test results by test suite
    What's done:
    * added pie chart
    Closes #475

--- save-frontend/src/main/kotlin/org/cqfn/save/frontend/externals/chart/PieChartProps.kt
+++ save-frontend/src/main/kotlin/org/cqfn/save/frontend/externals/chart/PieChartProps.kt
@@ -2,6 +2,7 @@
 
 package org.cqfn.save.frontend.externals.chart
 
+import org.w3c.dom.events.Event
 import react.PropsWithChildren
 
 /**
@@ -12,6 +13,41 @@ external interface PieChartProps : PropsWithChildren {
      * Dataset for pie chart
      */
     var data: Array<DataPieChart>
+
+    /**
+     * Animate segments on component mount
+     */
+    var animate: Boolean
+
+    /**
+     * Translates segments radially. If number set, provide shift value relative to viewBoxSize space
+     */
+    var segmentsShift: Int
+
+    /**
+     * width and height of SVG viewBox attribute
+     */
+    var viewBoxSize: IntArray
+
+    /**
+     * Radius of the pie (relative to viewBoxSize space)
+     */
+    var radius: Int
+
+    /**
+     * onClick event handler for each segment
+     */
+    var onClick: (Event, Int) -> Unit
+
+    /**
+     * onMouseOut event handler for each segment
+     */
+    var onMouseOut: (Event, Int) -> Unit
+
+    /**
+     * onMouseOver event handler for each segment
+     */
+    var onMouseOver: (Event, Int) -> Unit
 }
 
 /**
@@ -20,9 +56,11 @@ external interface PieChartProps : PropsWithChildren {
  * @property title segment name
  * @property value value of segment
  * @property color color of segment
+ * @property key custom value to be used as segments element keys
  */
 data class DataPieChart(
-    val title: String,
+    val title: String? = null,
     val value: Int,
-    val color: String,
+    var color: String,
+    val key: String? = null,
 )
