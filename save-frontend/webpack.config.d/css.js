d0192336e663d4f192b72016e2179de15a2c7e8b
Peter Trifanov <peter.trifanov@mail.ru>
2022-01-17 11:46:03 +0300

    Update postcss and postcss-loader (#530)

--- save-frontend/webpack.config.d/css.js
+++ save-frontend/webpack.config.d/css.js
@@ -7,13 +7,12 @@ config.module.rules.push(
             {
                 loader: 'postcss-loader', // Run postcss actions
                 options: {
-                    // postcss plugins, can be exported to postcss.config.js
-                    plugins: function () {
-                        return [
-                            require('autoprefixer')
-                        ];
-                    }
-                }
+                    postcssOptions: {
+                        plugins: [
+                            "autoprefixer",
+                        ],
+                    },
+                },
             },
             'sass-loader', // compiles Sass to CSS, using Node Sass by default
         ]
