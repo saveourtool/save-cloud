8c600cff044c756c053a53b21756a80038205be6
Peter Trifanov <peter.trifanov@mail.ru>
2022-03-16 18:49:56 +0300

    [skip ci] Improvements for ProjectPermissionEvaluator
    * WIP: Mores tests

--- save-backend/src/test/kotlin/org/cqfn/save/backend/security/ProjectPermissionEvaluatorTest.kt
+++ save-backend/src/test/kotlin/org/cqfn/save/backend/security/ProjectPermissionEvaluatorTest.kt
@@ -4,8 +4,11 @@ import org.cqfn.save.backend.repository.LnkUserProjectRepository
 import org.cqfn.save.backend.service.LnkUserProjectService
 import org.cqfn.save.backend.utils.AuthenticationDetails
 import org.cqfn.save.domain.Role
+import org.cqfn.save.entities.LnkUserProject
 import org.cqfn.save.entities.Project
+import org.cqfn.save.entities.User
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
@@ -16,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
@@ -24,15 +28,72 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 class ProjectPermissionEvaluatorTest {
     @Autowired private lateinit var projectPermissionEvaluator: ProjectPermissionEvaluator
     @MockBean private lateinit var lnkUserProjectRepository: LnkUserProjectRepository
-    private var mockProject = Project.stub(99)
+    private lateinit var mockProject: Project
+
+    @BeforeEach
+    fun setUp() {
+        mockProject = Project.stub(99)
+    }
 
     @Test
-    fun `should allow any operations for SUPER_ADMIN`() {
-        val auth = mockAuth("admin", Role.SUPER_ADMIN.asSpringSecurityRole())
-        whenever(lnkUserProjectRepository.findByUserIdAndProject(any(), any())).thenReturn(emptyList())
-        Assertions.assertTrue(projectPermissionEvaluator.hasPermission(auth, mockProject, Permission.READ))
-        Assertions.assertTrue(projectPermissionEvaluator.hasPermission(auth, mockProject, Permission.WRITE))
-        Assertions.assertTrue(projectPermissionEvaluator.hasPermission(auth, mockProject, Permission.DELETE))
+    fun `default permissions for users with only global roles`() {
+        userShouldHavePermissions(
+            "super_admin", Role.SUPER_ADMIN, emptyList(), *Permission.values()
+        )
+        userShouldHavePermissions(
+            "admin", Role.ADMIN, emptyList(), Permission.READ
+        )
+        userShouldHavePermissions(
+            "owner", Role.OWNER, emptyList(), Permission.READ
+        )
+        userShouldHavePermissions(
+            "viewer", Role.VIEWER, emptyList(), Permission.READ
+        )
+    }
+
+    @Test
+    fun `default permissions for users with only global roles for private projects`() {
+        mockProject.public = false
+        userShouldHavePermissions(
+            "super_admin", Role.SUPER_ADMIN, emptyList(), *Permission.values()
+        )
+        userShouldHavePermissions(
+            "admin", Role.ADMIN, emptyList()
+        )
+        userShouldHavePermissions(
+            "owner", Role.OWNER, emptyList()
+        )
+        userShouldHavePermissions(
+            "viewer", Role.VIEWER, emptyList()
+        )
+    }
+
+    private fun userShouldHavePermissions(
+        username: String,
+        role: Role,
+        projectRoles: List<Role>,
+        vararg permissions: Permission,
+    ) {
+        val authentication = mockAuth(username, role.asSpringSecurityRole())
+        whenever(lnkUserProjectRepository.findByUserIdAndProject(any(), any())).thenAnswer { invocation ->
+            projectRoles.map { role ->
+                LnkUserProject(
+                    invocation.arguments[1] as Project,
+                    mockUser((invocation.arguments[0] as Number).toLong()),
+                    role,
+                )
+            }
+        }
+        permissions.forEach {
+            Assertions.assertTrue(projectPermissionEvaluator.hasPermission(authentication, mockProject, it)) {
+                "User by authentication=$authentication is expected to have permission $it on project $mockProject"
+            }
+        }
+        Permission.values().filterNot { it in permissions }.forEach {
+            Assertions.assertFalse(projectPermissionEvaluator.hasPermission(authentication, mockProject, it)) {
+                "User by authentication=$authentication isn't expected to have permission $it on project $mockProject"
+            }
+        }
     }
 
     private fun mockAuth(principal: String, vararg roles: String, id: Long = 99) = UsernamePasswordAuthenticationToken(
@@ -42,4 +103,6 @@ class ProjectPermissionEvaluatorTest {
     ).apply {
         details = AuthenticationDetails(id = id, identitySource = "")
     }
+
+    private fun mockUser(id: Long) = User(null, null, null, "").apply { this.id = id }
 }
