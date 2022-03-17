b09c680929389b53e93c772c5515948754796b8b
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-03-17 15:38:56 +0300

    WIP ### What's done: * WIP

--- save-api/src/main/kotlin/org/cqfn/save/api/AutomaticTestInitializator.kt
+++ save-api/src/main/kotlin/org/cqfn/save/api/AutomaticTestInitializator.kt
@@ -1,9 +1,27 @@
 package org.cqfn.save.api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.util.*
+
+// TODO move into properties file
+private val backendUrl = "http://localhost:5000/internal"
+private val preprocessorUrl = "http://localhost:5200"
+
 
 class AutomaticTestInitializator {
-    fun start() {
-        val client = HttpClient()
+    @OptIn(InternalAPI::class)
+    suspend fun start() {
+        val httpClient = HttpClient()
+
+        httpClient.submitForm<HttpResponse> (
+            url = "${backendUrl}/submitExecutionRequest",
+            formParameters = Parameters.build {
+                append("first_name", "John")
+                append("last_name", "Doe")
+            }
+        )
     }
 }
