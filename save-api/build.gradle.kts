f71c392f8cd9477f0c32371d0872fdbdb684e707
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-03-18 13:52:05 +0300

    WIP ### What's done: * .

--- save-api/build.gradle.kts
+++ save-api/build.gradle.kts
@@ -2,8 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
-    //kotlin("multiplatform")
-    //alias(libs.plugins.kotlin.plugin.serialization)
 }
 
 tasks.withType<KotlinCompile> {
@@ -13,39 +11,13 @@ tasks.withType<KotlinCompile> {
     }
 }
 
+
 dependencies {
     implementation(projects.saveCloudCommon)
-    // implementation(file("$rootDir/save-cloud-common/build/libs/save-cloud-common-linuxX64-$version.jar"))
+    implementation(libs.save.common.jvm)
     implementation(libs.slf4j.api)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.apache)
     implementation(libs.ktor.client.serialization)
+    implementation(libs.kotlinx.serialization.properties)
 }
-
-
-//kotlin {
-//    // Create a target for the host platform.
-//    val hostTarget = linuxX64 {
-//        binaries.executable {
-//            entryPoint = "org.cqfn.save.api.main"
-//            baseName = "save-api"
-//        }
-//    }
-//
-//    sourceSets {
-//        all {
-//            languageSettings.optIn("kotlin.RequiresOptIn")
-//            languageSettings.optIn("kotlinx.serialization.ExperimentalSerializationApi")
-//        }
-//        val linuxX64Main by getting {
-//            dependencies {
-//                implementation(projects.saveCloudCommon)
-////                implementation(file("$rootDir/save-cloud-common/build/libs/save-cloud-common-linuxX64-$version.jar"))
-//                implementation(libs.slf4j.api)
-//                implementation(libs.ktor.client.core)
-//                implementation(libs.ktor.client.curl)
-//                implementation(libs.ktor.client.serialization)
-//            }
-//        }
-//    }
-//}
--- save-api/src/main/kotlin/org/cqfn/save/api/AutomaticTestInitializator.kt
+++ save-api/src/main/kotlin/org/cqfn/save/api/AutomaticTestInitializator.kt
@@ -1,56 +1,74 @@
 package org.cqfn.save.api
 
 import io.ktor.client.HttpClient
-import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.util.*
-//import org.cqfn.save.core.result.DebugInfo
-//import org.cqfn.save.core.result.Pass
-//import org.cqfn.save.domain.TestResultDebugInfo
-//import org.cqfn.save.domain.TestResultLocation
+import io.ktor.client.engine.apache.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import org.cqfn.save.core.result.DebugInfo
+import org.cqfn.save.core.result.Pass
+import org.cqfn.save.domain.TestResultDebugInfo
+import org.cqfn.save.domain.TestResultLocation
 
 // TODO move into properties file
-private val backendUrl = "http://localhost:5000/internal"
-private val preprocessorUrl = "http://localhost:5200"
+private const val BACKEND_URL = "http://localhost:5000/internal"
+private const val PREPROCESSOR_URL = "http://localhost:5200"
 
+
+internal val json = Json {
+    serializersModule = SerializersModule {
+        polymorphic(TestResultDebugInfo::class)
+        polymorphic(DebugInfo::class)
+        polymorphic(Pass::class)
+    }
+}
 
 class AutomaticTestInitializator {
     @OptIn(InternalAPI::class)
     suspend fun start() {
-        val httpClient = HttpClient()
+        val httpClient = HttpClient(Apache) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(json)
+            }
+        }
 
         println("-------------------Start post debug info---------------------")
 
-//        val testResultDebugInfo  = TestResultDebugInfo(
-//            TestResultLocation(
-//                "stub",
-//                "stub",
-//                "stub",
-//                "stub",
-//            ),
-//            DebugInfo(
-//                "stub",
-//                "stub",
-//                "stub",
-//                42
-//            ),
-//            Pass(
-//               "ok",
-//               "ok",
-//            )
-//        )
-//        httpClient.post<HttpResponse> {
-//            url("${backendUrl}/internal/files/debug-info?agentId=test-agent-id")
-//            contentType(ContentType.Application.Json)
-//            body = testResultDebugInfo
-//        }
+        val testResultDebugInfo  = TestResultDebugInfo(
+            TestResultLocation(
+                "stub",
+                "stub",
+                "stub",
+                "stub",
+            ),
+            DebugInfo(
+                "stub",
+                "stub",
+                "stub",
+                42
+            ),
+            Pass(
+               "ok",
+               "ok",
+            )
+        )
+        httpClient.post<HttpResponse> {
+            url("${BACKEND_URL}/internal/files/debug-info?agentId=test-agent-id")
+            contentType(ContentType.Application.Json)
+            body = testResultDebugInfo
+        }
 
         println("-------------------Start execution---------------------")
 
         httpClient.submitForm<HttpResponse> (
-            url = "${backendUrl}/submitExecutionRequest",
+            url = "${BACKEND_URL}/submitExecutionRequest",
             formParameters = Parameters.build {
                 append("first_name", "John")
                 append("last_name", "Doe")
