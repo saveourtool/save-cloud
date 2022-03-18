64df97aee8dabd1825333cc39641e3e79aaa2055
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-03-18 11:09:36 +0300

    Move to JVM ### What's done: * .

--- save-api/build.gradle.kts
+++ save-api/build.gradle.kts
@@ -1,31 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("multiplatform")
+    kotlin("jvm")
+    //kotlin("multiplatform")
     //alias(libs.plugins.kotlin.plugin.serialization)
 }
 
-kotlin {
-    // Create a target for the host platform.
-    val hostTarget = linuxX64 {
-        binaries.executable {
-            entryPoint = "org.cqfn.save.api.main"
-            baseName = "save-api"
-        }
-    }
-
-    sourceSets {
-        all {
-            languageSettings.optIn("kotlin.RequiresOptIn")
-            languageSettings.optIn("kotlinx.serialization.ExperimentalSerializationApi")
-        }
-        val linuxX64Main by getting {
-            dependencies {
-                implementation(projects.saveCloudCommon)
-//                implementation(file("$rootDir/save-cloud-common/build/libs/save-cloud-common-linuxX64-$version.jar"))
-                implementation(libs.slf4j.api)
-                implementation(libs.ktor.client.core)
-                implementation(libs.ktor.client.curl)
-                implementation(libs.ktor.client.serialization)
-            }
-        }
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = Versions.jdk
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
     }
 }
+
+dependencies {
+    implementation(projects.saveCloudCommon)
+    // implementation(file("$rootDir/save-cloud-common/build/libs/save-cloud-common-linuxX64-$version.jar"))
+    implementation(libs.slf4j.api)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.apache)
+    implementation(libs.ktor.client.serialization)
+}
+
+
+//kotlin {
+//    // Create a target for the host platform.
+//    val hostTarget = linuxX64 {
+//        binaries.executable {
+//            entryPoint = "org.cqfn.save.api.main"
+//            baseName = "save-api"
+//        }
+//    }
+//
+//    sourceSets {
+//        all {
+//            languageSettings.optIn("kotlin.RequiresOptIn")
+//            languageSettings.optIn("kotlinx.serialization.ExperimentalSerializationApi")
+//        }
+//        val linuxX64Main by getting {
+//            dependencies {
+//                implementation(projects.saveCloudCommon)
+////                implementation(file("$rootDir/save-cloud-common/build/libs/save-cloud-common-linuxX64-$version.jar"))
+//                implementation(libs.slf4j.api)
+//                implementation(libs.ktor.client.core)
+//                implementation(libs.ktor.client.curl)
+//                implementation(libs.ktor.client.serialization)
+//            }
+//        }
+//    }
+//}
