e0bccbf9dde31f92102d9b5963e43cdeca03bd88
sanyavertolet <lxnfrolov@gmail.com>
2022-04-13 12:21:34 +0300

    Managing people in organizations
    ### What's done:
    * Added column `role` to `lnk_user_organization` table
    
    (#)

---
+++ save-backend/src/main/kotlin/org/cqfn/save/backend/service/LnkUserOrganizationService.kt
@@ -0,0 +1,49 @@
+package org.cqfn.save.backend.service
+
+import org.cqfn.save.backend.repository.LnkUserOrganizationRepository
+import org.cqfn.save.backend.repository.LnkUserProjectRepository
+import org.cqfn.save.domain.Role
+import org.cqfn.save.entities.LnkUserProject
+import org.cqfn.save.entities.Organization
+import org.cqfn.save.entities.Project
+import org.cqfn.save.entities.User
+import org.springframework.stereotype.Service
+
+/**
+ * Service of lnkUserProjects
+ */
+@Service
+class LnkUserOrganizationService(private val lnkUserOrganizationRepository: LnkUserOrganizationRepository) {
+    /**
+     * @param project
+     * @return all users with role in project
+     */
+    fun getAllUsersAndRolesByOrganization(organization: Organization) =
+            lnkUserOrganizationRepository.findByOrganization(organization).associate { it.user to (it.role ?: Role.NONE) }
+
+    /**
+     * @param userId
+     * @param project
+     * @return role for user in [project] by user ID
+     */
+    fun findRoleByUserIdAndProject(userId: Long, project: Project) = lnkUserProjectRepository
+        .findByUserIdAndProject(userId, project)
+        ?.role
+        ?: Role.NONE
+
+    /**
+     * Set role of [user] on a project [project] to [role]
+     *
+     * @throws IllegalStateException if [role] is [Role.NONE]
+     */
+    @Suppress("KDOC_WITHOUT_PARAM_TAG", "UnsafeCallOnNullableType")
+    fun setRole(user: User, project: Project, role: Role) {
+        if (role == Role.NONE) {
+            throw IllegalStateException("Role NONE should not be present in database!")
+        }
+        val lnkUserProject = lnkUserProjectRepository.findByUserIdAndProject(user.id!!, project)
+            ?.apply { this.role = role }
+            ?: LnkUserProject(project, user, role)
+        lnkUserProjectRepository.save(lnkUserProject)
+    }
+}
