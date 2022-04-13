f0773f8632acc54ac9ceeb1a142ba397e7716830
Peter Trifanov <peter.trifanov@mail.ru>
2022-04-13 17:40:23 +0300

    Update diktat to 1.1.0
    * Fix diktat findings

--- buildSrc/src/main/kotlin/org/cqfn/save/buildutils/DiktatConfiguration.kt
+++ buildSrc/src/main/kotlin/org/cqfn/save/buildutils/DiktatConfiguration.kt
@@ -40,6 +40,7 @@ private fun Project.fixDiktatTasks() {
         javaLauncher.set(project.extensions.getByType<JavaToolchainService>().launcherFor {
             languageVersion.set(JavaLanguageVersion.of(Versions.jdk))
         })
+        // https://github.com/analysis-dev/diktat/issues/1269
         systemProperty("user.home", rootDir.toString())
     }
 }
