67b8654c5c6b37d88714582f205eb878937cc718
Peter Trifanov <peter.trifanov@mail.ru>
2022-04-14 13:14:26 +0300

    * Remove unused url-loader from webpack * Add mocking of `/sec/**` endpoints in webpack-dev-server * Fix versions of development dependencies

--- save-frontend/webpack.config.d/dev-server.js
+++ save-frontend/webpack.config.d/dev-server.js
@@ -11,6 +11,18 @@ config.devServer = Object.assign(
             proxyReq.setHeader("Authorization", "Basic YWRtaW46");
             proxyReq.setHeader("X-Authorization-Source", "basic");
           }
+        },
+        {
+          bypass: (req, res) => {
+            if (req.url.endsWith("/sec/user")) {
+              return res.send({
+                // mocked UserInfo object
+                name: "admin"
+              });
+            } else if (req.url.endsWith("/sec/oauth-providers")) {
+              return res.send([])
+            }
+          }
         }
       ]
     }
