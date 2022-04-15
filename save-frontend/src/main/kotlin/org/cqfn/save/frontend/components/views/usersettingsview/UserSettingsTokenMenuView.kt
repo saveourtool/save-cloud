023afd3ab73f6bbdcb3c0a4fa10724338cfd2cff
Vladislav Frolov <50615459+Cheshiriks@users.noreply.github.com>
2022-04-15 17:26:37 +0300

    Create settings view for users (#685)
    * Create settings view for users
    
    What's done:
    * Created settings view for users
    Closes #646

---
+++ save-frontend/src/main/kotlin/org/cqfn/save/frontend/components/views/usersettingsview/UserSettingsTokenMenuView.kt
@@ -0,0 +1,58 @@
+package org.cqfn.save.frontend.components.views.usersettingsview
+
+import org.cqfn.save.frontend.components.basic.cardComponent
+
+import react.FC
+import react.dom.*
+import react.fc
+import react.setState
+
+import kotlinx.html.ButtonType
+import kotlinx.html.js.onClickFunction
+
+@Suppress("MISSING_KDOC_TOP_LEVEL")
+class UserSettingsTokenMenuView : UserSettingsView() {
+    override fun renderMenu(): FC<UserSettingsProps> = fc { props ->
+        child(cardComponent(isBordered = false, hasBg = true) {
+            div("d-sm-flex align-items-center justify-content-center mb-4") {
+                h1("h3 mb-0 mt-2 text-gray-800") {
+                    +"Personal access tokens"
+                }
+            }
+
+            div("row justify-content-center") {
+                button(type = ButtonType.button, classes = "btn btn-primary mb-2 mr-2") {
+                    +"Generate new token"
+                    attrs.onClickFunction = {
+                        generateToken()
+                    }
+                }
+            }
+
+            state.token?.let {
+                div("col-md-12 mt-3") {
+                    input {
+                        attrs.value = state.token ?: ""
+                        attrs["required"] = true
+                        attrs["class"] = "form-control"
+                    }
+                    div("invalid-feedback d-block") {
+                        +"This is your unique token. It will be shown to you only once. Please remember it."
+                    }
+                }
+            }
+        })
+    }
+
+    @Suppress("MAGIC_NUMBER")
+    private fun generateToken() {
+        var token = "ghp_"
+        val charPool = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        while (token.length < 40) {
+            token += charPool.random()
+        }
+        setState {
+            this.token = token
+        }
+    }
+}
