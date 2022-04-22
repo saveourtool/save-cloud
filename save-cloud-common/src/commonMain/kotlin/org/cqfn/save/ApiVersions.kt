55c56263206b4f2ca971d712b9b25ebb189d347a
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-04-22 13:00:40 +0300

    detekt ### What's done: * .

--- save-cloud-common/src/commonMain/kotlin/org/cqfn/save/ApiVersions.kt
+++ save-cloud-common/src/commonMain/kotlin/org/cqfn/save/ApiVersions.kt
@@ -2,7 +2,7 @@
  * File, which contain the list of versions of backend API
  */
 
-@file:Suppress("VARIABLE_NAME_INCORRECT", "CONSTANT_UPPERCASE")
+@file:Suppress("VARIABLE_NAME_INCORRECT", "CONSTANT_UPPERCASE", "TopLevelPropertyNaming")
 
 package org.cqfn.save
 
