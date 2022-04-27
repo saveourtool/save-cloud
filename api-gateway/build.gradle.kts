14367e01b3236a95dd3475aed732415f6c5124ab
Peter Trifanov <peter.trifanov@mail.ru>
2022-04-27 16:12:19 +0300

    Integrate spring-cloud-kubernetes (#707)
    * Gateway: read OAuth properties from secrets in `kubernetes` profile
    * Backend: move postprocessor into a separate profile, add kubernetes support for database credentials
    * Support setting host IP for orchestrator via env variable `HOST_IP`

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation(libs.spring.cloud.starter.gateway)
     implementation(libs.spring.boot.starter.security)
     implementation(libs.spring.boot.starter.oauth2.client)
+    implementation(libs.spring.cloud.starter.kubernetes.client.config)
     implementation(libs.spring.security.core)
 }
 
