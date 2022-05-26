e532e849ea778e3d6fbd42e5e513b56399d9fddf
Peter Trifanov <peter.trifanov@mail.ru>
2022-05-26 18:15:32 +0300

    Import individual font-awesome icons to reduce bundle size

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/externals/fontawesome/Icons.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/externals/fontawesome/Icons.kt
@@ -2,41 +2,133 @@
  * External declarations of icons from fontawesome-solid
  */
 
-@file:JsModule("@fortawesome/free-solid-svg-icons")
-@file:JsNonModule
-
 package com.saveourtool.save.frontend.externals.fontawesome
 
-external val fas: dynamic
+@JsModule("@fortawesome/free-solid-svg-icons/faUser")
+@JsNonModule
 external val faUser: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faCogs")
+@JsNonModule
 external val faCogs: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faSignOutAlt")
+@JsNonModule
 external val faSignOutAlt: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faAngleUp")
+@JsNonModule
 external val faAngleUp: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faCheck")
+@JsNonModule
 external val faCheck: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faExclamationTriangle")
+@JsNonModule
 external val faExclamationTriangle: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faTimesCircle")
+@JsNonModule
 external val faTimesCircle: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faQuestionCircle")
+@JsNonModule
 external val faQuestionCircle: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faSpinner")
+@JsNonModule
 external val faSpinner: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faUpload")
+@JsNonModule
 external val faUpload: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faFile")
+@JsNonModule
 external val faFile: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faHistory")
+@JsNonModule
 external val faHistory: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faCalendarAlt")
+@JsNonModule
 external val faCalendarAlt: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faExternalLinkAlt")
+@JsNonModule
 external val faExternalLinkAlt: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faTrashAlt")
+@JsNonModule
 external val faTrashAlt: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faEdit")
+@JsNonModule
 external val faEdit: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faFilter")
+@JsNonModule
 external val faFilter: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faSearch")
+@JsNonModule
 external val faSearch: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faRedo")
+@JsNonModule
 external val faRedo: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faCopyright")
+@JsNonModule
 external val faCopyright: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faSignInAlt")
+@JsNonModule
 external val faSignInAlt: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faCopy")
+@JsNonModule
 external val faCopy: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faFolderOpen")
+@JsNonModule
 external val faFolderOpen: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faCheckCircle")
+@JsNonModule
 external val faCheckCircle: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faPlus")
+@JsNonModule
 external val faPlus: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faArrowRight")
+@JsNonModule
 external val faArrowRight: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faLightbulb")
+@JsNonModule
 external val faLightbulb: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faCog")
+@JsNonModule
 external val faCog: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faHome")
+@JsNonModule
 external val faHome: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faEnvelope")
+@JsNonModule
 external val faEnvelope: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faKey")
+@JsNonModule
 external val faKey: dynamic
+
+@JsModule("@fortawesome/free-solid-svg-icons/faCity")
+@JsNonModule
 external val faCity: dynamic
+
