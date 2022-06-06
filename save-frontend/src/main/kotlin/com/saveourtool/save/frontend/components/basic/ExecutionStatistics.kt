3d00924e670ff80aadbfcc49845dfd67ee5976f4
Nariman Abdullin <nulls.narik@gmail.com>
2022-06-06 10:45:33 +0300

    save-cloud: Precision and Recall
    ### What's done:
    - added tests for coverage

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/ExecutionStatistics.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/ExecutionStatistics.kt
@@ -40,30 +40,7 @@ external interface ExecutionStatisticsProps : Props {
     "ComplexMethod"
 )
 fun executionStatistics(classes: String = "") = fc<ExecutionStatisticsProps> { props ->
-    val isInProgress = props.executionDto?.run { status == ExecutionStatus.RUNNING || status == ExecutionStatus.PENDING } ?: true
-    val isSuccess = props.executionDto?.run { passedTests == allTests } ?: false
-    val style = if (isInProgress) {
-        "info"
-    } else if (isSuccess) {
-        "success"
-    } else {
-        "danger"
-    }
-
-    val allTests = props.executionDto?.allTests?.toString() ?: "0"
-    val passedTests = props.executionDto?.passedTests?.toString() ?: "0"
-    val failedTests = props.executionDto?.failedTests?.toString() ?: "0"
-    val runningTests = props.executionDto?.runningTests?.toString() ?: "0"
-
-    val passRate = props.executionDto
-        ?.let { calculateRate(it.passedTests, it.allTests) }
-        ?: "0"
-    val precisionRate = props.executionDto
-        ?.let { calculateRate(it.matchedChecks, it.matchedChecks + it.unmatchedChecks) }
-        ?: "0"
-    val recallRate = props.executionDto
-        ?.let { calculateRate(it.matchedChecks, it.expectedChecks) }
-        ?: "0"
+    val values = ExecutionStatisticsValues(props.executionDto)
 
     div("col-xl-3 col-md-6 mb-4") {
         div("card border-left-info shadow h-100 py-2") {
@@ -73,16 +50,16 @@ fun executionStatistics(classes: String = "") = fc<ExecutionStatisticsProps> { p
                         div("text-xs font-weight-bold text-info text-uppercase mb-1") { +"Pass Rate" }
                         div("row no-gutters align-items-center") {
                             div("col-auto") {
-                                div("h5 mb-0 mr-3 font-weight-bold text-gray-800") { +"$passRate%" }
+                                div("h5 mb-0 mr-3 font-weight-bold text-gray-800") { +"${values.passRate}%" }
                             }
                             div("col") {
                                 div("progress progress-sm mr-2") {
                                     div("progress-bar bg-info") {
                                         attrs["role"] = "progressbar"
                                         attrs["style"] = jso<CSSProperties> {
-                                            width = "$passRate%".unsafeCast<Width>()
+                                            width = "${values.passRate}%".unsafeCast<Width>()
                                         }
-                                        attrs["aria-valuenow"] = passRate
+                                        attrs["aria-valuenow"] = values.passRate
                                         attrs["aria-valuemin"] = "0"
                                         attrs["aria-valuemax"] = "100"
                                     }
@@ -100,38 +77,39 @@ fun executionStatistics(classes: String = "") = fc<ExecutionStatisticsProps> { p
     }
 
     div("col-xl-4 col-md-6 mb-4") {
-        div("card border-left-$style shadow h-100 py-2") {
+        div("card border-left-${values.style} shadow h-100 py-2") {
             div("card-body") {
                 div("row no-gutters align-items-center") {
                     div("col mr-2") {
                         div("text-xs font-weight-bold text-info text-uppercase mb-1") { +"Tests" }
-                        div("h5 mb-0 font-weight-bold text-gray-800") { +allTests }
+                        div("h5 mb-0 font-weight-bold text-gray-800") { +values.allTests }
                     }
                     div("col mr-2") {
                         div("text-xs font-weight-bold text-info text-uppercase mb-1") { +"Running" }
-                        div("h5 mb-0 font-weight-bold text-gray-800") { +runningTests }
+                        div("h5 mb-0 font-weight-bold text-gray-800") { +values.runningTests }
                     }
                     div("col mr-2") {
                         div("text-xs font-weight-bold text-danger text-uppercase mb-1") { +"Failed" }
-                        div("h5 mb-0 font-weight-bold text-gray-800") { +failedTests }
+                        div("h5 mb-0 font-weight-bold text-gray-800") { +values.failedTests }
                     }
                     div("col mr-2") {
                         div("text-xs font-weight-bold text-success text-uppercase mb-1") { +"Passed" }
-                        div("h5 mb-0 font-weight-bold text-gray-800") { +passedTests }
+                        div("h5 mb-0 font-weight-bold text-gray-800") { +values.passedTests }
                     }
                     div("col mr-2") {
                         div("text-xs font-weight-bold text-info text-uppercase mb-1") { +"Precision" }
-                        div("h5 mb-0 font-weight-bold text-gray-800") { +precisionRate }
+                        div("h5 mb-0 font-weight-bold text-gray-800") { +values.precisionRate }
                     }
                     div("col mr-2") {
                         div("text-xs font-weight-bold text-info text-uppercase mb-1") { +"Recall" }
-                        div("h5 mb-0 font-weight-bold text-gray-800") { +recallRate }
+                        div("h5 mb-0 font-weight-bold text-gray-800") { +values.recallRate }
                     }
                 }
             }
         }
     }
 }
+
 
 /**
  * A component which displays a GIF if tests not found
@@ -159,8 +137,82 @@ fun executionTestsNotFound() = fc<ExecutionStatisticsProps> { props ->
     }
 }
 
-private fun calculateRate(numerator: Long, denominator: Long) = denominator.takeIf { it > 0 }
-    ?.run { numerator.toDouble() / denominator }
-    ?.let { it * 100 }
-    ?.toInt()
-    ?.toString()
+/**
+ * Class contains all execution statistics values for rending
+ *
+ * @param executionDto
+ */
+internal class ExecutionStatisticsValues(executionDto: ExecutionDto?) {
+    /**
+     * The style for rending
+     */
+    val style: String
+
+    /**
+     * All tests in the current execution or 0
+     */
+    val allTests: String
+
+    /**
+     * Number of passed tests in the current execution or 0
+     */
+    val passedTests: String
+
+    /**
+     * Number of failed tests in the current execution or 0
+     */
+    val failedTests: String
+
+    /**
+     * Number of running tests in the current execution or 0
+     */
+    val runningTests: String
+
+
+    /**
+     * Rate of passed tests in the current execution or 0
+     */
+    val passRate: String
+
+    /**
+     * Precision rate in the current execution or 0
+     */
+    val precisionRate: String
+
+    /**
+     * Recall rate in the current execution or 0
+     */
+    val recallRate: String
+
+    init {
+        val isInProgress =
+            executionDto?.run { status == ExecutionStatus.RUNNING || status == ExecutionStatus.PENDING } ?: true
+        val isSuccess = executionDto?.run { passedTests == allTests } ?: false
+        this.style = if (isInProgress) {
+            "info"
+        } else if (isSuccess) {
+            "success"
+        } else {
+            "danger"
+        }
+        this.allTests = executionDto?.allTests?.toString() ?: "0"
+        this.passedTests = executionDto?.passedTests?.toString() ?: "0"
+        this.failedTests = executionDto?.failedTests?.toString() ?: "0"
+        this.runningTests = executionDto?.runningTests?.toString() ?: "0"
+        this.passRate = executionDto
+            ?.let { calculateRate(it.passedTests, it.allTests) }
+            ?: "0"
+        this.precisionRate = executionDto
+            ?.let { calculateRate(it.matchedChecks, it.matchedChecks + it.unexpectedChecks) }
+            ?: "0"
+        this.recallRate = executionDto
+            ?.let { calculateRate(it.matchedChecks, it.matchedChecks + it.unmatchedChecks) }
+            ?: "0"
+    }
+
+    private fun calculateRate(numerator: Long, denominator: Long) = denominator.takeIf { it > 0 }
+        ?.run { numerator.toDouble() / denominator }
+        ?.let { it * 100 }
+        ?.toInt()
+        ?.toString()
+}
