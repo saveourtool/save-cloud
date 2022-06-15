4047e208ad98c3e9847c09a4a630f31d0de7fe17
Peter Trifanov <peter.trifanov@mail.ru>
2022-06-15 11:57:55 +0300

    Update TestSuitesController.kt

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/TestSuitesController.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/TestSuitesController.kt
@@ -7,7 +7,6 @@ import com.saveourtool.save.testsuite.TestSuiteDto
 import com.saveourtool.save.v1
 
 import org.quartz.Scheduler
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
