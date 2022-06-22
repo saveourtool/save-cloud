b69f4f301ee7a5206a286613957aa10a6ce224ec
Peter Trifanov <peter.trifanov@mail.ru>
2022-06-22 16:19:31 +0300

    Update build.gradle.kts

--- save-cloud-common/build.gradle.kts
+++ save-cloud-common/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
+                implementation(platform(libs.spring.boot.dependencies))
                 implementation(libs.spring.security.core)
                 implementation(libs.jackson.module.kotlin)
                 implementation(libs.hibernate.jpa21.api)
