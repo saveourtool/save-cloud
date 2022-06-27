4e959f36e2cae3fdb8ff341e69f4bf7a3abeb4f5
Peter Trifanov <peter.trifanov@mail.ru>
2022-06-27 14:26:44 +0300

    Small spring-boot configuration enhancements (#844)
    * Add spring-context-indexer
    * Add `spring-boot-dependencies` BOM
    * Add `spring-boot-configuration-processor` to generate manifest for `@ConfigurationProperties` files
      * Make other classes for `@ConfigurationProperties` in orchestrator nested
    * Wrap logging in `startMysqlDb` task into `doFirst` to reduce confusion during configuration phase

--- save-orchestrator/src/main/kotlin/com/saveourtool/save/orchestrator/Utils.kt
+++ save-orchestrator/src/main/kotlin/com/saveourtool/save/orchestrator/Utils.kt
@@ -4,7 +4,7 @@
 
 package com.saveourtool.save.orchestrator
 
-import com.saveourtool.save.orchestrator.config.AgentSettings
+import com.saveourtool.save.orchestrator.config.ConfigProperties.AgentSettings
 
 import com.github.dockerjava.api.DockerClient
 import com.github.dockerjava.api.async.ResultCallback
