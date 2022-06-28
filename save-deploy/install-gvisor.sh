2de7f9cde7f17505c9d5bfcbde2735eebb5b3157
Peter Trifanov <peter.trifanov@mail.ru>
2022-06-28 16:31:47 +0300

    Finished script

--- save-deploy/install-gvisor.sh
+++ save-deploy/install-gvisor.sh
@@ -5,7 +5,10 @@
 cat > gvisor-install.sh << 'EOF'
 ARCH=$(uname -m)
 URL=https://storage.googleapis.com/gvisor/releases/release/latest/${ARCH}
-wget -nv ${URL}/runsc ${URL}/runsc.sha512 ${URL}/containerd-shim-runsc-v1 ${URL}/containerd-shim-runsc-v1.sha512
+curl ${URL}/runsc -o runsc
+curl ${URL}/runsc.sha512 -o runsc.sha512
+curl ${URL}/containerd-shim-runsc-v1 -o containerd-shim-runsc-v1
+curl ${URL}/containerd-shim-runsc-v1.sha512 -o containerd-shim-runsc-v1.sha512
 sha512sum -c runsc.sha512 -c containerd-shim-runsc-v1.sha512
 rm -f *.sha512
 chmod a+rx runsc containerd-shim-runsc-v1
@@ -13,12 +16,10 @@ mv runsc containerd-shim-runsc-v1 /usr/local/bin
 EOF
 cp gvisor-install.sh /k8s-node
 
-ls -l /k8s-node
 # Run the following steps on the host system
-/usr/bin/nsenter -m/proc/1/ns/mnt -- ls -l /tmp/gvisor
-/usr/bin/nsenter -m/proc/1/ns/mnt -- chmod u+x /tmp/gvisor/gvisor-install.sh
-/usr/bin/nsenter -m/proc/1/ns/mnt /tmp/gvisor/gvisor-install.sh
+/usr/bin/nsenter -m/proc/1/ns/mnt -- sh /tmp/gvisor/gvisor-install.sh
 /usr/bin/nsenter -m/proc/1/ns/mnt -- /usr/local/bin/runsc install
 /usr/bin/nsenter -m/proc/1/ns/mnt -- systemctl reload docker
 
+echo "Finished"
 sleep infinity
