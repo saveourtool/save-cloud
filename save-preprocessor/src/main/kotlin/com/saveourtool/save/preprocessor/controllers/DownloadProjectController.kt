c0b6af2e26aa5c87cec16c6d189badd3b17d6dca
Nariman Abdullin <nulls.narik@gmail.com>
2022-06-29 18:43:06 +0300

    fixed code style issue

--- save-preprocessor/src/main/kotlin/com/saveourtool/save/preprocessor/controllers/DownloadProjectController.kt
+++ save-preprocessor/src/main/kotlin/com/saveourtool/save/preprocessor/controllers/DownloadProjectController.kt
@@ -42,7 +42,6 @@ import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.BodyInserter
