d5ce0b958a0b12c453ca3070f1fe1a7a1e32ff91
Peter Trifanov <peter.trifanov@mail.ru>
2022-07-11 12:22:49 +0300

    [skip ci] Merge detekt SARIF reports

--- buildSrc/src/main/kotlin/com/saveourtool/save/buildutils/DetektConfiguration.kt
+++ buildSrc/src/main/kotlin/com/saveourtool/save/buildutils/DetektConfiguration.kt
@@ -7,9 +7,12 @@ package com.saveourtool.save.buildutils
 import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
 
 /**
@@ -22,8 +25,20 @@ fun Project.configureDetekt() {
         basePath = rootDir.canonicalPath
         buildUponDefaultConfig = true
     }
+    if (path == rootProject.path) {
+        tasks.register("mergeDetektReports", ReportMergeTask::class) {
+            output.set(buildDir.resolve("detekt-sarif-reports/detekt-merged.sarif"))
+        }
+    }
+    val reportMerge = rootProject.tasks.named<ReportMergeTask>("mergeDetektReports") {
+        input.from(
+            tasks.withType<Detekt>().map { it.sarifReportFile }
+        )
+        shouldRunAfter(tasks.withType<Detekt>())
+    }
     tasks.withType<Detekt>().configureEach {
         reports.sarif.required.set(true)
+        finalizedBy(reportMerge)
     }
 }
 
