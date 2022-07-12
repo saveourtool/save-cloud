deb208c1bafa714ff4006acbb205aa661fe0abdb
Alexey Votintsev <79669665+Arrgentum@users.noreply.github.com>
2022-07-12 17:10:54 +0300

    Correct filter in ExecutionView.kt (#892)
    ## Whats Added:
    * Corrected filter in ExecutionView.kt table
    * Some corrected README.md
    
    ## Issue(first part #838)

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/FiltersRow.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/FiltersRow.kt
@@ -18,6 +18,7 @@ import react.dom.input
 import react.dom.option
 import react.dom.select
 import react.fc
+import react.useState
 
 import kotlinx.html.InputType
 import kotlinx.html.classes
@@ -47,8 +48,8 @@ fun testExecutionFiltersRow(
     onChangeStatus: (String) -> Unit,
     onChangeTestSuite: (String) -> Unit,
 ) = fc<Props> {
-    var status: String = initialValueStatus
-    var testSuite: String = initialValueTestSuite
+    val (testSuite, setTestSuite) = useState(initialValueTestSuite)
+    val (status, setStatus) = useState(initialValueStatus)
     div("container-fluid") {
         div("row justify-content-start") {
             div("col-0 pr-1 align-self-center") {
@@ -71,7 +72,7 @@ fun testExecutionFiltersRow(
                     }
 
                     attrs.onChangeFunction = {
-                        status = (it.target as HTMLSelectElement).value
+                        setStatus((it.target as HTMLSelectElement).value)
                     }
                 }
             }
@@ -84,7 +85,7 @@ fun testExecutionFiltersRow(
                     attrs.defaultValue = initialValueTestSuite
                     attrs.required = false
                     attrs.onChangeFunction = {
-                        testSuite = (it.target as HTMLInputElement).value
+                        setTestSuite((it.target as HTMLInputElement).value)
                     }
                 }
             }
