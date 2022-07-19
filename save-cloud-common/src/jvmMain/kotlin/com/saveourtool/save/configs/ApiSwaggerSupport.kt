6dd2b62dce06c9241c2837e0e9b4a0c45df83d35
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-07-19 12:30:59 +0300

    Move config ### What's done: *

--- save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/configs/ApiSwaggerSupport.kt
+++ save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/configs/ApiSwaggerSupport.kt
@@ -2,7 +2,7 @@
  * Utilities related to Swagger
  */
 
-package com.saveourtool.save.backend.configs
+package com.saveourtool.save.configs
 
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.ParameterIn
