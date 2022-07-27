d7981f8ee732b0847f293df2951ade4cb6b1ecba
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2022-07-27 19:15:13 +0300

    Awesome benchmarks filtering (#975)
    ### What's done:
    - hotfix for filtering
    - making modal window transparent for loaders

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/externals/modal/Modal.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/externals/modal/Modal.kt
@@ -20,13 +20,28 @@ private val defaultModalStyle = Styles(
     ).unsafeCast<CssProperties>()
 )
 
+val loaderModalStyle = Styles(
+    content = json(
+        "top" to "25%",
+        "left" to "35%",
+        "right" to "35%",
+        "bottom" to "45%",
+        "overflow" to "hide",
+        "background-color" to "transparent",
+        // small hack to remove modal border and make loader prettier
+        "border" to "1px solid rgba(255, 255, 255, 0.01)"
+    ).unsafeCast<CssProperties>()
+)
+
 /**
- * @param block
+ * @param injectedStyle css style that you can add to the modal window
+ * @param block invoked properties
  */
 fun ChildrenBuilder.modal(
+    injectedStyle: Styles = defaultModalStyle,
     block: @ReactDsl ChildrenBuilder.(ModalProps) -> Unit,
 ): Unit = ReactModal::class.react.invoke {
-    style = defaultModalStyle
+    style = injectedStyle
     shouldCloseOnOverlayClick = true
     block.invoke(this, this)
 }
