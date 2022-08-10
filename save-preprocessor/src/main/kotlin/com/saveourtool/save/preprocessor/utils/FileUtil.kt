0b03a3cfec7f67160955c768e2d66405b19347b2
Nariman Abdullin <nulls.narik@gmail.com>
2022-08-10 17:48:47 +0300

    refactored awesome benchmarks + MD5

--- save-preprocessor/src/main/kotlin/com/saveourtool/save/preprocessor/utils/FileUtil.kt
+++ save-preprocessor/src/main/kotlin/com/saveourtool/save/preprocessor/utils/FileUtil.kt
@@ -3,16 +3,12 @@
 package com.saveourtool.save.preprocessor.utils
 
 import org.slf4j.LoggerFactory
-import org.springframework.util.FileSystemUtils
 
 import java.io.File
-import java.io.IOException
 import java.math.BigInteger
-import java.security.DigestInputStream
 import java.security.MessageDigest
 import java.util.Properties
 
-import kotlin.io.path.createDirectories
 import kotlin.io.path.inputStream
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.properties.decodeFromMap
@@ -28,11 +24,10 @@ fun Collection<okio.Path>.toHash(): String {
     this.forEach { path ->
         path.toNioPath()
             .inputStream()
-            .use { DigestInputStream(it, md) }
-            .let { digestInputStream ->
-                while (-1 != digestInputStream.read()) {
-                    // need to read input stream fully
-                }
+            .let { inputStream ->
+                generateSequence { inputStream.read() }
+                    .takeWhile { it != -1 }
+                    .forEach { md.update(it.toByte()) }
             }
     }
     return BigInteger(1, md.digest()).toString(RADIX)
@@ -54,58 +49,3 @@ inline fun <reified T> decodeFromPropertiesFile(file: File): T {
         rawProperties as Map<String, Any>
     )
 }
-
-/**
- * Create a temporary directory with name based on [seeds]
- *
- * @param seeds a list of strings for directory name creation
- * @param repository name of the repository used for the creation of tmp dir
- * @param deleteExisting whether to delete existing directory
- * @return a [File] representing the created temporary directory
- */
-internal fun generateDirectory(seeds: List<String>, repository: String, deleteExisting: Boolean = true): File {
-    val tmpDir = getTmpDirName(seeds, repository)
-    log.info("For $seeds: starting generate directory $tmpDir")
-    if (tmpDir.exists() && deleteExisting) {
-        deleteDirectory(tmpDir)
-    } else if (tmpDir.exists() && !deleteExisting) {
-        log.info("Directory $tmpDir already exists and delete strategy wasn't provided, won't perform any actions")
-        return tmpDir
-    }
-    return generateDirectory(tmpDir)
-}
-
-/**
- * @param tmpDir directory which need to be deleted
- */
-internal fun deleteDirectory(tmpDir: File) {
-    try {
-        if (FileSystemUtils.deleteRecursively(tmpDir.toPath())) {
-            log.debug("Dir $tmpDir was deleted")
-        }
-    } catch (e: IOException) {
-        log.error("Couldn't properly delete $tmpDir", e)
-    }
-}
-
-/**
- * @param tmpDir directory which need to be created
- * @return a [File] representing the created temporary directory
- */
-@Suppress("TooGenericExceptionCaught")
-internal fun generateDirectory(tmpDir: File): File {
-    try {
-        tmpDir.toPath().createDirectories()
-        log.debug("Dir $tmpDir was created")
-    } catch (e: Exception) {
-        log.error("Couldn't create directories for $tmpDir", e)
-    }
-    return tmpDir
-}
-
-/**
- * @param seeds
- * @param repository
- * @return the file entity with a hashcode created from the list of seeds
- */
-internal fun getTmpDirName(seeds: List<String>, repository: String) = File("$repository/${seeds.hashCode()}")
