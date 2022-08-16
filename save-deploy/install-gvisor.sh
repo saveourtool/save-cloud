3297619d73425d9d21fbb10e8edd95b4f6ba2241
Peter Trifanov <peter.trifanov@mail.ru>
2022-08-16 14:39:38 +0300

    [skip ci] Update install-gvisor.sh

--- save-deploy/install-gvisor.sh
+++ save-deploy/install-gvisor.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -e
 
 # Script from https://gvisor.dev/docs/user_guide/install/#install-latest
 
@@ -21,22 +22,22 @@ cp gvisor-install.sh /k8s-node
 if ! /usr/bin/nsenter -m/proc/1/ns/mnt -- grep -q 'plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runsc' /etc/containerd/config.toml; then
   echo runsc is not installed on "$NODE_NAME", will attempt to install it now
   # Add gvisor to containerd config (https://gvisor.dev/docs/user_guide/containerd/configuration/)
-  /usr/bin/nsenter -m/proc/1/ns/mnt -- cat <<EOF | tee -a /etc/containerd/config.toml
+  /usr/bin/nsenter -m/proc/1/ns/mnt -- sh -c 'cat <<EOF | tee -a /etc/containerd/config.toml
 version = 2
 [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runsc]
   runtime_type = "io.containerd.runsc.v1"
 [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runsc.options]
   TypeUrl = "io.containerd.runsc.v1.options"
   ConfigPath = "/etc/containerd/runsc.toml"
-EOF
-  /usr/bin/nsenter -m/proc/1/ns/mnt -- cat <<EOF >> /etc/containerd/runsc.toml
+EOF'
+  /usr/bin/nsenter -m/proc/1/ns/mnt -- sh -c 'cat <<EOF >> /etc/containerd/runsc.toml
 log_path = "/var/log/runsc/%ID%/shim.log"
 log_level = "debug"
 [runsc_config]
   network = "host"
   debug = "true"
   debug-log = "/var/log/runsc/%ID%/gvisor.%COMMAND%.log"
-EOF
+EOF'
   /usr/bin/nsenter -m/proc/1/ns/mnt -- cat /etc/containerd/runsc.toml
   echo Restarting containerd
   /usr/bin/nsenter -m/proc/1/ns/mnt -- systemctl restart containerd
