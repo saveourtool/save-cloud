80dcb3894ca172fc96a11b43b5c3b6f243d849c5
sanyavertolet <lxnfrolov@gmail.com>
2022-08-30 15:25:19 +0300

    ContestListView Enhancements on frontend
    ### What's done:
     * Reworked ContestListView - moved all the requests to FCs
     * Removed props of FCs - now there are VFCs
     * Made `Enroll` button work
     * Made featured contest tab work
     * Removed the most states from ContestListView

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/contests/NewContestsCard.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/contests/NewContestsCard.kt
@@ -39,7 +39,7 @@ fun ChildrenBuilder.newContestsCard() {
                     className = ClassName("mb-0")
                     a {
                         className = ClassName("text-dark")
-                        href = "#"
+                        href = "#/contests"
                         +"Hurry up!"
                     }
                 }
