1c0e3abc30d849ea40ce5116392e58846a2c5992
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2022-08-31 12:46:09 +0300

    Adding fixes and limitations to contest view
    ### What's done:
    - favicon addition

--- save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/entities/Annotations.kt
+++ save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/entities/Annotations.kt
@@ -13,7 +13,6 @@ import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 import javax.persistence.MappedSuperclass
 import javax.persistence.OneToMany
-import jakarta.validation.constraints.NotNull
 
 actual typealias Entity = Entity
 actual typealias Id = Id
@@ -24,4 +23,4 @@ actual typealias OneToMany = OneToMany
 actual typealias MappedSuperclass = MappedSuperclass
 actual typealias Enumerated = Enumerated
 actual typealias ForeignKey = ForeignKey
-actual typealias NotNull = NotNull
+actual typealias NotNull = javax.validation.constraints.NotNull
