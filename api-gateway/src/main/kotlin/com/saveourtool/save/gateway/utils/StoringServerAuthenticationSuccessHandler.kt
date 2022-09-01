7d7941e5ed7500b07b2a00a2e1665b3cfe7b764b
Vfrolov <vlad-20-11-94@yandex.ru>
2022-09-01 17:36:04 +0300

    Infinite recursion when trying to serialize Execution * fixed serialize and deserialize User Closes #1130

--- api-gateway/src/main/kotlin/com/saveourtool/save/gateway/utils/StoringServerAuthenticationSuccessHandler.kt
+++ api-gateway/src/main/kotlin/com/saveourtool/save/gateway/utils/StoringServerAuthenticationSuccessHandler.kt
@@ -4,11 +4,9 @@ import com.saveourtool.save.domain.Role
 import com.saveourtool.save.entities.User
 import com.saveourtool.save.gateway.config.ConfigurationProperties
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.security.core.Authentication
-import org.springframework.security.oauth2.client.jackson2.OAuth2ClientJackson2Module
 import org.springframework.security.web.server.WebFilterExchange
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler
 import org.springframework.web.reactive.function.client.WebClient
@@ -22,9 +20,6 @@ class StoringServerAuthenticationSuccessHandler(
     configurationProperties: ConfigurationProperties,
 ) : ServerAuthenticationSuccessHandler {
     private val logger = LoggerFactory.getLogger(javaClass)
-    private val objectMapper = ObjectMapper()
-        .findAndRegisterModules()
-        .registerModule(OAuth2ClientJackson2Module())
     private val webClient = WebClient.create(configurationProperties.backend.url)
 
     override fun onAuthenticationSuccess(
@@ -44,7 +39,7 @@ class StoringServerAuthenticationSuccessHandler(
         return webClient.post()
             .uri("/internal/users/new")
             .contentType(MediaType.APPLICATION_JSON)
-            .bodyValue(objectMapper.writeValueAsString(user))
+            .bodyValue(user)
             .retrieve()
             .onStatus({ it.is4xxClientError }) {
                 Mono.error(ResponseStatusException(it.statusCode()))
--- save-backend/src/test/kotlin/com/saveourtool/save/backend/controller/UsersControllerTest.kt
+++ save-backend/src/test/kotlin/com/saveourtool/save/backend/controller/UsersControllerTest.kt
@@ -1,55 +1,41 @@
 package com.saveourtool.save.backend.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.saveourtool.save.backend.configs.WebSecurityConfig
-import com.saveourtool.save.backend.controllers.UsersDetailsController
-import com.saveourtool.save.backend.repository.OriginalLoginRepository
-import com.saveourtool.save.backend.repository.UserRepository
-import com.saveourtool.save.backend.service.UserDetailsService
-import com.saveourtool.save.backend.utils.ConvertingAuthenticationManager
+import com.saveourtool.save.backend.SaveApplication
+import com.saveourtool.save.backend.utils.MySqlExtension
 import com.saveourtool.save.domain.Role
 import com.saveourtool.save.entities.OriginalLogin
 import com.saveourtool.save.entities.User
+import com.saveourtool.save.info.UserInfo
+import com.saveourtool.save.v1
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.context.annotation.Import
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
 
-@WebFluxTest(controllers = [UsersDetailsController::class])
-@Import(
-    WebSecurityConfig::class,
-    ConvertingAuthenticationManager::class,
-    UserDetailsService::class,
-)
+@SpringBootTest(classes = [SaveApplication::class])
 @AutoConfigureWebTestClient
-class UsersDetailsControllerTest {
+@ExtendWith(MySqlExtension::class)
+class UsersControllerTest {
     @Autowired
     private lateinit var webTestClient: WebTestClient
-
-    @MockBean
-    private lateinit var userRepository: UserRepository
-
-    @MockBean
-    private lateinit var originalLoginRepository: OriginalLoginRepository
-
-    private val objectMapper = ObjectMapper()
 
     @Test
     @WithMockUser
     fun `serialize and deserialize user`() {
         val user = User(
-            "admin",
+            "admin2",
             null,
             Role.VIEWER.asSpringSecurityRole(),
-            "basic",
+            "basic2",
             null,
             isActive = false,
-        ).apply { id = 1 }
+        ).apply { id = 99 }
 
         val originalLogin = OriginalLogin(
             "admin",
@@ -58,14 +44,24 @@ class UsersDetailsControllerTest {
         ).apply { id = 4 }
         user.apply { originalLogins = listOf(originalLogin) }
 
-        val result = objectMapper.writeValueAsString(user)
-
         webTestClient.post()
             .uri("/internal/users/new")
             .contentType(MediaType.APPLICATION_JSON)
-            .bodyValue(result)
+            .bodyValue(user)
             .exchange()
             .expectStatus()
             .isOk
+
+        webTestClient.get()
+            .uri("/api/$v1/users/admin2")
+            .exchange()
+            .expectStatus()
+            .isOk
+            .expectBody<UserInfo>()
+            .consumeWith {
+                requireNotNull(it.responseBody)
+                Assertions.assertEquals("admin2", it.responseBody!!.name)
+            }
+
     }
 }
