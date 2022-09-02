3a34dd405035c51c6cc4611340936e9c806f589c
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2022-09-02 19:54:58 +0300

    Adding contest proposal card
    ### What's done:
    - e-mail card

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/contests/ProposeYourContest.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/contests/ProposeYourContest.kt
@@ -5,11 +5,15 @@
 package com.saveourtool.save.frontend.components.views.contests
 
 import csstype.ClassName
+import csstype.Height
+import csstype.Width
 import csstype.rem
 import react.ChildrenBuilder
 import react.dom.html.ReactHTML.div
 
 import kotlinx.js.jso
+import react.dom.html.ReactHTML
+import react.dom.html.ReactHTML.img
 
 /**
  * rendering of a card where we suggest to propose new custom contests
@@ -24,7 +28,14 @@ fun ChildrenBuilder.proposeContest() {
                 minHeight = 7.rem
             }
 
-            // FixMe: Want to propose contest? Write e-mail! undraw_mailbox_re_dvds.svg
+            div {
+                className = ClassName("row")
+                img {
+                    src = "img/undraw_mailbox_re_dvds.svg"
+                }
+            }
+
+            // FixMe: Want to propose a contest? Write e-mail! undraw_mailbox_re_dvds.svg
         }
     }
 }
