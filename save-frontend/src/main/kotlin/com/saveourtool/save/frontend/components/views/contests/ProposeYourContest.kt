b1b7dfd759b89a448189f2cb9e9613c7c9a8d220
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2022-09-03 22:38:13 +0300

    Adding contest proposal card
    ### What's done:
    - e-mail card

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/contests/ProposeYourContest.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/contests/ProposeYourContest.kt
@@ -4,16 +4,15 @@
 
 package com.saveourtool.save.frontend.components.views.contests
 
-import csstype.ClassName
-import csstype.Height
-import csstype.Width
-import csstype.rem
+import csstype.*
 import react.ChildrenBuilder
 import react.dom.html.ReactHTML.div
 
 import kotlinx.js.jso
 import react.dom.html.ReactHTML
 import react.dom.html.ReactHTML.img
+import react.dom.html.ReactHTML.p
+import react.dom.html.ReactHTML.strong
 
 /**
  * rendering of a card where we suggest to propose new custom contests
@@ -21,7 +20,7 @@ import react.dom.html.ReactHTML.img
 @Suppress("MAGIC_NUMBER")
 fun ChildrenBuilder.proposeContest() {
     div {
-        className = ClassName("col-lg-2")
+        className = ClassName("col-lg-3")
         div {
             className = ClassName("card flex-md-row mb-1 box-shadow")
             style = jso {
@@ -29,13 +28,50 @@ fun ChildrenBuilder.proposeContest() {
             }
 
             div {
-                className = ClassName("row")
-                img {
-                    src = "img/undraw_mailbox_re_dvds.svg"
+                className = ClassName("col-lg-12 mt-2")
+                div {
+                    className = ClassName("row")
+
+                    div {
+                        className = ClassName("col-lg-3 pr-0")
+                        img {
+                            src = "img/undraw_mailbox_re_dvds.svg"
+                            style = jso {
+                                width = "100%".unsafeCast<Width>()
+                            }
+                        }
+                    }
+
+                    div {
+                        className = ClassName("col-lg-9")
+                        p {
+                            +"Want to make your own contest? Write us an e-mail:"
+                        }
+                    }
+                }
+
+                div {
+                    className = ClassName("row")
+                    div {
+                        className = ClassName("col-lg-3 pr-0")
+                    }
+
+                    div {
+                        className = ClassName("col-lg-9")
+                        style = jso {
+                            justifyContent = JustifyContent.center
+                            display = Display.flex
+                            alignItems = AlignItems.center
+                            alignSelf = AlignSelf.flexEnd
+                        }
+
+                        strong {
+                            className = ClassName("d-inline-block mb-2 text-success")
+                            +"saveourtool@gmail.com"
+                        }
+                    }
                 }
             }
-
-            // FixMe: Want to propose a contest? Write e-mail! undraw_mailbox_re_dvds.svg
         }
     }
 }
