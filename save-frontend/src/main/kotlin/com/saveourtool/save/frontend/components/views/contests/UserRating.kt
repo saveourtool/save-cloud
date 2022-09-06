d7fceba108e439236082ec00ebbdf367e9d16b87
Arrgentum <alexeyvotintsev@yandex.ru>
2022-09-07 00:00:12 +0300

    Whats added: * Corrected Url query with selected tab in OrganizationView.kt, ProjectView.kt, ContestView.kt, AwesomeBenchmarksView.kt * Changed hyperlinks in topBar * Check selected tab with users Role * Create new files - ParseUrInTab.kt with 2 function which working with url and TabMenubar.kt - parent interface for working with tab menu Enums
    ## Issue(#1044)

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/contests/UserRating.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/contests/UserRating.kt
@@ -190,10 +190,7 @@ private fun userRating() = VFC {
 
                     a {
                         className = ClassName("mb-5")
-                        href = when (selectedTab) {
-                            UserRatingTab.ORGS -> "#/${FrontendRoutes.CONTESTS_GLOBAL_RATING.path}"
-                            UserRatingTab.TOOLS -> "#/${FrontendRoutes.CONTESTS_GLOBAL_RATING.path}/${selectedTab.name.lowercase()}"
-                        }
+                        href = "#/${FrontendRoutes.CONTESTS_GLOBAL_RATING.path}/${selectedTab.name.lowercase()}"
                         +"View more "
                     }
                 }
