9bec01b6f3198992e62508344d3b06b2bbb4c917
Peter Trifanov <peter.trifanov@mail.ru>
2022-09-09 17:39:10 +0300

    Restore router for `error.html`

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/configs/WebConfiguration.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/configs/WebConfiguration.kt
@@ -4,14 +4,11 @@ import com.saveourtool.save.backend.utils.toInstant
 import com.saveourtool.save.v1
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.Resource
 import org.springframework.http.CacheControl
-import org.springframework.util.StringUtils.getFilenameExtension
 import org.springframework.web.reactive.function.server.RouterFunctionDsl
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
@@ -41,6 +38,22 @@ class WebConfiguration(
             "${configProperties.fileStorage.location}/images/avatars/users",
         )
         resources("/api/$v1/resource/**", FileSystemResource("${configProperties.fileStorage.location}/storage/"))
+    }
+
+
+    /**
+     * @param errorPage resource for error.html
+     * @return router bean
+     */
+    @Bean
+    fun indexRouter(
+        @Value("classpath:/error.html") errorPage: Resource,
+    ) = router {
+        GET("/error") {
+            ok().header("Content-Type", "text/html; charset=utf8")
+                .cacheControl(shortExpirationTime) { cachePublic() }
+                .bodyValue(errorPage)
+        }
     }
 
     private fun RouterFunctionDsl.cacheableFsResource(
