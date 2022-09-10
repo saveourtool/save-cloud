4be5576e869ed9eff1930d23b5c3dba22006c4a5
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2022-09-10 16:06:08 +0300

    Adding new cards to contest view (#1184)
    ### What's done:
    - new cards at contest view: projects, rating, etc.

---
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/contests/ContestsStatistics.kt
@@ -0,0 +1,108 @@
+/**
+ * Statistics of SAVE contests: active and finished contests
+ */
+
+package com.saveourtool.save.frontend.components.views.contests
+
+import com.saveourtool.save.entities.ContestDto
+import com.saveourtool.save.frontend.utils.*
+
+import csstype.AlignItems
+import csstype.ClassName
+import csstype.JustifyContent
+import csstype.rem
+import react.VFC
+import react.dom.html.ReactHTML
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.h2
+import react.dom.html.ReactHTML.strong
+import react.useState
+
+import kotlinx.js.jso
+
+val statistics = statistics()
+
+@Suppress("TOO_LONG_FUNCTION", "LongMethod")
+private fun statistics() = VFC {
+    val (activeContests, setActiveContests) = useState<Set<ContestDto>>(emptySet())
+    useRequest {
+        val contests: List<ContestDto> = get(
+            url = "$apiUrl/contests/active",
+            headers = jsonHeaders,
+            loadingHandler = ::loadingHandler,
+        )
+            .decodeFromJsonString()
+        setActiveContests(contests.toSet())
+    }
+
+    val (finishedContests, setFinishedContests) = useState<Set<ContestDto>>(emptySet())
+    useRequest {
+        val contests: List<ContestDto> = get(
+            url = "$apiUrl/contests/active",
+            headers = jsonHeaders,
+            loadingHandler = ::loadingHandler,
+        )
+            .decodeFromJsonString()
+        setFinishedContests(contests.toSet())
+    }
+
+    div {
+        className = ClassName("col-lg-4")
+        ReactHTML.div {
+            className = ClassName("card flex-md-row mb-1 box-shadow")
+            style = jso {
+                minHeight = 10.rem
+            }
+            div {
+                className = ClassName("col-lg-6 mt-2")
+                div {
+                    className = ClassName("row")
+                    style = jso {
+                        justifyContent = JustifyContent.center
+                        alignItems = AlignItems.center
+                    }
+                    strong {
+                        className = ClassName("d-inline-block mb-2 card-text")
+                        +"Active contests:"
+                    }
+                }
+                div {
+                    className = ClassName("row")
+                    style = jso {
+                        justifyContent = JustifyContent.center
+                        alignItems = AlignItems.center
+                    }
+                    h2 {
+                        className = ClassName("text-dark")
+                        +activeContests.size.toString()
+                    }
+                }
+            }
+            div {
+                className = ClassName("col-lg-6 mt-2")
+                div {
+                    className = ClassName("row")
+                    style = jso {
+                        justifyContent = JustifyContent.center
+                        alignItems = AlignItems.center
+                    }
+                    strong {
+                        className = ClassName("d-inline-block mb-2 card-text ")
+                        +"Finished contests:"
+                    }
+                }
+                div {
+                    className = ClassName("row")
+                    style = jso {
+                        justifyContent = JustifyContent.center
+                        alignItems = AlignItems.center
+                    }
+                    h2 {
+                        className = ClassName("text-dark")
+                        +finishedContests.size.toString()
+                    }
+                }
+            }
+        }
+    }
+}
