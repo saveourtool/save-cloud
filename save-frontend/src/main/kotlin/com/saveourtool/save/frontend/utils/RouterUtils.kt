fe3474513e8d803a9d8302e373f43b8be28a9969
Peter Trifanov <peter.trifanov@gmail.com>
2022-09-13 17:39:32 +0300

    Code style

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/utils/RouterUtils.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/utils/RouterUtils.kt
@@ -2,11 +2,38 @@
  * Utilities to work with react-router
  */
 
+@file:Suppress("FILE_NAME_MATCH_CLASS")
+
 package com.saveourtool.save.frontend.utils
 
 import history.Location
 import react.*
 import react.router.*
+
+/**
+ * Interface that provides [NavigateFunction] to use [navigation support](https://reactrouter.com/en/v6.3.0/api#navigation)
+ * from react-router v6
+ */
+interface NavigateFunctionContext {
+    /**
+     * Function that performs navigation using react-router library
+     */
+    val navigate: NavigateFunction
+}
+
+/**
+ * @param handler DOM builder that can consume [NavigateFunctionContext]
+ */
+fun <T : Props> ChildrenBuilder.withNavigate(handler: ChildrenBuilder.(NavigateFunctionContext) -> Unit) {
+    val wrapper: FC<T> = FC {
+        val navigate = useNavigate()
+        val ctx = object : NavigateFunctionContext {
+            override val navigate: NavigateFunction = navigate
+        }
+        handler(ctx)
+    }
+    wrapper()
+}
 
 /**
  * Wrapper function component to allow using router props in class components
@@ -20,20 +47,4 @@ fun <T : Props> withRouter(handler: ChildrenBuilder.(Location, Params) -> Unit) 
     val location = useLocation()
     val params = useParams()
     handler(location, params)
-}
-
-interface NavigateFunctionContext {
-    val navigate: NavigateFunction
-}
-
-fun <T : Props> ChildrenBuilder.withNavigate(handler: ChildrenBuilder.(NavigateFunctionContext) -> Unit) {
-    val wrapper = FC<T> {
-        val navigate = useNavigate()
-        val ctx = object : NavigateFunctionContext {
-            override val navigate: NavigateFunction
-                get() = navigate
-        }
-        handler(ctx)
-    }
-    wrapper()
 }
