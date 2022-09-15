b52758fa9226a75ffeae30e30ac9812add4fc982
Nariman Abdullin <nulls.narik@gmail.com>
2022-09-15 17:25:48 +0300

    [skip-ci] WIP

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/DownloadFilesController.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/DownloadFilesController.kt
@@ -4,10 +4,12 @@ import com.saveourtool.save.agent.TestExecutionDto
 import com.saveourtool.save.backend.ByteBufferFluxResponse
 import com.saveourtool.save.backend.configs.ApiSwaggerSupport
 import com.saveourtool.save.backend.repository.AgentRepository
+import com.saveourtool.save.backend.service.ExecutionService
 import com.saveourtool.save.backend.service.OrganizationService
 import com.saveourtool.save.backend.service.ProjectService
 import com.saveourtool.save.backend.service.UserDetailsService
 import com.saveourtool.save.backend.storage.*
+import com.saveourtool.save.backend.utils.mapToInputStream
 import com.saveourtool.save.domain.*
 import com.saveourtool.save.from
 import com.saveourtool.save.permission.Permission
@@ -19,6 +21,7 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import io.swagger.v3.oas.annotations.tags.Tags
+import org.apache.commons.io.IOUtils
 
 import org.slf4j.LoggerFactory
 import org.springframework.core.io.ClassPathResource
@@ -28,14 +31,19 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.security.core.Authentication
+import org.springframework.util.FileSystemUtils
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
 
 import java.io.FileNotFoundException
 import java.nio.ByteBuffer
+import kotlin.io.path.deleteExisting
+import kotlin.io.path.div
+import kotlin.io.path.outputStream
 
 /**
  * A Spring controller for file downloading
@@ -55,6 +63,7 @@ class DownloadFilesController(
     private val organizationService: OrganizationService,
     private val userDetailsService: UserDetailsService,
     private val projectService: ProjectService,
+    private val executionService: ExecutionService,
 ) {
     private val logger = LoggerFactory.getLogger(DownloadFilesController::class.java)
 
@@ -142,6 +151,45 @@ class DownloadFilesController(
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .build()
         )
+
+    @GetMapping(path = ["/internal/files/download-by-execution-id"], produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE])
+    fun downloadAllByExecutionId(
+        @RequestParam executionId: Long,
+    ): Mono<ByteBufferFluxResponse> = blockingToMono {
+        executionService.findExecution(executionId)
+    }
+        .switchIfEmptyToNotFound {
+            "Not found execution by id $executionId"
+        }
+        .map { it.getFileKeys() }
+        .flatMap { fileKeys ->
+            val tempDir = createTempDir().toPath()
+            val tempFile = createTempFile().toPath()
+
+            fileKeys.toFlux()
+                .flatMap { fileKey ->
+                    fileStorage.download(fileKey)
+                        .mapToInputStream()
+                        .map { inputStream ->
+                            (tempDir / fileKey.name).outputStream().use {
+                                IOUtils.copy(inputStream, it)
+                            }
+                        }
+                }
+                .then()
+                .map {
+                    tempDir.compressAsZipTo(tempFile)
+                    FileSystemUtils.deleteRecursively(tempDir)
+                }
+                .map {
+                    ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .body(tempFile.toByteBufferFlux())
+                }
+                .doAfterTerminate {
+                    tempFile.deleteExisting()
+                }
+        }
 
     @Operation(
         method = "POST",
