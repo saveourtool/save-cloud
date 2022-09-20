9a64ee1b72700fdff3f8f8acdad7b13869f64c27
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2022-09-20 15:02:39 +0300

    Adding a temp mock for the sandbox (#1250)
    ### What's done:
    - this mock is a copy-paste of orchestrator

---
+++ save-sandbox/build.gradle.kts
@@ -0,0 +1,74 @@
+import com.saveourtool.save.buildutils.*
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("com.saveourtool.save.buildutils.spring-boot-configuration")
+    id("de.undercouch.download")  // can't use `alias`, because this plugin is a transitive dependency of kotlin-gradle-plugin
+    id("org.gradle.test-retry") version "1.4.1"
+}
+
+configureJacoco()
+configureSpotless()
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = Versions.jdk
+        freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlin.RequiresOptIn" + "-Xcontext-receivers"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    retry {
+        // There once were flaky tests in sandbox, but it seems like they became stable.
+        // Settings can be restored or removed, as required.
+        failOnPassedAfterRetry.set(false)
+        maxFailures.set(5)
+        maxRetries.set(1)
+    }
+}
+
+dependencies {
+    api(projects.saveCloudCommon)
+    implementation(libs.dockerJava.core)
+    implementation(libs.dockerJava.transport.httpclient5)
+    implementation(libs.kotlinx.serialization.json.jvm)
+    implementation(libs.commons.compress)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.zip4j)
+    implementation(libs.spring.cloud.starter.kubernetes.client.config)
+    implementation(libs.fabric8.kubernetes.client)
+    implementation(libs.spring.kafka)
+    testImplementation(projects.testUtils)
+    testImplementation(libs.fabric8.kubernetes.server.mock)
+}
+
+// todo: this logic is duplicated between agent and frontend, can be moved to a shared plugin in buildSrc
+val generateVersionFileTaskProvider: TaskProvider<Task> = tasks.register("generateVersionFile") {
+    val versionsFile = File("$buildDir/generated/src/generated/Versions.kt")
+
+    dependsOn(rootProject.tasks.named("getSaveCliVersion"))
+    inputs.file(pathToSaveCliVersion)
+    outputs.file(versionsFile)
+
+    doFirst {
+        val saveCliVersion = readSaveCliVersion()
+        versionsFile.parentFile.mkdirs()
+        versionsFile.writeText(
+            """
+            package generated
+
+            internal const val SAVE_CORE_VERSION = "$saveCliVersion"
+
+            """.trimIndent()
+        )
+    }
+}
+kotlin.sourceSets.getByName("main") {
+    kotlin.srcDir("$buildDir/generated/src")
+}
+tasks.withType<KotlinCompile>().forEach {
+    it.dependsOn(generateVersionFileTaskProvider)
+}
