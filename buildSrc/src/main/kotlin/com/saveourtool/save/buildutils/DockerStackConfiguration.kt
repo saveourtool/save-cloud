2e13115ba2cd125ae11e18755cc2797ad35762f7
Vfrolov <vlad-20-11-94@yandex.ru>
2022-09-29 12:29:49 +0300

    Create db schema to Sandbox * Added save_sandbox schema * Created agent and execution tables closes #1241

--- buildSrc/src/main/kotlin/com/saveourtool/save/buildutils/DockerStackConfiguration.kt
+++ buildSrc/src/main/kotlin/com/saveourtool/save/buildutils/DockerStackConfiguration.kt
@@ -53,8 +53,6 @@ fun Project.createStackDeployTask(profile: String) {
                            |      - "3306:3306"
                            |    environment:
                            |      - "MYSQL_ROOT_PASSWORD=123"
-                           |    volumes:
-                           |      - ${rootProject.projectDir}/db/init:/docker-entrypoint-initdb.d:rw
                            |  zookeeper:
                            |    image: confluentinc/cp-zookeeper:latest
                            |    environment:
