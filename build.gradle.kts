1b95f3375d843710730ce6c149fb9e06975fea64
Arrgentum <alexeyvotintsev@yandex.ru>
2022-10-04 11:18:08 +0300

    ## Whats added - Added trash button in settings/organization - Added the check for the possibility of deleting an organization depending on the number of projects
    ## Issue(#1227)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import com.saveourtool.save.buildutils.*
 
+import org.apache.tools.ant.taskdefs.condition.Os
+
 plugins {
     alias(libs.plugins.talaiot.base)
     alias(libs.plugins.liquibase.gradle)
