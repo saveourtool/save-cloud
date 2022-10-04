8af4ac9c8675f176408c26a1a43e96d8c0e60b9f
Arrgentum <alexeyvotintsev@yandex.ru>
2022-10-04 11:16:32 +0300

    ## Whats added - Added trash button in settings/organization - Added the check for the possibility of deleting an organization depending on the number of projects
    ## Issue(#1227)

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ tasks.withType<org.liquibase.gradle.LiquibaseTask>().configureEach {
     @Suppress("MAGIC_NUMBER")
     this.javaLauncher.set(project.extensions.getByType<JavaToolchainService>().launcherFor {
         // liquibase-core 4.7.0 and liquibase-gradle 2.1.1 fails on Java >= 13 on Windows; works on Mac
-        val javaVersion = 17  // if (Os.isFamily(Os.FAMILY_MAC)) 17 else 11
+        val javaVersion = if (Os.isFamily(Os.FAMILY_MAC)) 17 else 11
         languageVersion.set(JavaLanguageVersion.of(javaVersion))
     })
 }
