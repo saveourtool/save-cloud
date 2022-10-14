c235283a8051b23966f6fae261941b45a276c8db
Peter Trifanov <peter.trifanov@gmail.com>
2022-10-14 17:58:00 +0300

    [skip ci] WIP

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/utils/KubernetesAuthenticationUtils.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/utils/KubernetesAuthenticationUtils.kt
@@ -8,13 +8,16 @@ import io.fabric8.kubernetes.client.utils.Serialization
 import org.intellij.lang.annotations.Language
 import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform
 import org.springframework.boot.cloud.CloudPlatform
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.switchIfEmpty
 import reactor.kotlin.core.publisher.toMono
 
 @Component
@@ -24,9 +27,12 @@ class ServiceAccountTokenExtractorConverter : ServerAuthenticationConverter {
         return Mono.justOrEmpty(
             exchange.request.headers["X-Service-Account-Token"]?.firstOrNull()
         ).map { token ->
+            logger.debug { "Starting to process `X-Service-Account-Token` of an incoming request" }
             PreAuthenticatedAuthenticationToken("TokenSupplier", token)
         }
     }
+
+    private val logger = getLogger<ServiceAccountTokenExtractorConverter>()
 }
 
 @Component
@@ -35,31 +41,43 @@ class ServiceAccountAuthenticatingManager(
     val kubernetesClient: KubernetesClient,
 ) : ReactiveAuthenticationManager {
     override fun authenticate(authentication: Authentication): Mono<Authentication> {
-        return (authentication as PreAuthenticatedAuthenticationToken).credentials.toMono().map { token ->
-            @Language("yaml")
-            val tokenReview = """
-            |apiVersion: authentication.k8s.io/v1
-            |kind: TokenReview
-            |metadata:
-            |  name: service-account-validity-check
-            |  namespace: ${kubernetesClient.namespace}
-            |spec:
-            |  token: $token
-        """.trimMargin()
-            logger.debug {
-                "Will create k8s resource from the following YAML:\n${tokenReview.prependIndent("    ")}"
+        return authentication.toMono()
+            .filter { it is PreAuthenticatedAuthenticationToken }
+            .map {
+                val token = it.credentials
+                @Language("yaml")
+                val tokenReview = """
+                    |apiVersion: authentication.k8s.io/v1
+                    |kind: TokenReview
+                    |metadata:
+                    |  name: service-account-validity-check
+                    |  namespace: ${kubernetesClient.namespace}
+                    |spec:
+                    |  token: $token
+                """.trimMargin()
+                logger.debug {
+                    "Will create k8s resource from the following YAML:\n${tokenReview.prependIndent("    ")}"
+                }
+                val response = kubernetesClient.resource(tokenReview).createOrReplace() as TokenReview
+                logger.debug {
+                    "Got the following response from the API server:\n${
+                        Serialization.yamlMapper().writeValueAsString(response).prependIndent("    ")
+                    }"
+                }
+                response
             }
-            val response = kubernetesClient.resource(tokenReview).createOrReplace() as TokenReview
-            logger.debug {
-                "Got the following response from the API server:\n${
-                    Serialization.yamlMapper().writeValueAsString(response).prependIndent("    ")
-                }"
+            .filter { response ->
+                val isAuthenticated = response.status.error.isNullOrEmpty() && response.status.authenticated
+                logger.debug { "After the response from TokenReview, request authentication is $isAuthenticated" }
+                isAuthenticated
             }
-            response
-        }
-            .map { response ->
-                authentication.isAuthenticated = response.status.error == null && response.status.authenticated
-                authentication
+            .map<Authentication> {
+                with (authentication) {
+                    UsernamePasswordAuthenticationToken.authenticated(principal, credentials, authorities)
+                }
+            }
+            .switchIfEmpty {
+                Mono.error { BadCredentialsException("Invalid token") }
             }
     }
 
