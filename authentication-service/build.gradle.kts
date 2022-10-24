8a6a84c0bdce88af22e92933fd33e18f72d89936
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-10-24 18:09:32 +0300

    WIP ### What's done: * WIP

--- authentication-service/build.gradle.kts
+++ authentication-service/build.gradle.kts
@@ -2,7 +2,7 @@ import com.saveourtool.save.buildutils.*
 
 plugins {
     id("com.saveourtool.save.buildutils.kotlin-jvm-configuration")
-    id("com.saveourtool.save.buildutils.spring-data-configuration")
+    id("com.saveourtool.save.buildutils.spring-boot-configuration")
     alias(libs.plugins.download)
     kotlin("plugin.allopen")
     alias(libs.plugins.kotlin.plugin.jpa)
