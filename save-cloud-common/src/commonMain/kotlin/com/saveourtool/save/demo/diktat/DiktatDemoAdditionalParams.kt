011dc4a02ee3161c813bf62f310da28b5d249609
sanyavertolet <lxnfrolov@gmail.com>
2022-11-07 14:41:38 +0300

    [Sandbox] DemoService
    ### What's done:
    * Added AbstractDemoService interface
    * Added DiktatDemoController
    * Added DiktatDemoService
    * Added DemoAdditionalParams interface
    * Added DemoResult interface
    
    (#1415)

--- save-cloud-common/src/commonMain/kotlin/com/saveourtool/save/demo/diktat/DiktatDemoAdditionalParams.kt
+++ save-cloud-common/src/commonMain/kotlin/com/saveourtool/save/demo/diktat/DiktatDemoAdditionalParams.kt
@@ -1,12 +1,14 @@
 package com.saveourtool.save.demo.diktat
 
 import com.saveourtool.save.demo.DemoAdditionalParams
+import kotlinx.serialization.Serializable
 
 /**
  * @property mode
  * @property tool
  * @property config
  */
+@Serializable
 data class DiktatDemoAdditionalParams(
     val mode: DiktatDemoMode,
     val tool: DiktatDemoTool,
--- save-sandbox/src/main/kotlin/com/saveourtool/save/sandbox/controller/DiktatDemoController.kt
+++ save-sandbox/src/main/kotlin/com/saveourtool/save/sandbox/controller/DiktatDemoController.kt
@@ -1,10 +1,7 @@
 package com.saveourtool.save.sandbox.controller
 
 import com.saveourtool.save.configs.ApiSwaggerSupport
-import com.saveourtool.save.demo.diktat.DiktatDemoAdditionalParams
-import com.saveourtool.save.demo.diktat.DiktatDemoMode
-import com.saveourtool.save.demo.diktat.DiktatDemoResult
-import com.saveourtool.save.demo.diktat.DiktatDemoTool
+import com.saveourtool.save.demo.diktat.*
 import com.saveourtool.save.sandbox.service.DiktatDemoService
 import com.saveourtool.save.utils.*
 
@@ -26,20 +23,13 @@ class DiktatDemoController(
     private val diktatDemoService: DiktatDemoService,
 ) {
     /**
-     * @param inputFiles pair of code file and config file
-     * @param tool one of [DiktatDemoTool]
-     * @param mode one of [DiktatDemoMode]
+     * @param diktatDemoRunRequest request with all required additional info
      * @return [DiktatDemoResult]
      */
     @PostMapping("/run")
     fun runCheckDemo(
-        @RequestBody inputFiles: Pair<String, String?>,
-        @RequestParam(required = false, defaultValue = "DIKTAT") tool: DiktatDemoTool,
-        @RequestParam(required = false, defaultValue = "FIX") mode: DiktatDemoMode,
+        @RequestBody diktatDemoRunRequest: DiktatDemoRunRequest
     ): Mono<DiktatDemoResult> = blockingToMono {
-        diktatDemoService.runDemo(
-            inputFiles.first,
-            DiktatDemoAdditionalParams(mode = mode, tool = tool, config = inputFiles.second)
-        )
+        diktatDemoService.runDemo(diktatDemoRunRequest.codeLines, diktatDemoRunRequest.params)
     }
 }
