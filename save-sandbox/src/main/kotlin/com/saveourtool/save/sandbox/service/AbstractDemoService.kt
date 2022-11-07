b5139fd7015ad68b6fd802dd61992121e5f60911
Alexander Frolov <lxnfrolov@gmail.com>
2022-11-07 15:01:02 +0300

    [Sandbox] DemoService (#1416)
    * [Sandbox] DemoService
    
    ### What's done:
    * Added AbstractDemoService interface
    * Added DiktatDemoController
    * Added DiktatDemoService
    * Added DemoAdditionalParams interface
    * Added DemoResult interface
    
    (#1415)
    
    Co-authored-by: Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>

---
+++ save-sandbox/src/main/kotlin/com/saveourtool/save/sandbox/service/AbstractDemoService.kt
@@ -0,0 +1,18 @@
+package com.saveourtool.save.sandbox.service
+
+import com.saveourtool.save.demo.DemoAdditionalParams
+import com.saveourtool.save.demo.DemoResult
+
+/**
+ * Abstract service interface for different demonstration services
+ */
+interface AbstractDemoService<in P : DemoAdditionalParams, out R : DemoResult> {
+    /**
+     * Run demo on [demoFileLines] with [demoAdditionalParams] and return result as [DemoResult]
+     *
+     * @param demoFileLines input file that will be used for demo
+     * @param demoAdditionalParams additional params as [DemoAdditionalParams]
+     * @return report as [DemoResult]
+     */
+    fun runDemo(demoFileLines: String, demoAdditionalParams: P? = null): R
+}
