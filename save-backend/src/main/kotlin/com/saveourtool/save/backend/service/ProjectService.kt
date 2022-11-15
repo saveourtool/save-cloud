bd297e1cf8bbc865d49fe3d9de7e11e4f2d72023
Alexey Votintsev <79669665+Arrgentum@users.noreply.github.com>
2022-11-15 14:25:11 +0300

    Add delete, recover and banned Project logic in backend (#1444)
    ## Whats added:
    - Add field ```banned``` in OrganizationStatus and ProjectStatus
    - Add deleted, banned and recovery Project logic in backend
    - Correct lnk-user-organiaztion.csv and lnk-user-project.csv - change ```SUPER_ADMIN``` to ```OWNER```
    - Correct tests
    ## Issue(#1438)
    
    Co-authored-by: Arrgentum <alexeyvotintsev@yandex.ru>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andrey Kuleshov <andrewkuleshov7@gmail.com>

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/service/ProjectService.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/service/ProjectService.kt
@@ -4,10 +4,7 @@ import com.saveourtool.save.backend.repository.ProjectRepository
 import com.saveourtool.save.backend.repository.UserRepository
 import com.saveourtool.save.backend.security.ProjectPermissionEvaluator
 import com.saveourtool.save.domain.ProjectSaveStatus
-import com.saveourtool.save.entities.Organization
-import com.saveourtool.save.entities.Project
-import com.saveourtool.save.entities.ProjectStatus
-import com.saveourtool.save.entities.User
+import com.saveourtool.save.entities.*
 import com.saveourtool.save.filters.ProjectFilters
 import com.saveourtool.save.permission.Permission
 
@@ -52,6 +49,50 @@ class ProjectService(
     }
 
     /**
+     * Mark organization with [project] as deleted
+     *
+     * @param newStatus is new status for [project]
+     * @param project is organization in which the status will be changed
+     * @return project
+     */
+    @Suppress("UnsafeCallOnNullableType")
+    private fun changeProjectStatus(project: Project, newStatus: ProjectStatus): Project = project
+        .apply {
+            status = newStatus
+        }
+        .let {
+            projectRepository.save(it)
+        }
+
+    /**
+     * Mark organization [project] as deleted
+     *
+     * @param project an [project] to delete
+     * @return deleted organization
+     */
+    fun deleteProject(project: Project): Project =
+            changeProjectStatus(project, ProjectStatus.DELETED)
+
+    /**
+     * Mark organization with [project] as created.
+     * If an organization was previously banned, then all its projects become deleted.
+     *
+     * @param project an [project] to create
+     * @return recovered project
+     */
+    fun recoverProject(project: Project): Project =
+            changeProjectStatus(project, ProjectStatus.CREATED)
+
+    /**
+     * Mark organization with [project] and all its projects as banned.
+     *
+     * @param project an [project] to ban
+     * @return banned project
+     */
+    fun banProject(project: Project): Project =
+            changeProjectStatus(project, ProjectStatus.BANNED)
+
+    /**
      * @param project [Project] to be updated
      * @return updated [project]
      */
@@ -77,6 +118,7 @@ class ProjectService(
     /**
      * @param name
      * @param organizationName
+     * @return project
      */
     fun findByNameAndOrganizationName(name: String, organizationName: String) = projectRepository.findByNameAndOrganizationName(name, organizationName)
 
