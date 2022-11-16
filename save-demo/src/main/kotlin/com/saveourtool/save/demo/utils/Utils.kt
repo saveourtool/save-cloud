725883de2f0a492300e0ed872ca6e8074bd58abc
sanyavertolet <lxnfrolov@gmail.com>
2022-11-16 12:21:48 +0300

    [Demo] Refactoring and supporting storage
    ### What's done:
     * Reworked CliRunner - now it implements Runner interface
     * Supported downloading tools from storage
     * Marked all the created structures and interfaces with Component annotation
    
    (#1451)

---
+++ save-demo/src/main/kotlin/com/saveourtool/save/demo/utils/Utils.kt
@@ -0,0 +1,12 @@
+/**
+ * Utils
+ */
+
+package com.saveourtool.save.demo.utils
+
+import java.util.*
+
+/**
+ * @return randomly generated name from UUID
+ */
+fun generateName() = UUID.randomUUID().toString()
