a5a2b64cd87c2cd68bec7a2e021dccf12eab0c97
Dmitry Morozovsky <27895587+icemachined@users.noreply.github.com>
2022-11-21 23:06:44 +0800

    counters in Execution are not updated #877 (#1406)
    Co-authored-by: Andrey Kuleshov <andrewkuleshov7@gmail.com>
    Co-authored-by: Nariman Abdullin <nulls.narik@gmail.com>

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/service/TestExecutionService.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/service/TestExecutionService.kt
@@ -4,7 +4,6 @@ import com.saveourtool.save.agent.TestExecutionDto
 import com.saveourtool.save.backend.repository.AgentRepository
 import com.saveourtool.save.backend.repository.ExecutionRepository
 import com.saveourtool.save.backend.repository.TestExecutionRepository
-import com.saveourtool.save.backend.repository.TestRepository
 import com.saveourtool.save.backend.utils.secondsToLocalDateTime
 import com.saveourtool.save.core.result.CountWarnings
 import com.saveourtool.save.domain.TestResultLocation
@@ -21,13 +20,11 @@ import org.slf4j.Logger
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.PlatformTransactionManager
-import org.springframework.transaction.TransactionDefinition
+import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.transaction.support.TransactionTemplate
 
 import java.nio.file.Paths
 import java.time.LocalDateTime
-import java.util.concurrent.ConcurrentHashMap
 
 import kotlin.io.path.pathString
 
@@ -36,16 +33,10 @@ import kotlin.io.path.pathString
  */
 @Service
 class TestExecutionService(private val testExecutionRepository: TestExecutionRepository,
-                           private val testRepository: TestRepository,
                            private val agentRepository: AgentRepository,
                            private val executionRepository: ExecutionRepository,
                            transactionManager: PlatformTransactionManager,
 ) {
-    private val locks: ConcurrentHashMap<Long, Any> = ConcurrentHashMap()
-    private val transactionTemplate = TransactionTemplate(transactionManager).apply {
-        propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
-    }
-
     /**
      * Returns a page of [TestExecution]s with [executionId]
      *
@@ -166,7 +157,7 @@ class TestExecutionService(private val testExecutionRepository: TestExecutionRep
         "MagicNumber",
         "PARAMETER_NAME_IN_OUTER_LAMBDA",
     )
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun saveTestResult(testExecutionsDtos: List<TestExecutionDto>): List<TestExecutionDto> {
         log.debug { "Saving ${testExecutionsDtos.size} test results from agent ${testExecutionsDtos.first().agentContainerId}" }
         // we take agent id only from first element, because all test executions have same execution
@@ -224,35 +215,30 @@ class TestExecutionService(private val testExecutionRepository: TestExecutionRep
                         log.error("Test execution $testExecDto with id=$testExecutionId for execution id=$executionId cannot be updated because its status is not RUNNING")
                     })
         }
-        val lock = locks.computeIfAbsent(executionId) { Any() }
-        synchronized(lock) {
-            transactionTemplate.execute {
-                val execution = executionRepository.findById(executionId).get()
-                execution.apply {
-                    log.debug {
-                        "Updating counters in execution id=$executionId: running=$runningTests-${counters.total()}, " +
-                                "passed=$passedTests+${counters.passed}, failed=$failedTests+${counters.failed}, skipped=$skippedTests+${counters.skipped}"
-                    }
-                    runningTests -= counters.total()
-                    passedTests += counters.passed
-                    failedTests += counters.failed
-                    skippedTests += counters.skipped
-
-                    unmatchedChecks += counters.unmatchedChecks
-                    matchedChecks += counters.matchedChecks
-                    expectedChecks += counters.expectedChecks
-                    unexpectedChecks += counters.unexpectedChecks
-
-                    val executionScore = toDto().calculateScore(scoreType = ScoreType.F_MEASURE)
-
-                    if (!executionScore.isValidScore()) {
-                        log.error("Execution score for execution id $id is invalid: $executionScore")
-                    }
-                    score = executionScore
-                }
-                executionRepository.save(execution)
+        val execution = executionRepository.findWithLockingById(executionId).orNotFound()
+        execution.apply {
+            log.debug {
+                "Updating counters in execution id=$executionId: running=$runningTests-${counters.total()}, " +
+                        "passed=$passedTests+${counters.passed}, failed=$failedTests+${counters.failed}, skipped=$skippedTests+${counters.skipped}"
             }
+            runningTests -= counters.total()
+            passedTests += counters.passed
+            failedTests += counters.failed
+            skippedTests += counters.skipped
+
+            unmatchedChecks += counters.unmatchedChecks
+            matchedChecks += counters.matchedChecks
+            expectedChecks += counters.expectedChecks
+            unexpectedChecks += counters.unexpectedChecks
+
+            val executionScore = toDto().calculateScore(scoreType = ScoreType.F_MEASURE)
+
+            if (!executionScore.isValidScore()) {
+                log.error("Execution score for execution id $id is invalid: $executionScore")
+            }
+            score = executionScore
         }
+        executionRepository.save(execution)
         return lostTests
     }
 
