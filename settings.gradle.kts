8435cdfbe63f105a77fecb02675096e27b62902e
Peter Trifanov <peter.trifanov@gmail.com>
2022-11-23 13:44:42 +0300

    Remove maven repo for sarif4k

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,18 +8,6 @@ dependencyResolutionManagement {
                 includeGroup("com.saveourtool.save")
             }
         }
-        maven {
-            url = uri("https://maven.pkg.github.com/saveourtool/sarif4k")
-            val gprUser: String? by settings
-            val gprKey: String? by settings
-            credentials {
-                username = gprUser
-                password = gprKey
-            }
-            content {
-                includeGroup("io.github.detekt.sarif4k")
-            }
-        }
     }
 }
 
