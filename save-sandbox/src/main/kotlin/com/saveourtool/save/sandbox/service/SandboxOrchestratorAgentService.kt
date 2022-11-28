620cbd3c16d1abfb490d1824f3babce13c55b670
Nariman Abdullin <nulls.narik@gmail.com>
2022-11-28 21:03:59 +0300

    Renamed AgentRepository to OrchestratorAgentService to avoid duplication with repository name (#1522)

--- save-sandbox/src/main/kotlin/com/saveourtool/save/sandbox/service/SandboxOrchestratorAgentService.kt
+++ save-sandbox/src/main/kotlin/com/saveourtool/save/sandbox/service/SandboxOrchestratorAgentService.kt
@@ -9,6 +9,7 @@ import com.saveourtool.save.entities.AgentStatusesForExecution
 import com.saveourtool.save.execution.ExecutionStatus
 import com.saveourtool.save.orchestrator.service.AgentStatusList
 import com.saveourtool.save.orchestrator.service.IdList
+import com.saveourtool.save.orchestrator.service.OrchestratorAgentService
 import com.saveourtool.save.orchestrator.service.TestExecutionList
 import com.saveourtool.save.request.RunExecutionRequest
 import com.saveourtool.save.sandbox.entity.SandboxAgent
@@ -38,13 +39,13 @@ internal typealias BodilessResponseEntity = ResponseEntity<Void>
  * Sandbox implementation for agent service
  */
 @Component("SandboxAgentRepositoryForOrchestrator")
-class SandboxAgentRepository(
+class SandboxOrchestratorAgentService(
     private val sandboxAgentRepository: SandboxAgentRepository,
     private val sandboxAgentStatusRepository: SandboxAgentStatusRepository,
     private val sandboxExecutionRepository: SandboxExecutionRepository,
     private val sandboxStorage: SandboxStorage,
     @Value("\${sandbox.agent-settings.sandbox-url}/sandbox/internal") private val sandboxUrlForAgent: String,
-) : com.saveourtool.save.orchestrator.service.AgentRepository {
+) : OrchestratorAgentService {
     override fun getInitConfig(containerId: String): Mono<AgentInitConfig> = getExecutionAsMonoByContainerId(containerId)
         .zipWhen { execution ->
             sandboxStorage.list(execution.userId, SandboxStorageKeyType.FILE)
