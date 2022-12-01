dcfbf79b9951b3400a88ec5bb99f301f4fc49bb1
sanyavertolet <lxnfrolov@gmail.com>
2022-12-01 12:46:02 +0300

    Demo minor fixes
    ### What's done:
     * Removed diktat from demo resources
     * Removed diktat.cmd from demo resources
     * Removed ktlint-cli from demo resources
     * Removed ktlint-cli.cmd from demo resources
     * Changed save-demo port from 5401 to 5421
     * Overwritten ToolStorage#isKey method to escape .DS_Store on Mac that leads to IndexOutOfBoundsException
    
    ### Todo:
     * Support kubernetes deployment

--- save-demo/src/main/kotlin/com/saveourtool/save/demo/storage/ToolStorage.kt
+++ save-demo/src/main/kotlin/com/saveourtool/save/demo/storage/ToolStorage.kt
@@ -6,6 +6,7 @@ import com.saveourtool.save.utils.pathNamesTill
 import org.springframework.stereotype.Component
 import java.nio.file.Path
 import kotlin.io.path.div
+import kotlin.io.path.name
 
 private const val TOOLS_PATH = "tools"
 
@@ -18,9 +19,15 @@ class ToolStorage(
 ) : AbstractFileBasedStorage<ToolKey>(Path.of(configProperties.fileStorage.location) / TOOLS_PATH) {
     @Suppress("DestructuringDeclarationWithTooManyEntries")
     override fun buildKey(rootDir: Path, pathToContent: Path): ToolKey {
-        val (_, ownerName, toolName, vcsTagName, executableName) = pathToContent.pathNamesTill(rootDir)
+        val (ownerName, toolName, vcsTagName, executableName) = pathToContent.pathNamesTill(rootDir)
         return ToolKey(ownerName, toolName, vcsTagName, executableName)
     }
 
     override fun buildPathToContent(rootDir: Path, key: ToolKey): Path = rootDir / key.ownerName / key.toolName / key.vcsTagName / key.executableName
+
+    override fun isKey(rootDir: Path, pathToContent: Path): Boolean = pathToContent.name != DS_STORE
+
+    companion object {
+        private const val DS_STORE = ".DS_Store"
+    }
 }
