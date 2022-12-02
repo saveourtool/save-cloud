bbd3a0e8d584bca4285041c43e977c6a5c8cb30d
Vfrolov <vlad-20-11-94@yandex.ru>
2022-12-02 16:24:04 +0300

    Added show logs for cpg view

--- save-cloud-common/src/commonMain/kotlin/com/saveourtool/save/demo/cpg/CpgResult.kt
+++ save-cloud-common/src/commonMain/kotlin/com/saveourtool/save/demo/cpg/CpgResult.kt
@@ -5,8 +5,8 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 
 /**
- * @property cpgGraph
- * @property logs
+ * @property cpgGraph file that contains CpgGraph definition
+ * @property logs execution logs
  * @property applicationName query id SQL request to a NEO4
  */
 @Serializable
