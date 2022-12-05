cc8fee802cb3216fe35b0c47f9949445f7638189
Nariman Abdullin <nulls.narik@gmail.com>
2022-12-05 17:02:20 +0300

    Filtering when avatar doesn't exist (#1563)

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/AvatarController.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/AvatarController.kt
@@ -5,7 +5,7 @@ import com.saveourtool.save.backend.storage.AvatarKey
 import com.saveourtool.save.backend.storage.AvatarStorage
 import com.saveourtool.save.configs.ApiSwaggerSupport
 import com.saveourtool.save.utils.AvatarType
-import com.saveourtool.save.utils.orNotFound
+import com.saveourtool.save.utils.switchIfEmptyToNotFound
 import com.saveourtool.save.v1
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 import kotlin.time.Duration.Companion.days
 import kotlin.time.toJavaDuration
 
@@ -57,7 +58,7 @@ internal class AvatarController(
         imageName = imageName
     )
         .toMonoResponse()
-        .orNotFound {
+        .switchIfEmptyToNotFound {
             "Not found avatar for user $userName with name $imageName"
         }
 
@@ -82,11 +83,12 @@ internal class AvatarController(
         imageName = imageName
     )
         .toMonoResponse()
-        .orNotFound {
+        .switchIfEmptyToNotFound {
             "Not found avatar for organization $organizationName with name $imageName"
         }
 
-    private fun AvatarKey.toMonoResponse(): Mono<ByteBufferFluxResponse> = avatarStorage.doesExist(this)
+    private fun AvatarKey.toMonoResponse(): Mono<ByteBufferFluxResponse> = this.toMono()
+        .filterWhen(avatarStorage::doesExist)
         .flatMap {
             avatarStorage.lastModified(this)
                 .map { lastModified ->
