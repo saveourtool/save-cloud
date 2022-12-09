d39f36086967a3a9a5ae8e495224470dbc0203b6
Nariman Abdullin <nulls.narik@gmail.com>
2022-12-09 14:51:12 +0300

    fixed repository for AgentStatus

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/service/AgentStatusService.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/service/AgentStatusService.kt
@@ -1,26 +1,22 @@
 package com.saveourtool.save.backend.service
 
 import com.saveourtool.save.backend.repository.AgentStatusRepository
+import com.saveourtool.save.backend.repository.LnkExecutionAgentRepository
 import org.springframework.stereotype.Service
 
 /**
  * Service for agent status
  */
 @Service
-class AgentStatusService(private val agentStatusRepository: AgentStatusRepository) {
-    /**
-     * @param projectId
-     * @return Unit
-     */
-    internal fun deleteAgentStatusWithProjectId(projectId: Long) =
-            agentStatusRepository.findByAgentExecutionProjectId(projectId).forEach {
-                agentStatusRepository.delete(it)
-            }
-
+class AgentStatusService(
+    private val agentStatusRepository: AgentStatusRepository,
+    private val lnkExecutionAgentRepository: LnkExecutionAgentRepository,
+) {
     /**
      * @param executionIds
      * @return Unit
      */
-    internal fun deleteAgentStatusWithExecutionIds(executionIds: List<Long>) =
-            agentStatusRepository.deleteByAgentExecutionIdIn(executionIds)
+    internal fun deleteAgentStatusWithExecutionIds(executionIds: List<Long>) = lnkExecutionAgentRepository.findByExecutionIdIn(executionIds)
+        .map { it.agent.requiredId() }
+        .let { agentStatusRepository.deleteByAgentIdIn(it) }
 }
