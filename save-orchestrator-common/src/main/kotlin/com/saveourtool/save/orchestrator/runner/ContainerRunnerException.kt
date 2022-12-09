76f5ad626fa85606b9ef0db1d55405080f683589
Nariman Abdullin <nulls.narik@gmail.com>
2022-12-09 18:57:23 +0300

    renaming agentId (agent.containerId) to containerId (#1591)

--- save-orchestrator-common/src/main/kotlin/com/saveourtool/save/orchestrator/runner/ContainerRunnerException.kt
+++ save-orchestrator-common/src/main/kotlin/com/saveourtool/save/orchestrator/runner/ContainerRunnerException.kt
@@ -3,4 +3,4 @@ package com.saveourtool.save.orchestrator.runner
 /**
  * Base exception during interaction with an engine responsible for running save-agents
  */
-class AgentRunnerException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
+class ContainerRunnerException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
