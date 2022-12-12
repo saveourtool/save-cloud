bbd1b34dcec86910eabbf9a0e85835f66408fe7b
Peter Trifanov <peter.trifanov@gmail.com>
2022-12-12 15:45:41 +0300

    Eclude security autoconfuguration from preprocessor

--- save-preprocessor/src/main/kotlin/com/saveourtool/save/preprocessor/SavePreprocessor.kt
+++ save-preprocessor/src/main/kotlin/com/saveourtool/save/preprocessor/SavePreprocessor.kt
@@ -2,8 +2,12 @@ package com.saveourtool.save.preprocessor
 
 import com.saveourtool.save.authservice.config.SecurityWebClientCustomizers
 import com.saveourtool.save.preprocessor.config.ConfigProperties
+
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Import
 import org.springframework.web.reactive.config.EnableWebFlux
@@ -11,7 +15,11 @@ import org.springframework.web.reactive.config.EnableWebFlux
 /**
  * An entrypoint for spring for save-preprocessor
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = [
+    ReactiveSecurityAutoConfiguration::class,
+    ReactiveUserDetailsServiceAutoConfiguration::class,
+    ReactiveManagementWebSecurityAutoConfiguration::class,
+])
 @EnableWebFlux
 @EnableConfigurationProperties(ConfigProperties::class)
 @Import(SecurityWebClientCustomizers::class)
