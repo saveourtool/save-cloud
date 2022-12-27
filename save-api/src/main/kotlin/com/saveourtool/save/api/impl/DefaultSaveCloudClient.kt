ad47045c2a2d203791c0f466b658883148f9c248
Nariman Abdullin <nulls.narik@gmail.com>
2022-12-27 17:47:30 +0300

    diktatFix & fixed test (commented for now)

--- save-api/src/main/kotlin/com/saveourtool/save/api/impl/DefaultSaveCloudClient.kt
+++ save-api/src/main/kotlin/com/saveourtool/save/api/impl/DefaultSaveCloudClient.kt
@@ -10,6 +10,7 @@ import com.saveourtool.save.api.http.deleteAndCheck
 import com.saveourtool.save.api.http.getAndCheck
 import com.saveourtool.save.api.http.postAndCheck
 import com.saveourtool.save.api.io.readChannel
+import com.saveourtool.save.entities.*
 import com.saveourtool.save.execution.ExecutionDto
 import com.saveourtool.save.execution.TestingType.CONTEST_MODE
 import com.saveourtool.save.filters.ProjectFilters
@@ -19,12 +20,12 @@ import com.saveourtool.save.testsuite.TestSuiteDto
 import com.saveourtool.save.utils.getLogger
 import com.saveourtool.save.utils.supportJLocalDateTime
 import com.saveourtool.save.v1
+
 import arrow.core.Either
 import arrow.core.flatMap
 import arrow.core.getOrHandle
 import arrow.core.left
 import arrow.core.right
-import com.saveourtool.save.entities.*
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.auth.Auth
@@ -46,11 +47,13 @@ import io.ktor.http.HttpHeaders.ContentType
 import io.ktor.http.contentType
 import io.ktor.http.escapeIfNeeded
 import io.ktor.serialization.kotlinx.json.json
+
 import java.lang.System.nanoTime
 import java.net.URL
 import java.nio.file.Path
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.MILLISECONDS
+
 import kotlin.coroutines.CoroutineContext
 import kotlin.io.path.exists
 import kotlin.io.path.fileSize
