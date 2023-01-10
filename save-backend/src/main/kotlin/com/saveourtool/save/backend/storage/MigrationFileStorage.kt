8977decf75d7b0b884bfb03330f846a8f41c0e14
Nariman Abdullin <nulls.narik@gmail.com>
2023-01-10 12:54:54 +0300

    Added AbstractMigrationStorage
    ### What's done:
    - added AbstractMigrationStorage
    - refactored MigrationFileStorage
    
    It's part of #1670

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/storage/MigrationFileStorage.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/storage/MigrationFileStorage.kt
@@ -4,52 +4,33 @@ import com.saveourtool.save.domain.FileInfo
 import com.saveourtool.save.domain.FileKey
 import com.saveourtool.save.domain.ProjectCoordinates
 import com.saveourtool.save.entities.FileDto
-import com.saveourtool.save.storage.Storage
-import com.saveourtool.save.utils.getLogger
-import com.saveourtool.save.utils.info
-import com.saveourtool.save.utils.millisToInstant
+import com.saveourtool.save.entities.toFileDto
+import com.saveourtool.save.storage.AbstractMigrationStorage
 
-import org.slf4j.Logger
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
-import java.nio.ByteBuffer
-import java.time.Instant
 import javax.annotation.PostConstruct
-
-import kotlinx.datetime.*
 
 /**
  * Storage for evaluated tools are loaded by users
  */
 @Service
 class MigrationFileStorage(
-    private val oldFileStorage: FileStorage,
-    private val newFileStorage: NewFileStorage,
-) : Storage<FileKey> {
+    oldFileStorage: FileStorage,
+    newFileStorage: NewFileStorage,
+) : AbstractMigrationStorage<FileKey, FileDto>(oldFileStorage, newFileStorage) {
     /**
      * A temporary init method which copies file from one storage to another
      */
     @PostConstruct
-    fun migration() {
-        oldFileStorage.list()
-            .map { fileKey ->
-                fileKey to fileKey.toFileDto()
-            }
-            .flatMap { (fileKey, fileDto) ->
-                newFileStorage.upload(fileDto, oldFileStorage.download(fileKey))
-                    .map {
-                        log.info {
-                            "Copied $fileKey to new storage with key $fileDto"
-                        }
-                    }
-                    .flatMap {
-                        oldFileStorage.delete(fileKey)
-                    }
-            }
-            .subscribe()
+    fun init() {
+        super.migrate()
     }
+
+    override fun FileDto.toOldKey(): FileKey = toFileKey()
+
+    override fun FileKey.toNewKey(): FileDto = toFileDto()
 
     /**
      * @param projectCoordinates
@@ -73,36 +54,4 @@ class MigrationFileStorage(
      */
     fun list(projectCoordinates: ProjectCoordinates): Flux<FileKey> = list()
         .filter { it.projectCoordinates == projectCoordinates }
-
-    @Suppress("WRONG_OVERLOADING_FUNCTION_ARGUMENTS")
-    override fun list(): Flux<FileKey> = newFileStorage.list()
-        .map { fileDto ->
-            FileKey(
-                projectCoordinates = fileDto.projectCoordinates,
-                name = fileDto.name,
-                uploadedMillis = fileDto.uploadedTime.toInstant(TimeZone.UTC).toEpochMilliseconds()
-            )
-        }
-
-    override fun download(key: FileKey): Flux<ByteBuffer> = newFileStorage.download(key.toFileDto())
-
-    override fun upload(key: FileKey, content: Flux<ByteBuffer>): Mono<Long> = newFileStorage.upload(key.toFileDto(), content)
-
-    override fun delete(key: FileKey): Mono<Boolean> = newFileStorage.delete(key.toFileDto())
-
-    override fun lastModified(key: FileKey): Mono<Instant> = newFileStorage.lastModified(key.toFileDto())
-
-    override fun contentSize(key: FileKey): Mono<Long> = newFileStorage.contentSize(key.toFileDto())
-
-    override fun doesExist(key: FileKey): Mono<Boolean> = newFileStorage.doesExist(key.toFileDto())
-
-    private fun FileKey.toFileDto(): FileDto = FileDto(
-        projectCoordinates = this.projectCoordinates,
-        name = this.name,
-        uploadedTime = this.uploadedMillis.millisToInstant().toLocalDateTime(TimeZone.UTC),
-    )
-
-    companion object {
-        private val log: Logger = getLogger<MigrationFileStorage>()
-    }
 }
