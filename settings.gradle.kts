4821f67c96f7a4a3886db5f2ea346a08cc002305
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-01-19 12:59:57 +0300

    Enable build scan publishing for GitHub Action-triggered builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,17 @@ dependencyResolutionManagement {
     }
 }
 
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.12.2"
+}
+
 includeBuild("gradle/plugins")
 include("api-gateway")
 include("save-backend")
@@ -25,3 +36,13 @@ include("save-demo-cpg")
 include("test-analysis-core")
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
