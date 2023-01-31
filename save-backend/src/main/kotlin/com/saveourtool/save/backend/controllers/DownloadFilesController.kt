ee2a89e2866a25f31585d373132c10b00a46002a
Nariman Abdullin <nulls.narik@gmail.com>
2023-01-31 00:12:14 -0800

    diktatFix & detektAll

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/DownloadFilesController.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/DownloadFilesController.kt
@@ -1,6 +1,5 @@
 package com.saveourtool.save.backend.controllers
 
-import com.saveourtool.save.agent.TestExecutionDto
 import com.saveourtool.save.backend.service.*
 import com.saveourtool.save.backend.service.AgentService
 import com.saveourtool.save.backend.service.OrganizationService
