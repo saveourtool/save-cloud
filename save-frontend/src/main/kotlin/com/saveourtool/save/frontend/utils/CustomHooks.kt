404a28fb2007dbb20e6a8013be9bed043a547a8a
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-02-01 20:10:21 +0300

    Implement the `useEventStream()` (SSE, HTTP GET only) and `useNewlineDelimitedJson()` _React_ hooks
    ### What's done:
    
     - This change allows live event streaming from the back-end (`Flux<T>` or
       `ParallelFlux<T>`) to the font-end, using either `text/event-stream`
       (arbitrary text data, incl. JSON) or `application/x-ndjson` (JSON only).
     - On the server side, _Spring_ has support for both content types.
     - This is a part of #1096.

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/utils/CustomHooks.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/utils/CustomHooks.kt
@@ -8,13 +8,22 @@ package com.saveourtool.save.frontend.utils
 
 import com.saveourtool.save.frontend.components.requestStatusContext
 
+import js.core.jso
+import org.w3c.dom.EventSource
+import org.w3c.dom.EventSource.Companion.CONNECTING
+import org.w3c.dom.MessageEvent
+import org.w3c.dom.events.Event
+import org.w3c.fetch.Headers
 import org.w3c.fetch.Response
+import react.EffectBuilder
 import react.useContext
 import react.useEffect
 import react.useState
 
 import kotlinx.browser.document
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.onCompletion
 
 /**
  * Runs the provided [action] only once of first render
@@ -152,3 +161,153 @@ fun useBackground(colorStyle: Style) {
         configureTopBar(colorStyle)
     }
 }
+
+/**
+ * Creates a callback that is run synchronously.
+ *
+ * Only works inside functional components.
+ *
+ * @param effect the callback body (executed under [useEffect]).
+ * @return a lambda that triggers the callback.
+ * @see useEffect
+ */
+fun useDeferredEffect(
+    effect: EffectBuilder.() -> Unit,
+): () -> Unit {
+    var isRunning by useState(initialValue = false)
+
+    useEffect(isRunning) {
+        if (!isRunning) {
+            return@useEffect
+        }
+
+        effect()
+
+        isRunning = false
+    }
+
+    return {
+        if (!isRunning) {
+            isRunning = true
+        }
+    }
+}
+
+/**
+ * Reads the response of `text/event-stream` `Content-Type`.
+ * _Server-Sent Events_ (SSE) are limited to `HTTP GET` method.
+ *
+ * Only works inside functional components.
+ *
+ * @param url the URL that accepts an `HTTP GET` and can respond with a
+ *   `text/event-stream` `Content-Type`.
+ * @param withCredentials whether
+ *   [CORS](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS)
+ *   should be allowed, the default is `false`.
+ * @param eventType the event type selector.
+ *   The same HTTP endpoint may return events of different types, and this
+ *   selector allows to receive only a certain subset of the whole event volume.
+ *   The default is `message`.
+ *   On the server side, use `org.springframework.http.codec.ServerSentEvent` to
+ *   create an event with a custom type.
+ * @param init invoked before an event stream is requested.
+ *   Allowed to change the state of components.
+ * @param onCompletion invoked if the response is `HTTP 200 OK`, and when the
+ *   server closes the connection.
+ *   Allowed to change the state of components.
+ * @param onError invoked when the event source reports an error.
+ *   Allowed to change the state of components.
+ * @param onEvent invoked when a new event arrives.
+ *   Allowed to change the state of components.
+ * @return a lambda that triggers the callback.
+ * @see useNewlineDelimitedJson
+ */
+@Suppress(
+    "LongParameterList",
+    "TOO_MANY_PARAMETERS",
+    "TYPE_ALIAS",
+)
+fun useEventStream(
+    url: String,
+    withCredentials: Boolean = false,
+    eventType: String = "message",
+    init: EffectBuilder.() -> Unit = {},
+    onCompletion: EffectBuilder.() -> Unit = {},
+    onError: EffectBuilder.(error: Event, readyState: Short) -> Unit = { _, _ -> },
+    onEvent: EffectBuilder.(message: MessageEvent) -> Unit,
+): () -> Unit =
+        useDeferredEffect {
+            init()
+
+            val source = EventSource(
+                url = url,
+                eventSourceInitDict = jso {
+                    this.withCredentials = withCredentials
+                },
+            )
+
+            source.addEventListener(eventType, { event ->
+                onEvent(event as MessageEvent)
+            })
+
+            source.onerror = { error ->
+                if (source.readyState == CONNECTING) {
+                    source.close()
+                    onCompletion()
+                } else {
+                    onError(error, source.readyState)
+                }
+            }
+        }
+
+/**
+ * Reads the response of `application/x-ndjson` `Content-Type`.
+ *
+ * Only works inside functional components.
+ *
+ * @param url the URL that accepts an `HTTP GET` and can respond with a
+ *   `application/x-ndjson` `Content-Type`.
+ * @param init invoked before an event stream is requested.
+ *   Allowed to change the state of components.
+ * @param onCompletion invoked if the response is `HTTP 200 OK`, and when the
+ *   server closes the connection.
+ *   Allowed to change the state of components.
+ * @param onError invoked when the event source reports an error.
+ *   Allowed to change the state of components.
+ * @param onEvent invoked when a new event arrives.
+ *   Allowed to change the state of components.
+ * @return a lambda that triggers the callback.
+ * @see useEventStream
+ */
+internal fun useNewlineDelimitedJson(
+    url: String,
+    init: () -> Unit = {},
+    onCompletion: () -> Unit = {},
+    onError: suspend (response: Response) -> Unit = { _ -> },
+    onEvent: (message: String) -> Unit,
+): () -> Unit =
+        useDeferredRequest {
+            init()
+
+            val response = get(
+                url = url,
+                params = jso(),
+                headers = Headers(jso {
+                    Accept = "application/x-ndjson"
+                }),
+                loadingHandler = ::noopLoadingHandler,
+                responseHandler = ::noopResponseHandler,
+            )
+
+            when {
+                response.ok -> response
+                    .readLines()
+                    .filter(String::isNotEmpty)
+                    .onCompletion {
+                        onCompletion()
+                    }
+                    .collect(onEvent)
+
+                else -> onError(response)
+            }
+        }
--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/utils/RequestUtils.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/utils/RequestUtils.kt
@@ -11,16 +11,27 @@ import com.saveourtool.save.frontend.components.requestStatusContext
 import com.saveourtool.save.frontend.http.HttpStatusException
 import com.saveourtool.save.v1
 
+import js.buffer.ArrayBuffer
 import js.core.jso
+import js.typedarrays.Int8Array
+import js.typedarrays.Uint8Array
 import org.w3c.dom.url.URLSearchParams
 import org.w3c.fetch.Headers
 import org.w3c.fetch.RequestCredentials
 import org.w3c.fetch.RequestInit
 import org.w3c.fetch.Response
+import web.streams.ReadableStream
+import web.streams.ReadableStreamDefaultReadValueResult
 
-import kotlin.js.undefined
+import kotlin.js.Promise
 import kotlinx.browser.window
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.*
@@ -530,6 +541,122 @@ suspend fun WithRequestStatusContext.loadingHandler(request: suspend () -> Respo
  */
 @Suppress("MAGIC_NUMBER")
 fun Response.isConflict(): Boolean = this.status == 409.toShort()
+
+/**
+ * Reads the HTTP response body as a flow of strings.
+ *
+ * @return the string  flow produced from the body of this HTTP response.
+ * @see Response.inputStream
+ */
+suspend fun Response.readLines(): Flow<String> =
+        inputStream().decodeToString()
+
+/**
+ * Reads the HTTP response body as a byte flow.
+ *
+ * @return the byte flow produced from the body of this HTTP response.
+ * @see Response.readLines
+ */
+@OptIn(FlowPreview::class)
+suspend fun Response.inputStream(): Flow<Byte> {
+    val reader = body.unsafeCast<ReadableStream<Uint8Array>>().getReader()
+
+    return flow {
+        /*
+         * Read the response body in byte chunks, emitting each chunk as it's
+         * available.
+         */
+        while (true) {
+            @Suppress(
+                "GENERIC_VARIABLE_WRONG_DECLARATION",
+                "TYPE_ALIAS",
+            )
+            val resultAsync = reader
+                .read()
+                .unsafeCast<Promise<ReadableStreamDefaultReadValueResult<Uint8Array>>>()
+
+            val result = resultAsync.await()
+
+            val jsBytes: Uint8Array = result.value
+
+            if (jsBytes == undefined || result.done) {
+                break
+            }
+
+            emit(jsBytes.asByteArray())
+        }
+    }
+        .flatMapConcat { bytes ->
+            /*
+             * Concatenate all chunks into a byte flow.
+             */
+            bytes.asSequence().asFlow()
+        }
+        .onCompletion {
+            /*
+             * Wait for the stream to get closed.
+             */
+            reader.closed.asDeferred().await()
+
+            /*
+             * Release the reader's lock on the stream.
+             * See https://developer.mozilla.org/en-US/docs/Web/API/ReadableStreamDefaultReader/releaseLock
+             */
+            reader.releaseLock()
+        }
+}
+
+/**
+ * Decodes this byte flow into a flow of strings, assuming UTF-8 encoding.
+ *
+ * Malformed byte sequences are replaced with `\uFFFD`.
+ *
+ * @see ByteArray.decodeToString
+ */
+fun Flow<Byte>.decodeToString(): Flow<String> =
+        flow {
+            var accumulator: MutableList<Byte> = arrayListOf()
+
+            collect { value ->
+                accumulator = when (value) {
+                    /*
+                     * Ignore.
+                     */
+                    '\r'.code.toByte() -> accumulator
+
+                    '\n'.code.toByte() -> {
+                        emit(accumulator)
+                        arrayListOf()
+                    }
+
+                    else -> accumulator.apply {
+                        add(value)
+                    }
+                }
+            }
+
+            emit(accumulator)
+        }
+            .map(Collection<Byte>::toByteArray)
+            .map(ByteArray::decodeToString)
+
+/**
+ * Converts this [Uint8Array] (most probably obtained by reading an HTTP
+ * response body) to the standard [ByteArray].
+ *
+ * Conversion from an `Uint8Array` to an `Int8Array` is necessary &mdash;
+ * otherwise, non-ASCII data will get corrupted.
+ *
+ * @return the converted instance.
+ */
+@Suppress("UnsafeCastFromDynamic")
+fun Uint8Array.asByteArray(): ByteArray =
+        Int8Array(
+            buffer = buffer.unsafeCast<ArrayBuffer>(),
+            byteOffset = byteOffset,
+            length = length,
+        )
+            .asDynamic()
 
 /**
  * If this component has context, set [response] in this context. Otherwise, fallback to redirect.
