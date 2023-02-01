37774a2a43aff7f533f1badfb6f3e08fea7f8d37
Nariman Abdullin <nulls.narik@gmail.com>
2023-02-01 08:45:10 -0800

    removed unused imports

--- save-backend/src/test/kotlin/com/saveourtool/save/backend/DownloadFilesTest.kt
+++ save-backend/src/test/kotlin/com/saveourtool/save/backend/DownloadFilesTest.kt
@@ -23,7 +23,6 @@ import org.jetbrains.annotations.Blocking
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.io.TempDir
 import org.mockito.kotlin.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -38,8 +37,6 @@ import org.springframework.http.MediaType
 import org.springframework.http.client.MultipartBodyBuilder
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
 import org.springframework.test.web.reactive.server.expectBodyList
@@ -50,7 +47,6 @@ import reactor.core.scheduler.Schedulers
 import reactor.kotlin.core.publisher.toMono
 import java.nio.ByteBuffer
 
-import java.nio.file.Path
 import java.time.LocalDateTime
 import java.util.*
 import java.util.concurrent.Future
