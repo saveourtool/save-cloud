941796e1bb704865272bb7b3566c1c1a37eeb495
Peter Trifanov <peter.trifanov@gmail.com>
2023-02-01 13:09:13 +0000

    chore(deps): Update Kotlni to 1.8.0 along with related dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    `kotlin-dsl`
+    // Using dependencies with Kotlin 1.8 requires compatible embedded Kotlin in Gradle.
+    // Gradle >= 7.6.0 (containing `kotlin-dsl` >= 2.4.0) is compatible; Gradle 7.5.x still embeds Kotlin 1.6.x.
+    `kotlin-dsl` version "2.4.1"
 }
 
 repositories {
@@ -18,7 +20,6 @@ dependencies {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "17"
         freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlin.RequiresOptIn"
     }
 }
