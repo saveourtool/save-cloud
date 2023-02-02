8b1139a06fc19c672fad3615fa450919a856d0a5
Nariman Abdullin <nulls.narik@gmail.com>
2023-02-02 19:47:18 +0300

    added kdocs

--- save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/s3/S3Operations.kt
+++ save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/s3/S3Operations.kt
@@ -6,17 +6,48 @@ import software.amazon.awssdk.core.async.ResponsePublisher
 import software.amazon.awssdk.services.s3.model.*
 import java.nio.ByteBuffer
 
+typealias GetObjectResponsePublisher = ResponsePublisher<GetObjectResponse>
+
+/**
+ * Base interface to operate with S3 in single bucket (like JdbcOperations)
+ */
 interface S3Operations {
+    /**
+     * @param prefix
+     * @return [Flux] with all response
+     */
     fun listObjectsV2(prefix: String): Flux<ListObjectsV2Response>
 
-    fun getObject(s3Key: String): Mono<ResponsePublisher<GetObjectResponse>>
+    /**
+     * @param s3Key
+     * @return [Mono] with response as [ResponsePublisher]
+     */
+    fun getObject(s3Key: String): Mono<GetObjectResponsePublisher>
 
+    /**
+     * @param s3Key
+     * @param content
+     * @return [Mono] with response
+     */
     fun uploadObject(s3Key: String, content: Flux<ByteBuffer>): Mono<CompleteMultipartUploadResponse>
 
+    /**
+     * @param s3key
+     * @param contentLength
+     * @param content
+     * @return [Mono] with response
+     */
     fun uploadObject(s3key: String, contentLength: Long, content: Flux<ByteBuffer>): Mono<PutObjectResponse>
 
+    /**
+     * @param s3key
+     * @return [Mono] with response
+     */
     fun deleteObject(s3key: String): Mono<DeleteObjectResponse>
 
+    /**
+     * @param s3key
+     * @return [Mono] with response
+     */
     fun headObject(s3key: String): Mono<HeadObjectResponse>
-
 }
