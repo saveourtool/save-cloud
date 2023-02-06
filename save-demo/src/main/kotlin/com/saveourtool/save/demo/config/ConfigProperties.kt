80614de3e574b77eeb5505f440f5b187e25b11b6
Nariman Abdullin <nulls.narik@gmail.com>
2023-02-06 13:14:32 +0300

    added S3Configuration

--- save-demo/src/main/kotlin/com/saveourtool/save/demo/config/ConfigProperties.kt
+++ save-demo/src/main/kotlin/com/saveourtool/save/demo/config/ConfigProperties.kt
@@ -9,21 +9,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 /**
- * @property fileStorage
  * @property s3Storage configuration of S3 storage
  * @property backend
  */
 @ConstructorBinding
 @ConfigurationProperties(prefix = "demo")
 data class ConfigProperties(
-    val fileStorage: FileStorageConfig,
     val s3Storage: S3OperationsProperties,
     val backend: String,
-)
-
-/**
- * @property location
- */
-data class FileStorageConfig(
-    val location: String,
 )
