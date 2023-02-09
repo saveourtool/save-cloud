41efe5b5b18b9d05aa4661fce054d189b6576680
sanyavertolet <lxnfrolov@gmail.com>
2023-02-09 14:30:17 +0300

    save-demo-agent-configuration.gradle.kts
    ### What's done:
     * Added save-demo-agent-configuration.gradle.kts
     * Added runtime dependency to save-demo (save-demo now depends on save-demo-agent)

---
+++ gradle/plugins/src/main/kotlin/com/saveourtool/save/buildutils/save-demo-agent-configuration.gradle.kts
@@ -0,0 +1,35 @@
+/**
+ * Configuration utilities for spring boot projects
+ */
+
+package com.saveourtool.save.buildutils
+
+import de.undercouch.gradle.tasks.download.Download
+import org.gradle.api.tasks.TaskProvider
+import org.gradle.kotlin.dsl.*
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+import java.io.File
+plugins {
+    kotlin("jvm")
+    id("de.undercouch.download")
+}
+
+@Suppress("GENERIC_VARIABLE_WRONG_DECLARATION")
+val downloadSaveDemoAgentDistroTaskProvider: TaskProvider<Download> = tasks.register<Download>("downloadSaveDemoAgentDistro") {
+    enabled = findProperty("saveDemoAgentDistroFilepath") != null
+
+    val saveDemoAgentDistroFilepath = findProperty("saveDemoAgentDistroFilepath")?.toString() ?: "file:\\\\not-found"
+    src { saveDemoAgentDistroFilepath }
+    dest { "$buildDir/demoAgentDistro/${File(saveDemoAgentDistroFilepath).name}" }
+
+    overwrite(false)
+}
+
+dependencies {
+    if (DefaultNativePlatform.getCurrentOperatingSystem().isWindows) {
+        logger.warn("Dependency `save-demo-agent` is omitted on Windows because of problems with cross-compilation.")
+        addProvider("runtimeOnly", downloadSaveDemoAgentDistroTaskProvider.map { it.outputs.files })
+    } else {
+        add("runtimeOnly", project(":save-demo-agent", "distribution"))
+    }
+}
--- save-demo/build.gradle.kts
+++ save-demo/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("com.saveourtool.save.buildutils.kotlin-jvm-configuration")
     id("com.saveourtool.save.buildutils.spring-boot-app-configuration")
+    id("com.saveourtool.save.buildutils.save-demo-agent-configuration")
     id("com.saveourtool.save.buildutils.spring-data-configuration")
     id("com.saveourtool.save.buildutils.code-quality-convention")
     alias(libs.plugins.kotlin.plugin.serialization)
