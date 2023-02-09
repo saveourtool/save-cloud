a5e5b9cae794c4d904fe45bce9254fab66c9d61e
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-02-09 12:01:13 +0300

    WIP

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/routing/BasicRouting.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/routing/BasicRouting.kt
@@ -120,6 +120,7 @@ val basicRouting: FC<AppProps> = FC { props ->
     Routes {
         listOf(
             WelcomeView::class.react.create { userInfo = props.userInfo } to "/",
+            testSuiteValidationView.create() to "/a",
             SandboxView::class.react.create() to "/$SANDBOX",
             AboutUsView::class.react.create() to "/$ABOUT_US",
             CreationView::class.react.create() to "/$CREATE_PROJECT",
