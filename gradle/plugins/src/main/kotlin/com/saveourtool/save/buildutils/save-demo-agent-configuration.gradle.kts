819ec165c75a13d08610987ffe68fba6281b69d1
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-02-10 19:47:06 +0300

    Correct the path/URL manupulation when `saveDemoAgentDistroFileUrl` is unset (#1861)
    This change also renames the property from `saveDemoAgentDistroFilepath` to
    `saveDemoAgentDistroFileUrl`.

--- gradle/plugins/src/main/kotlin/com/saveourtool/save/buildutils/save-demo-agent-configuration.gradle.kts
+++ gradle/plugins/src/main/kotlin/com/saveourtool/save/buildutils/save-demo-agent-configuration.gradle.kts
@@ -5,25 +5,20 @@
 package com.saveourtool.save.buildutils
 
 import de.undercouch.gradle.tasks.download.Download
-import org.gradle.api.tasks.TaskProvider
-import org.gradle.kotlin.dsl.*
 import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
-import java.nio.file.Path
 
 plugins {
     kotlin("jvm")
     id("de.undercouch.download")
 }
 
-@Suppress("GENERIC_VARIABLE_WRONG_DECLARATION")
-val downloadSaveDemoAgentDistroTaskProvider: TaskProvider<Download> = tasks.register<Download>("downloadSaveDemoAgentDistro") {
-    enabled = findProperty("saveDemoAgentDistroFilepath") != null
-
-    val saveDemoAgentDistroFilepath = findProperty("saveDemoAgentDistroFilepath")?.toString() ?: "file://not-found"
-    src { saveDemoAgentDistroFilepath }
-    dest { "$buildDir/demoAgentDistro/${Path.of(saveDemoAgentDistroFilepath).fileName}" }
-    overwrite(false)
-}
+val downloadSaveDemoAgentDistroTaskProvider: TaskProvider<Download> = tasks.register(
+    name = "downloadSaveDemoAgentDistro",
+    configuration = downloadTaskConfiguration(
+        urlPropertyName = "saveDemoAgentDistroFileUrl",
+        targetDirectory = "demoAgentDistro",
+    ),
+)
 
 dependencies {
     if (DefaultNativePlatform.getCurrentOperatingSystem().isWindows) {
