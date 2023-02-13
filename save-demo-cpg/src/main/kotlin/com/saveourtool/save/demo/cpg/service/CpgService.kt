113267045aeef176fec476acb6a75ba3d9eef26b
Dmitry Morozovsky <27895587+icemachined@users.noreply.github.com>
2023-02-13 16:12:46 +0300

    adapt for new version of cpg

--- save-demo-cpg/src/main/kotlin/com/saveourtool/save/demo/cpg/service/CpgService.kt
+++ save-demo-cpg/src/main/kotlin/com/saveourtool/save/demo/cpg/service/CpgService.kt
@@ -3,6 +3,7 @@ package com.saveourtool.save.demo.cpg.service
 import com.saveourtool.save.demo.cpg.utils.LogbackCapturer
 import com.saveourtool.save.demo.cpg.utils.ResultWithLogs
 import de.fraunhofer.aisec.cpg.*
+import de.fraunhofer.aisec.cpg.frontends.Language
 import de.fraunhofer.aisec.cpg.frontends.python.PythonLanguageFrontend
 import org.springframework.stereotype.Service
 import java.nio.file.Path
@@ -31,13 +32,12 @@ class CpgService {
             .get()
     }
 
-    @OptIn(ExperimentalPython::class)
     private fun createTranslationConfiguration(folder: Path, applicationName: String): TranslationConfiguration = TranslationConfiguration.builder()
         .topLevel(null)
         // c++/java
         .defaultLanguages()
         // you can register non-default languages
-        .registerLanguage(PythonLanguageFrontend::class.java, listOf(".py"))
+        .registerLanguage<Language<PythonLanguageFrontend>>()
         .debugParser(true)
         // the directory with sources
         .softwareComponents(mutableMapOf(applicationName to listOf(folder.toFile())))
