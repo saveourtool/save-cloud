51dfb6a211494b257fc8eac1f96f289ff4bf367c
Nariman Abdullin <nulls.narik@gmail.com>
2023-03-06 14:55:33 +0300

    Dev infra cleanup (#1939)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,62 +7,12 @@ plugins {
     id("com.saveourtool.save.buildutils.code-quality-convention")
     id("com.saveourtool.save.buildutils.publishing-configuration")
     alias(libs.plugins.talaiot.base)
-    alias(libs.plugins.liquibase.gradle)
     java
 }
 
 val profile = properties.getOrDefault("save.profile", "dev") as String
 
-liquibase {
-    activities {
-        val commonArguments = mapOf(
-            "logLevel" to "info",
-            "contexts" to when (profile) {
-                "prod" -> "prod"
-                "dev" -> "dev"
-                else -> throw GradleException("Profile $profile not configured to map on a particular liquibase context")
-            }
-        )
-        // Configuring liquibase
-        register("main") {
-            arguments = mapOf("changeLogFile" to "db/db.changelog-master.xml") +
-                    getBackendDatabaseCredentials(profile).toLiquibaseArguments() +
-                    commonArguments
-        }
-        register("sandbox") {
-            arguments = mapOf(
-                "changeLogFile" to "save-sandbox/db/db.changelog-sandbox.xml",
-                "liquibaseSchemaName" to "save_sandbox",
-                "defaultSchemaName" to "save_sandbox",
-            ) +
-                    getSandboxDatabaseCredentials(profile).toLiquibaseArguments() +
-                    commonArguments
-        }
-        register("demo") {
-            arguments = mapOf(
-                "changeLogFile" to "save-demo/db/db.changelog-demo.xml",
-                "liquibaseSchemaName" to "save_demo",
-                "defaultSchemaName" to "save_demo",
-            ) +
-                    getDemoDatabaseCredentials(profile).toLiquibaseArguments() +
-                    commonArguments
-        }
-    }
-}
-
-dependencies {
-    liquibaseRuntime(libs.liquibase.core)
-    liquibaseRuntime(libs.mysql.connector.java)
-    liquibaseRuntime(libs.picocli)
-}
-
-tasks.withType<org.liquibase.gradle.LiquibaseTask>().configureEach {
-    @Suppress("MAGIC_NUMBER")
-    this.javaLauncher.set(project.extensions.getByType<JavaToolchainService>().launcherFor {
-        // liquibase-core 4.7.0 and liquibase-gradle 2.1.1 fails on Java >= 13 on Windows; works on Mac
-        languageVersion.set(JavaLanguageVersion.of(11))
-    })
-}
+registerLiquibaseTask(profile)
 
 talaiot {
     metrics {
