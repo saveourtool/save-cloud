8b0f8ddca98dd5cdd8bfa2aa0f8196bea63d9e1b
Nariman Abdullin <nulls.narik@gmail.com>
2023-03-07 11:24:11 +0300

    added repository for snapshot dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ dependencyResolutionManagement {
             url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
             content {
                 includeGroup("com.saveourtool.sarifutils")
+                includeGroup("com.saveourtool.save")
             }
         }
         mavenCentral()
