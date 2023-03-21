d23777d0c8421630a8d20a7e3de9c8169dead300
Vfrolov <vlad-20-11-94@yandex.ru>
2023-03-21 13:30:22 +0300

    Backend for update vulnerability
    * added methods for update vulnerability
    * added flag the admin vulnerability check flag
    * created and update date column in the vulnerability table
    
    Closes #2006

--- save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/listeners/DateListener.kt
+++ save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/listeners/DateListener.kt
@@ -1,6 +1,6 @@
 package com.saveourtool.save.listeners
 
-import com.saveourtool.save.entities.vulnerabilities.Vulnerability
+import com.saveourtool.save.spring.entity.BaseEntityWithDate
 import java.time.LocalDateTime
 import javax.persistence.PrePersist
 import javax.persistence.PreUpdate
@@ -8,14 +8,14 @@ import javax.persistence.PreUpdate
 @Suppress("MISSING_KDOC_TOP_LEVEL")
 class DateListener {
     @PrePersist
-    private fun beforeSave(entity: Vulnerability) {
+    private fun beforeSave(entity: BaseEntityWithDate) {
         with(entity) {
             createDate = LocalDateTime.now()
             updateDate = LocalDateTime.now()
         }
     }
     @PreUpdate
-    private fun beforeUpdate(entity: Vulnerability) {
+    private fun beforeUpdate(entity: BaseEntityWithDate) {
         with(entity) {
             updateDate = LocalDateTime.now()
         }
