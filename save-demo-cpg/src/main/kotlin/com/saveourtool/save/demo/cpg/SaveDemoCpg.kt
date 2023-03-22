c3025c5977008c5d9fa08fdf405bcee3885a77c2
Nariman Abdullin <nulls.narik@gmail.com>
2023-03-22 12:18:39 +0300

    Revert "Revert "Added diktat-merged.sarif explicitly""
    This reverts commit a41bec4899fb6198d4ac22bdbbe86a5e1a35a62d.

--- save-demo-cpg/src/main/kotlin/com/saveourtool/save/demo/cpg/SaveDemoCpg.kt
+++ save-demo-cpg/src/main/kotlin/com/saveourtool/save/demo/cpg/SaveDemoCpg.kt
@@ -4,6 +4,7 @@ import com.saveourtool.save.demo.cpg.config.ConfigProperties
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.http.ResponseEntity
 
 /**
  * An entrypoint for spring boot for save-demo
