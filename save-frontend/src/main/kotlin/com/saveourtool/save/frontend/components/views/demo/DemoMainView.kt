2ad70bd7d58da33fd46f98a210e8a1c9b500386e
sanyavertolet <lxnfrolov@gmail.com>
2023-03-29 19:21:26 +0300

    DemoMainView
    ### What's done:
    * Added view for demo browsing - an entry point to save-demo frontend
    * Removed buggy demo dropdown from topBarLinks
    * Added two pics for demoMainView
    
    (#2053)

---
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/demo/DemoMainView.kt
@@ -0,0 +1,48 @@
+/**
+ * Main view for Demos
+ */
+
+package com.saveourtool.save.frontend.components.views.demo
+
+import com.saveourtool.save.frontend.components.basic.demo.welcome.demoList
+import com.saveourtool.save.frontend.components.basic.demo.welcome.featuredDemos
+import com.saveourtool.save.frontend.components.basic.demo.welcome.introductionSection
+import com.saveourtool.save.frontend.components.basic.demo.welcome.statistics
+import com.saveourtool.save.frontend.utils.*
+import csstype.ClassName
+import react.VFC
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.main
+
+val demoMainView: VFC = VFC {
+    useBackground(Style.BLUE)
+
+    main {
+        className = ClassName("main-content mt-0 ps")
+        div {
+            className = ClassName("page-header align-items-start min-vh-100")
+            div {
+                className = ClassName("row justify-content-center")
+
+                div {
+                    className = ClassName("col-md-4")
+                    div {
+                        className = ClassName("mb-2")
+                        introductionSection()
+                    }
+                    div {
+                        className = ClassName("mb-2")
+                        featuredDemos()
+                    }
+                    div {
+                        statistics()
+                    }
+                }
+                div {
+                    className = ClassName("col-lg-7 d-flex align-items-stretch")
+                    demoList()
+                }
+            }
+        }
+    }
+}
