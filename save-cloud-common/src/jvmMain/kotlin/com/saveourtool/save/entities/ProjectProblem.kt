a61bda6f95dddbbf220bdb728a5bfcea6178fa54
Vfrolov <vlad-20-11-94@yandex.ru>
2023-04-13 17:07:32 +0300

    FossGraph. Added backend for new project problem view

--- save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/entities/ProjectProblem.kt
+++ save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/entities/ProjectProblem.kt
@@ -10,6 +10,7 @@ import javax.persistence.*
  * @property critical
  * @property vulnerability
  * @property project
+ * @property userId
  */
 @Entity
 class ProjectProblem(
@@ -29,11 +30,16 @@ class ProjectProblem(
     @JoinColumn(name = "project_id")
     var project: Project,
 
+    var userId: Long,
+
 ) : BaseEntityWithDateAndDto<ProjectProblemDto>() {
     override fun toDto() = ProjectProblemDto(
         name = name,
         description = description,
         critical = critical,
         vulnerabilityName = vulnerability?.name,
+        organizationName = project.organization.name,
+        projectName = project.name,
+        id = id,
     )
 }
