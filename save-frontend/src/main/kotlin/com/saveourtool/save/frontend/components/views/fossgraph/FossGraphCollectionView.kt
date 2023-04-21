39f4a35b534275af6ddf89a6f9a79ed7336b0f9e
Vladislav Frolov <50615459+Cheshiriks@users.noreply.github.com>
2023-04-21 17:44:57 +0300

    FossGraph. Improved Foss Graph view (#2099)
    * FossGraph. Improved Foss Graph view

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/fossgraph/FossGraphCollectionView.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/fossgraph/FossGraphCollectionView.kt
@@ -8,6 +8,7 @@ import com.saveourtool.save.domain.Role
 import com.saveourtool.save.entities.vulnerability.VulnerabilityDto
 import com.saveourtool.save.filters.VulnerabilityFilter
 import com.saveourtool.save.frontend.TabMenuBar
+import com.saveourtool.save.frontend.components.basic.fossGraphIntroductionComponent
 import com.saveourtool.save.frontend.components.basic.nameFiltersRow
 import com.saveourtool.save.frontend.components.tables.*
 import com.saveourtool.save.frontend.components.views.contests.tab
@@ -19,6 +20,7 @@ import com.saveourtool.save.validation.FrontendRoutes
 import csstype.ClassName
 import react.*
 import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.main
 import react.dom.html.ReactHTML.td
 import react.dom.html.ReactHTML.th
 import react.dom.html.ReactHTML.tr
@@ -32,7 +34,7 @@ import kotlinx.serialization.json.Json
  * [FC] for fossGraphCollection view
  */
 val fossGraphCollectionView: FC<FossGraphCollectionViewProps> = FC { props ->
-    useBackground(Style.WHITE)
+    useBackground(Style.BLUE)
     val navigate = useNavigate()
 
     val (vulnerabilityFilters, setVulnerabilityFilters) = useState(VulnerabilityFilter.created)
@@ -67,6 +69,7 @@ val fossGraphCollectionView: FC<FossGraphCollectionViewProps> = FC { props ->
         initialPageSize = 10,
         useServerPaging = false,
         usePageSelection = false,
+        isTransparentGrid = true,
         getAdditionalDependencies = {
             arrayOf(it.filters)
         },
@@ -97,46 +100,83 @@ val fossGraphCollectionView: FC<FossGraphCollectionViewProps> = FC { props ->
         }
     )
 
-    div {
-        className = ClassName("d-flex justify-content-center mb-2")
-        withNavigate { navigateContext ->
-            buttonBuilder(label = "Propose a vulnerability", style = "info") {
-                navigateContext.navigate("/${FrontendRoutes.CREATE_VULNERABILITY}")
-            }
-        }
-    }
-
-    div {
-        @Suppress("TOO_MANY_LINES_IN_LAMBDA")
-        props.currentUserInfo?.globalRole?.let { role ->
-            val tabList = if (role.isHigherOrEqualThan(Role.SUPER_ADMIN)) {
-                VulnerabilityListTab.values().map { it.name }
-            } else {
-                VulnerabilityListTab.values().filter { it != VulnerabilityListTab.ADMIN }.map { it.name }
-            }
-            tab(selectedMenu.name, tabList, "nav nav-tabs mt-3") { value ->
-                setSelectedMenu { VulnerabilityListTab.valueOf(value) }
-                setVulnerabilityFilters {
-                    when (VulnerabilityListTab.valueOf(value)) {
-                        VulnerabilityListTab.PUBLIC -> VulnerabilityFilter(prefixName = "", active = true)
-                        VulnerabilityListTab.ADMIN -> VulnerabilityFilter(prefixName = "", active = false)
-                        VulnerabilityListTab.OWNER -> VulnerabilityFilter(prefixName = "", active = false, isOwner = true)
+    main {
+        className = ClassName("main-content mt-0 ps")
+        div {
+            className = ClassName("page-header align-items-start min-vh-100")
+            div {
+                className = ClassName("d-flex justify-content-center")
+                div {
+                    className = ClassName("col-md-4 d-flex align-items-stretch")
+                    div {
+                        div {
+                            className = ClassName("mb-2")
+                            fossGraphIntroductionComponent()
+                        }
                     }
                 }
-            }
-        }
+                div {
+                    className = ClassName("card flex-md-row col-lg-7 d-flex align-items-stretch")
+                    div {
+                        className = ClassName("col")
+                        div {
+                            className = ClassName("d-flex justify-content-center mb-1 mt-2")
+                            withNavigate { navigateContext ->
+                                buttonBuilder(label = "Propose a new vulnerability", style = "info") {
+                                    navigateContext.navigate("/${FrontendRoutes.CREATE_VULNERABILITY}")
+                                }
+                            }
+                        }
 
-        fossGraphTable {
-            filters = vulnerabilityFilters
-            getData = { _, _ ->
-                post(
-                    url = "$apiUrl/vulnerabilities/by-filters",
-                    headers = jsonHeaders,
-                    body = Json.encodeToString(vulnerabilityFilters),
-                    loadingHandler = ::noopLoadingHandler,
-                    responseHandler = ::noopResponseHandler,
-                ).unsafeMap {
-                    it.decodeFromJsonString()
+                        div {
+                            @Suppress("TOO_MANY_LINES_IN_LAMBDA")
+                            props.currentUserInfo?.globalRole?.let { role ->
+                                val tabList = if (role.isHigherOrEqualThan(Role.SUPER_ADMIN)) {
+                                    VulnerabilityListTab.values().map { it.name }
+                                } else {
+                                    VulnerabilityListTab.values().filter { it != VulnerabilityListTab.ADMIN }
+                                        .map { it.name }
+                                }
+                                tab(selectedMenu.name, tabList, "nav nav-tabs mt-3") { value ->
+                                    setSelectedMenu { VulnerabilityListTab.valueOf(value) }
+                                    setVulnerabilityFilters {
+                                        when (VulnerabilityListTab.valueOf(value)) {
+                                            VulnerabilityListTab.PUBLIC -> VulnerabilityFilter(
+                                                prefixName = "",
+                                                active = true
+                                            )
+
+                                            VulnerabilityListTab.ADMIN -> VulnerabilityFilter(
+                                                prefixName = "",
+                                                active = false
+                                            )
+
+                                            VulnerabilityListTab.OWNER -> VulnerabilityFilter(
+                                                prefixName = "",
+                                                active = false,
+                                                isOwner = true
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+
+                            fossGraphTable {
+                                filters = vulnerabilityFilters
+                                getData = { _, _ ->
+                                    post(
+                                        url = "$apiUrl/vulnerabilities/by-filters",
+                                        headers = jsonHeaders,
+                                        body = Json.encodeToString(vulnerabilityFilters),
+                                        loadingHandler = ::noopLoadingHandler,
+                                        responseHandler = ::noopResponseHandler,
+                                    ).unsafeMap {
+                                        it.decodeFromJsonString()
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
