1875425eec1a1421ac3fedd353175378b48034c2
Nariman Abdullin <nulls.narik@gmail.com>
2023-04-24 11:45:45 +0300

    supported reckon changes

--- gradle/plugins/src/main/kotlin/com/saveourtool/save/buildutils/VersioningConfiguration.kt
+++ gradle/plugins/src/main/kotlin/com/saveourtool/save/buildutils/VersioningConfiguration.kt
@@ -4,10 +4,12 @@
 
 package com.saveourtool.save.buildutils
 
-import org.ajoberstar.grgit.gradle.GrgitServiceExtension
-import org.ajoberstar.grgit.gradle.GrgitServicePlugin
+import org.ajoberstar.reckon.core.Scope
 import org.ajoberstar.reckon.gradle.ReckonExtension
 import org.ajoberstar.reckon.gradle.ReckonPlugin
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.internal.storage.file.FileRepository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.*
 
@@ -16,27 +18,27 @@ import org.gradle.kotlin.dsl.*
  */
 fun Project.configureVersioning() {
     apply<ReckonPlugin>()
-    apply<GrgitServicePlugin>()
-    val grgitProvider = project.extensions.getByType<GrgitServiceExtension>()
-        .service
-        .map { it.grgit }
 
     // should be provided in the gradle.properties
     val isDevelopmentVersion = hasProperty("save.profile") && property("save.profile") == "dev"
     configure<ReckonExtension> {
-        scopeFromProp()
+        setDefaultInferredScope(Scope.MINOR.name)
+        setScopeCalc(calcScopeFromProp())
         if (isDevelopmentVersion) {
             // this should be used during local development most of the time, so that constantly changing version
             // on a dirty git tree doesn't cause other task updates
-            snapshotFromProp()
+            snapshots()
+            setStageCalc(calcStageFromProp())
         } else {
-            stageFromProp("alpha", "rc", "final")
+            stages("alpha", "rc", "final")
+            setStageCalc(calcStageFromProp())
         }
     }
 
-    val grgit = grgitProvider.get()
-    val status = grgit.repository.jgit.status()
+    val status = Git(FileRepository(FileRepositoryBuilder().findGitDir(project.rootDir).setup()))
+        .status()
         .call()
+
     if (!status.isClean) {
         logger.warn("git tree is not clean; " +
                 "Untracked files: ${status.untracked}, uncommitted changes: ${status.uncommittedChanges}"
