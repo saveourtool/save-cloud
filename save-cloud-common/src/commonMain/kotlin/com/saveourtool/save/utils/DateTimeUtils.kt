8a5204fd6b24f80c5c053ea1bcc1124234cd7e13
Vfrolov <vlad-20-11-94@yandex.ru>
2023-05-05 14:54:15 +0300

    Added comment creation date

--- save-cloud-common/src/commonMain/kotlin/com/saveourtool/save/utils/DateTimeUtils.kt
+++ save-cloud-common/src/commonMain/kotlin/com/saveourtool/save/utils/DateTimeUtils.kt
@@ -30,11 +30,12 @@ fun LocalDateTime.prettyPrint(timeZone: TimeZone = TimeZone.UTC) = toInstant(Tim
 
 /**
  * @param timeZone timezone to print the date time in
- * @return very pretty string representation of [LocalDateTime]
+ * @return string representation of [LocalDateTime] in [Thursday, 1 Jan 1970 00:00] format
  */
-fun LocalDateTime.veryPrettyPrint(timeZone: TimeZone = TimeZone.UTC) = toInstant(TimeZone.UTC).toLocalDateTime(timeZone)
+fun LocalDateTime.toUnixCalendarFormat(timeZone: TimeZone = TimeZone.UTC) = toInstant(TimeZone.UTC).toLocalDateTime(timeZone)
     .let {
-        "${it.dayOfWeek.name.toLowerCaseWithFirstCharUpperCase()}, ${it.dayOfMonth} ${it.month.name.toLowerCaseWithFirstCharUpperCase()} ${it.year} ${it.hour}:${it.minute}"
+        "${it.dayOfWeek.name.toLowerCaseWithFirstCharUpperCase()}, ${it.dayOfMonth} ${it.month.name.toLowerCaseWithFirstCharUpperCase()} ${it.year} ${it.hour.plusZero()}:${it
+            .minute.plusZero()}"
     }
 
 /**
@@ -46,6 +47,9 @@ operator fun LocalDateTime.plus(duration: Duration) = toInstant(TimeZone.UTC).pl
  * @param duration
  */
 operator fun LocalDateTime.minus(duration: Duration) = toInstant(TimeZone.UTC).minus(duration).toLocalDateTime(TimeZone.UTC)
+
+@Suppress("MAGIC_NUMBER")
+private fun Int.plusZero(): String = this.let { if (it < 10) "0$it" else it }.toString()
 
 private fun String.toLowerCaseWithFirstCharUpperCase() = this.toLowerCasePreservingASCIIRules().replaceFirstChar { char -> char.titlecase() }
 
