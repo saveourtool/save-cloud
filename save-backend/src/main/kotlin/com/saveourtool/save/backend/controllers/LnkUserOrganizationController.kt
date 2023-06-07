d314e40710146762ca04cb5ace654a49e760fdbe
sanyavertolet <lxnfrolov@gmail.com>
2023-06-07 12:16:08 +0300

    OrganizationView unauthorized fix
    ### What's done:
    * Fixed `/api/$v1/organizations/{organizationName}/users` to expect nullable `authentication`

--- save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/LnkUserOrganizationController.kt
+++ save-backend/src/main/kotlin/com/saveourtool/save/backend/controllers/LnkUserOrganizationController.kt
@@ -60,8 +60,6 @@ class LnkUserOrganizationController(
     private val organizationPermissionEvaluator: OrganizationPermissionEvaluator,
 ) {
     @GetMapping("/{organizationName}/users")
-    @RequiresAuthorizationSourceHeader
-    @PreAuthorize("permitAll()")
     @Operation(
         method = "GET",
         summary = "Get list of users that are connected with given organization.",
@@ -74,7 +72,7 @@ class LnkUserOrganizationController(
     @ApiResponse(responseCode = "404", description = "Contest with such name was not found.")
     fun getAllUsersByOrganizationName(
         @PathVariable organizationName: String,
-        authentication: Authentication,
+        authentication: Authentication?,
     ): Mono<List<UserInfo>> = organizationService.findByNameAndCreatedStatus(organizationName)
         .toMono()
         .switchIfEmptyToNotFound {
