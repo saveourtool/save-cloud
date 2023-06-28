2a067cd4d10a345295f7f7df7a1fd300a3dc85de
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2023-06-28 20:07:26 +0300

    Hotfix with a sanity test for api-gateway (#2251)
    ### What's done:
    
    - fixed gateway typo
    - due to the change in snakeyaml API (2.0), api-gateway is not able to start: Caused by: java.lang.NoSuchMethodError: org.yaml.snakeyaml.constructor.SafeConstructor: method 'void <init>()' not found

---
+++ api-gateway/src/test/kotlin/com/saveourtool/save/gateway/StartupTest.kt
@@ -0,0 +1,16 @@
+package com.saveourtool.save.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+
+
+@SpringBootTest
+@SpringJUnitConfig
+class StartupTest {
+    // This test method will be executed to check the application startup
+    // If the application starts successfully, the test will pass
+    // If there are any exceptions or failures during startup, the test will fail
+    @Test
+    fun contextLoads() = Unit
+}
