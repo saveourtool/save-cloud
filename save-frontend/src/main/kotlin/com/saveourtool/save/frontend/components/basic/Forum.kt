4fbba382b6c9eff32dad6f2ae0244ed0d9c3d842
Cheshiriks <vlad-20-11-94@yandex.ru>
2023-06-28 23:44:08 +0300

    Added avatar for new comment block

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/Forum.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/Forum.kt
@@ -6,10 +6,12 @@ import com.saveourtool.save.entities.CommentDto
 import com.saveourtool.save.frontend.components.inputform.InputTypes
 import com.saveourtool.save.frontend.utils.*
 import com.saveourtool.save.frontend.utils.AVATAR_PLACEHOLDER
+import com.saveourtool.save.info.UserInfo
 import com.saveourtool.save.utils.toUnixCalendarFormat
 import com.saveourtool.save.v1
 
 import js.core.jso
+import react.ChildrenBuilder
 import react.FC
 import react.PropsWithChildren
 import react.dom.aria.ariaDescribedBy
@@ -35,6 +37,7 @@ import kotlinx.serialization.json.Json
 )
 val newCommentWindow: FC<NewCommentWindowProps> = FC { props ->
     val (comment, setComment) = useState(CommentDto.empty)
+    val (user, setUser) = useStateFromProps(props.currentUserInfo ?: UserInfo("Unknown"))
 
     val enrollRequest = useDeferredRequest {
         val commentNew = comment.copy(section = window.location.hash)
@@ -51,16 +54,28 @@ val newCommentWindow: FC<NewCommentWindowProps> = FC { props ->
     }
 
     div {
-        className = ClassName("input-group needs-validation")
-        textarea {
-            className = ClassName("form-control")
-            onChange = { event ->
-                setComment { it.copy(message = event.target.value) }
+        className = ClassName("row no-gutters mx-auto border-secondary")
+        renderLeftColumn(
+            user.avatar,
+            user.name,
+            user.rating,
+        )
+        div {
+            className = ClassName("card col-10 input-group needs-validation")
+            textarea {
+                className = ClassName("form-control")
+                style = jso {
+                    width = "100%".unsafeCast<Width>()
+                    height = "100%".unsafeCast<Height>()
+                }
+                onChange = { event ->
+                    setComment { it.copy(message = event.target.value) }
+                }
+                ariaDescribedBy = "${InputTypes.COMMENT.name}Span"
+                rows = 5
+                id = InputTypes.COMMENT.name
+                required = true
             }
-            ariaDescribedBy = "${InputTypes.COMMENT.name}Span"
-            rows = 5
-            id = InputTypes.COMMENT.name
-            required = true
         }
     }
     div {
@@ -81,56 +96,14 @@ val newCommentWindow: FC<NewCommentWindowProps> = FC { props ->
 val commentWindow: FC<CommentWindowProps> = FC { props ->
 
     val columnCard = cardComponent(isBordered = false, hasBg = true, isNoPadding = false, isPaddingBottomNull = true, isFilling = true)
-    val (avatar, setAvatar) = useState(props.comment.userAvatar?.let { "/api/$v1/avatar$it" } ?: "img/undraw_profile.svg")
 
     div {
         className = ClassName("row no-gutters mx-auto border-secondary")
-        div {
-            className = ClassName("card card-body col-2 border-secondary")
-            style = jso {
-                background = "#e1e9ed".unsafeCast<Background>()
-            }
-            div {
-                className = ClassName("mb-0 font-weight-bold text-gray-800")
-                form {
-                    div {
-                        className = ClassName("row justify-content-center g-3 ml-3 mr-3 pb-2 pt-2 border-bottom")
-                        div {
-                            className = ClassName("md-4 pl-0 pr-0")
-                            img {
-                                className =
-                                        ClassName("avatar avatar-user width-full border color-bg-default rounded-circle")
-                                src = avatar
-                                height = 80.0
-                                width = 80.0
-                                onError = {
-                                    setAvatar(AVATAR_PLACEHOLDER)
-                                }
-                            }
-                        }
-                        div {
-                            className = ClassName("row mt-2 md-6 pl-0")
-                            style = jso {
-                                display = Display.flex
-                                alignItems = AlignItems.center
-                            }
-                            div {
-                                className = ClassName("col text-center text-xs font-weight-bold text-info text-uppercase")
-                                +"Rating"
-                            }
-                            div {
-                                className = ClassName("col text-center")
-                                +props.comment.userRating.toString()
-                            }
-                            h1 {
-                                className = ClassName("col text-center font-weight-bold h4")
-                                +props.comment.userName
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        renderLeftColumn(
+            props.comment.userAvatar,
+            props.comment.userName,
+            props.comment.userRating,
+        )
         div {
             className = ClassName("card col-10 text-left border-secondary")
             val comment = props.comment
@@ -166,4 +139,67 @@ external interface NewCommentWindowProps : PropsWithChildren {
      * Callback invoked when added new comment
      */
     var addComment: () -> Unit
+
+    /**
+     * Information about current user
+     */
+    var currentUserInfo: UserInfo?
+}
+
+@Suppress(
+    "MAGIC_NUMBER",
+)
+private fun ChildrenBuilder.renderLeftColumn(
+    userAvatar: String?,
+    name: String,
+    rating: Long,
+) {
+    val (avatar, setAvatar) = useState(userAvatar?.let { "/api/$v1/avatar$it" } ?: "img/undraw_profile.svg")
+
+    div {
+        className = ClassName("card card-body col-2 border-secondary")
+        style = jso {
+            background = "#e1e9ed".unsafeCast<Background>()
+        }
+        div {
+            className = ClassName("mb-0 font-weight-bold text-gray-800")
+            form {
+                div {
+                    className = ClassName("row justify-content-center g-3 ml-3 mr-3 pb-2 pt-2 border-bottom")
+                    div {
+                        className = ClassName("md-4 pl-0 pr-0")
+                        img {
+                            className =
+                                    ClassName("avatar avatar-user width-full border color-bg-default rounded-circle")
+                            src = avatar
+                            height = 80.0
+                            width = 80.0
+                            onError = {
+                                setAvatar(AVATAR_PLACEHOLDER)
+                            }
+                        }
+                    }
+                    div {
+                        className = ClassName("row mt-2 md-6 pl-0")
+                        style = jso {
+                            display = Display.flex
+                            alignItems = AlignItems.center
+                        }
+                        div {
+                            className = ClassName("col-12 text-center text-xs font-weight-bold text-info text-uppercase")
+                            +"Rating"
+                        }
+                        div {
+                            className = ClassName("col-12 text-center text-xs")
+                            +rating.toString()
+                        }
+                        h1 {
+                            className = ClassName("col-12 text-center font-weight-bold h5")
+                            +name
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
