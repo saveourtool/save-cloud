ebac5b1bf4cff5657f3b3b1e1f05139fa22666b7
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2023-06-28 19:34:10 +0300

    Hotfix with a sanity test for api-gateway
    ### What's done:
    - fixed gateway typo
    - made strict version for snakeyaml

--- api-gateway/src/test/kotlin/com/saveourtool/save/gateway/StartupTest.kt
+++ api-gateway/src/test/kotlin/com/saveourtool/save/gateway/StartupTest.kt
@@ -8,11 +8,9 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 @SpringBootTest
 @SpringJUnitConfig
 class StartupTest {
+    // This test method will be executed to check the application startup
+    // If the application starts successfully, the test will pass
+    // If there are any exceptions or failures during startup, the test will fail
     @Test
-    fun contextLoads() {
-        // This test method will be executed to check the application startup
-        // If the application starts successfully, the test will pass
-        // If there are any exceptions or failures during startup, the test will fail
-    }
+    fun contextLoads() = Unit
 }
-
