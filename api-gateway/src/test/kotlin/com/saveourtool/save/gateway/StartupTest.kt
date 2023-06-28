1c9e106d404f55f504d1f45c25dc06eb0fa42e49
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2023-06-28 17:23:45 +0300

    Hotfix with a sanity test for api-gateway
    ### What's done:
    - fixed gateway typo

---
+++ api-gateway/src/test/kotlin/com/saveourtool/save/gateway/StartupTest.kt
@@ -0,0 +1,18 @@
+package com.saveourtool.save.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+
+
+@SpringBootTest
+@SpringJUnitConfig
+class ApplicationStartupTest {
+    @Test
+    fun contextLoads() {
+        // This test method will be executed to check the application startup
+        // If the application starts successfully, the test will pass
+        // If there are any exceptions or failures during startup, the test will fail
+    }
+}
+
