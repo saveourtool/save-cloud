e451d338a575e3bb96bdb1032d7d3506026fe290
Vfrolov <vlad-20-11-94@yandex.ru>
2023-07-03 16:47:22 +0300

    Added link users to vulnerability

--- save-backend/src/test/kotlin/com/saveourtool/save/backend/controller/DeleteEntitiesTest.kt
+++ save-backend/src/test/kotlin/com/saveourtool/save/backend/controller/DeleteEntitiesTest.kt
@@ -42,6 +42,7 @@ import java.util.Optional
 @ExtendWith(InfraExtension::class)
 @MockBeans(
 )
+@Suppress("UnusedPrivateProperty")
 class DeleteEntitiesTest {
     @Autowired
     lateinit var webClient: WebTestClient
