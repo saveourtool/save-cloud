080232e25a5b5cf518ef958b213d978d1af41251
sanyavertolet <lxnfrolov@gmail.com>
2023-07-03 15:04:54 +0400

    vulnerabilityInfoTab should rely on currentUserInfo
    ### What's done:
     * Implemented passing currentUserInfo to vulnerabilityInfoTab using props
     * Hid several buttons if user is unauthorized

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/fossgraph/FossGraphView.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/fossgraph/FossGraphView.kt
@@ -213,6 +213,7 @@ val fossGraph: FC<FossGraphViewProps> = FC { props ->
                     when (selectedMenu) {
                         VulnerabilityTab.INFO -> vulnerabilityInfoTab {
                             this.vulnerability = vulnerability
+                            this.currentUserInfo = props.currentUserInfo
                         }
                         VulnerabilityTab.COMMENTS -> vulnerabilityCommentTab {
                             this.vulnerability = vulnerability
