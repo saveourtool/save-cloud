2f1a79778e5008aac0dd15b105b40965a9e59041
sanyavertolet <lxnfrolov@gmail.com>
2023-07-03 18:02:25 +0400

    timelineComponent
    ### What's done:
     * Polished vulnerabilityBadge component
     * Added stylesheets for timelineComponent
     * Implemented timelineComponent
     * Used timelineComponent on fossGraphView

---
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/TimelineComponent.kt
@@ -0,0 +1,60 @@
+@file:Suppress("FILE_NAME_INCORRECT", "FILE_NAME_MATCH_CLASS")
+
+package com.saveourtool.save.frontend.components.basic
+
+import react.*
+import react.dom.html.ReactHTML.div
+import web.cssom.*
+
+import kotlinx.datetime.LocalDateTime
+
+val timelineComponent: FC<TimelineComponentProps> = FC { props ->
+    div {
+        className = ClassName("mb-3")
+        div {
+            className = ClassName("mt-3 mb-3 text-xs text-center font-weight-bold text-primary text-uppercase")
+            +"Timeline"
+        }
+        div {
+            className = ClassName("card card-body p-0 timeline-container")
+            div {
+                className = ClassName("steps-container")
+                div {
+                    className = ClassName("line completed")
+                }
+                props.dates
+                    .map { (dateTime, label) -> dateTime.date to label }
+                    .sortedBy { it.first }
+                    .forEach { (date, label) ->
+                        div {
+                            className = ClassName("step completed")
+                            div {
+                                className = ClassName("text-label completed")
+                                +label
+                            }
+                            div {
+                                className = ClassName("date-label completed")
+                                +date.toString()
+                            }
+                        }
+                        div {
+                            className = ClassName("line completed")
+                        }
+                    }
+                div {
+                    className = ClassName("line-end")
+                }
+            }
+        }
+    }
+}
+
+/**
+ * [Props] of [timelineComponent]
+ */
+external interface TimelineComponentProps : Props {
+    /**
+     * Map with dates where key is [LocalDateTime] and value is label
+     */
+    var dates: Map<LocalDateTime, String>
+}
