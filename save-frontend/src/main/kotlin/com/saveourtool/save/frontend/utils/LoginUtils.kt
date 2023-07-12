6d4c90844633a1062cbe391c1c75ada97c2a2422
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2023-07-12 23:47:03 +0300

    Adding new authorization providers
    ### What's done:
    - testing experiments with google, huawei and gitee
    - dev configuration for oauth providers (no prod keys)
    - frontend changes to support it

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/utils/LoginUtils.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/utils/LoginUtils.kt
@@ -1,3 +1,9 @@
+/**
+ * Utilities for visualization of auth providers on welcome and index pages
+ */
+
+@file:Suppress("FILE_NAME_MATCH_CLASS")
+
 package com.saveourtool.save.frontend.utils
 
 import com.saveourtool.save.frontend.externals.fontawesome.*
@@ -10,68 +16,69 @@ import react.dom.html.ReactHTML.img
 import web.cssom.*
 
 /**
- * @param size font size of oauth logos
- * @param provider oauth provider (Huawei, Gitee, Github, etc.)
- * @param icon icon logo
- * @param animate
- * @param label
- * @return
+ * Configruation for setting up oauth provider buttons on the frontend
+ *
+ * @property size font size of oauth logos
+ * @property provider oauth provider (Huawei, Gitee, Github, etc.)
+ * @property animate animation for these logos
+ * @property label text for logo
  */
-fun ChildrenBuilder.processRegistrationId(
-    size: FontSize,
-    provider: OauthProviderInfo,
-    animate: String,
-    label: String = ""
-) = oauthLoginForKnownAwesomeIcons(
-    size,
-    provider,
-    animate,
-    label,
-    mapUnknownIcons(provider.registrationId),
-    mapKnownIcons(provider.registrationId)
+data class OauthProvidersFeConfig(
+    val size: FontSize,
+    val provider: OauthProviderInfo,
+    val animate: String,
+    val label: String = ""
 )
 
 /**
- * @param size font size of oauth logos
- * @param provider oauth provider (Huawei, Gitee, Github, etc.)
- * @param icon icon logo
- * @param animate
- * @param label
+ * @param oauthProvidersFeConfig configuration for the frontend
+ * @return html block with logo of the provider
+ */
+fun ChildrenBuilder.processRegistrationId(
+    oauthProvidersFeConfig: OauthProvidersFeConfig
+) = oauthLoginForKnownAwesomeIcons(
+    oauthProvidersFeConfig,
+    mapUnknownIcons(oauthProvidersFeConfig.provider.registrationId),
+    mapKnownIcons(oauthProvidersFeConfig.provider.registrationId)
+)
+
+/**
+ * @param oauthProvidersFeConfig all configurations
+ * @param staticSvg
+ * @param awesomeIcon known icon implemented in awesomeIcon
  */
 private fun ChildrenBuilder.oauthLoginForKnownAwesomeIcons(
-    size: FontSize,
-    provider: OauthProviderInfo,
-    animate: String,
-    label: String = "",
+    oauthProvidersFeConfig: OauthProvidersFeConfig,
     staticSvg: String = "",
-    icon: dynamic
+    awesomeIcon: dynamic
 ) {
     div {
-        className = ClassName("animated-provider col animate__animated $animate")
+        className = ClassName("animated-provider col animate__animated ${oauthProvidersFeConfig.animate}")
         a {
-            href = provider.authorizationLink
+            href = oauthProvidersFeConfig.provider.authorizationLink
             className = ClassName("text-center")
             div {
                 className = ClassName("col text-center")
                 style = jso {
-                    fontSize = size
+                    fontSize = oauthProvidersFeConfig.size
                 }
                 // if there is no fontAwesome for this brand we use simple static SVG
                 if (staticSvg.isEmpty()) {
-                    fontAwesomeIcon(icon)
+                    fontAwesomeIcon(awesomeIcon)
                 } else {
                     img {
                         src = staticSvg
                         style = jso {
-                            width = size.unsafeCast<Width>()
-                            height = size.unsafeCast<Height>()
+                            width = oauthProvidersFeConfig.size.unsafeCast<Width>()
+                            height = oauthProvidersFeConfig.size.unsafeCast<Height>()
+                            cursor = "pointer".unsafeCast<Cursor>()
                         }
                     }
                 }
             }
             div {
                 className = ClassName("col text text-center")
-                +label
+                +oauthProvidersFeConfig.label
             }
         }
     }
