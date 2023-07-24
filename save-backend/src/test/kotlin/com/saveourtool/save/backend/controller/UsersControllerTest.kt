b38cbcb3a6446e258608182c66c60ddc405e1026
Nariman Abdullin <nulls.narik@gmail.com>
2023-07-24 16:13:53 +0300

    removed old tests

--- save-backend/src/test/kotlin/com/saveourtool/save/backend/controller/UsersControllerTest.kt
+++ save-backend/src/test/kotlin/com/saveourtool/save/backend/controller/UsersControllerTest.kt
@@ -2,11 +2,7 @@ package com.saveourtool.save.backend.controller
 
 import com.saveourtool.save.backend.SaveApplication
 import com.saveourtool.save.backend.utils.InfraExtension
-import com.saveourtool.save.domain.Role
-import com.saveourtool.save.entities.OriginalLogin
-import com.saveourtool.save.entities.User
 import com.saveourtool.save.info.UserInfo
-import com.saveourtool.save.info.UserStatus
 import com.saveourtool.save.v1
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -14,8 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.boot.test.mock.mockito.MockBeans
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -30,40 +24,23 @@ class UsersControllerTest {
 
     @Test
     @WithMockUser
-    fun `serialize and deserialize user`() {
-        val user = User(
-            "admin2",
-            null,
-            Role.VIEWER.asSpringSecurityRole(),
-            "basic2",
-            null,
-            status = UserStatus.CREATED,
-        ).apply { id = 99 }
-
-        val originalLogin = OriginalLogin(
-            "admin",
-            user,
-            "basic"
-        ).apply { id = 4 }
-        user.apply { originalLogins = listOf(originalLogin) }
-
+    fun `new user`() {
         webTestClient.post()
-            .uri("/internal/users/new")
+            .uri("/internal/users/new/basic/admin")
             .contentType(MediaType.APPLICATION_JSON)
-            .bodyValue(user)
             .exchange()
             .expectStatus()
             .isOk
 
         webTestClient.get()
-            .uri("/api/$v1/users/admin2")
+            .uri("/api/$v1/users/admin")
             .exchange()
             .expectStatus()
             .isOk
             .expectBody<UserInfo>()
             .consumeWith {
                 requireNotNull(it.responseBody)
-                Assertions.assertEquals("admin2", it.responseBody!!.name)
+                Assertions.assertEquals("admin", it.responseBody!!.name)
             }
     }
 }
