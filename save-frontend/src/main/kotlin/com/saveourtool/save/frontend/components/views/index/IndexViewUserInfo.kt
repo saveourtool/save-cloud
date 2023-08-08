ce466f3cdd16224a87bb8373b71efbfe857d5042
Vfrolov <vlad-20-11-94@yandex.ru>
2023-08-08 18:48:47 +0300

    Added block for notifications

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/index/IndexViewUserInfo.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/index/IndexViewUserInfo.kt
@@ -7,7 +7,6 @@ package com.saveourtool.save.frontend.components.views.index
 import js.core.jso
 import react.ChildrenBuilder
 import react.FC
-import react.dom.html.ReactHTML
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h2
 import react.dom.html.ReactHTML.h4
