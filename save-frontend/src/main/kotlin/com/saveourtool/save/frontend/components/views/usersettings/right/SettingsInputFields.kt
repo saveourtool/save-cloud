ff8e91b0e87939913c823d24a92560ca3332df69
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2023-08-13 22:58:25 +0300

    Hotfixes for profile and settings views
    ### What's done:
    - Fixed incorrect fill of UserInfo (twitter/website) fields
    - Fixed trimming and showing of urls on Profile view
    - Removed legacy user settings

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/usersettings/right/SettingsInputFields.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/views/usersettings/right/SettingsInputFields.kt
@@ -111,7 +111,7 @@ data class SettingsInputFields(
             linkedin = this.linkedIn.value?.trim() ?: userInfo.linkedin,
             gitHub = this.github.value?.trim() ?: userInfo.gitHub,
             twitter = this.twitter.value?.trim() ?: userInfo.twitter,
-            website = this.website.value?.trim() ?: userInfo.twitter,
+            website = this.website.value?.trim() ?: userInfo.website,
             realName = this.realName.value?.trim() ?: userInfo.realName,
             freeText = this.freeText.value?.trim() ?: userInfo.freeText,
         )
---
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/utils/UsefulUrls.kt
@@ -0,0 +1,11 @@
+package com.saveourtool.save.frontend.utils
+
+enum class UsefulUrls(val value: String) {
+    XCOM("https://x.com/"),
+    TWITTER("https://twitter.com/"),
+    LINKEDIN("https://linkedin.com/"),
+    GITHUB("https://github.com/"),
+    GITEE("https://gitee.com/"),
+    HTTP("http://"),
+    HTTPS("https://")
+}
