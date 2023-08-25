7a042e5dd8a42d0af71ac2dbda07b80aa788954f
Nariman Abdullin <nulls.narik@gmail.com>
2023-08-25 18:56:22 +0300

    migrated to latest cosv

--- save-osv/src/main/kotlin/com/saveourtool/save/osv/processor/AbstractOsvProcessor.kt
+++ save-osv/src/main/kotlin/com/saveourtool/save/osv/processor/AbstractOsvProcessor.kt
@@ -5,6 +5,8 @@ import com.saveourtool.save.info.UserInfo
 import com.saveourtool.save.osv.storage.OsvStorage
 
 import com.saveourtool.osv4k.OsvSchema
+import com.saveourtool.osv4k.TimeLineEntry
+import com.saveourtool.osv4k.TimeLineEntryType
 import reactor.core.publisher.Mono
 
 import kotlinx.datetime.LocalDateTime
@@ -36,17 +38,18 @@ abstract class AbstractOsvProcessor<D : Any, A_E : Any, A_D : Any, A_R_D : Any>(
 
     private fun <T : AnyOsvSchema> createFromCoreFields(osv: T): VulnerabilityDto = VulnerabilityDto(
         name = osv.id,
-        vulnerabilityIdentifier = osv.id,
-        progress = 0,
-        projects = emptyList(),
+        vulnerabilityIdentifier = osv.id, // should be replaced by alias
+        progress = 0, // TODO: it can be presented in two ways cvss v3 and cvss v2
+        projects = emptyList(), // TODO: need to refactor VulnerabilityProjectDto, COSV is basic
         description = osv.details,
         shortDescription = osv.summary.orEmpty(),
         relatedLink = null,
-        language = VulnerabilityLanguage.OTHER,
+        language = VulnerabilityLanguage.OTHER, // it seems to be removed, since language is invalid here and valid on package level (affected)
         userInfo = UserInfo(name = ""),  // will be set on saving to database
         organization = null,
         dates = buildList {
-            add(osv.modified.asVulnerabilityDateDto(VulnerabilityDateType.CVE_UPDATED))
+            osv.timeLine?.map { it.asVulnerabilityDateDto() }?.let { addAll(it) }
+            add(osv.modified.asVulnerabilityDateDto(VulnerabilityDateType.CVE_UPDATED)) // TODO: do we need it?
             osv.published?.asVulnerabilityDateDto(VulnerabilityDateType.INTRODUCED)?.run { add(this) }
             osv.withdrawn?.asVulnerabilityDateDto(VulnerabilityDateType.FIXED)?.run { add(this) }
         },
@@ -59,7 +62,18 @@ abstract class AbstractOsvProcessor<D : Any, A_E : Any, A_D : Any, A_R_D : Any>(
         private fun LocalDateTime.asVulnerabilityDateDto(type: VulnerabilityDateType) = VulnerabilityDateDto(
             date = this,
             type = type,
-            vulnerabilityName = "NOT_USED_ON_SAVE",
+            vulnerabilityName = "NOT_USED_WHEN_SAVING_IN_DATABASE",
+        )
+
+        private fun TimeLineEntry.asVulnerabilityDateDto() = VulnerabilityDateDto(
+            date = value,
+            type = when (type) {
+                TimeLineEntryType.introduced -> VulnerabilityDateType.INTRODUCED
+                TimeLineEntryType.found -> VulnerabilityDateType.DISCOVERED
+                TimeLineEntryType.fixed -> VulnerabilityDateType.FIXED
+                TimeLineEntryType.disclosed -> VulnerabilityDateType.RELEASED
+            },
+            vulnerabilityName = "NOT_USED_WHEN_SAVING_IN_DATABASE",
         )
     }
 }
