ef01dd892718cb95bee5d37fa2e0440cd5e1b5ff
Vfrolov <vlad-20-11-94@yandex.ru>
2023-08-31 12:07:49 +0300

    Added COSV files upload button

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/fileuploader/CosvFileManagerComponent.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/fileuploader/CosvFileManagerComponent.kt
@@ -3,6 +3,8 @@
 package com.saveourtool.save.frontend.components.basic.fileuploader
 
 import com.saveourtool.save.frontend.components.inputform.dragAndDropForm
+import com.saveourtool.save.frontend.externals.fontawesome.faDownload
+import com.saveourtool.save.frontend.externals.fontawesome.fontAwesomeIcon
 import com.saveourtool.save.frontend.utils.*
 import com.saveourtool.save.frontend.utils.noopLoadingHandler
 import com.saveourtool.save.utils.FILE_PART_NAME
@@ -10,12 +12,15 @@ import js.core.asList
 import org.w3c.fetch.Headers
 import react.FC
 import react.Props
+import react.dom.html.ReactHTML.a
+import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.li
 import react.dom.html.ReactHTML.ul
 import react.useState
 import web.cssom.ClassName
 import web.file.File
+import web.html.ButtonType
 import web.http.FormData
 
 val cosvFileManagerComponent: FC<Props> = FC { props ->
@@ -26,13 +31,13 @@ val cosvFileManagerComponent: FC<Props> = FC { props ->
     val (filesForUploading, setFilesForUploading) = useState<List<File>>(emptyList())
 
     val uploadCosvFiles = useDeferredRequest {
-        val uploadedFiles: List<String> = post(
+        val uploadedIds: List<String> = post(
             url = "$apiUrl/cosv/batch-upload",
             Headers(),
             FormData().apply { filesForUploading.forEach { append(FILE_PART_NAME, it) } },
             loadingHandler = ::noopLoadingHandler,
         ).decodeFromJsonString()
-        setCosvAvailableFiles { uploadedFiles }
+        setCosvAvailableFiles { uploadedIds }
     }
 
     div {
@@ -40,6 +45,17 @@ val cosvFileManagerComponent: FC<Props> = FC { props ->
             className = ClassName("list-group shadow")
             cosvAvailableFiles.map { file ->
                 li {
+                    className = ClassName("list-group-item")
+                    a {
+                        button {
+                            type = ButtonType.button
+                            className = ClassName("btn")
+                            fontAwesomeIcon(icon = faDownload)
+                        }
+                        download = file
+                        href = "$apiUrl/cosv/get-by-id/$file"
+                    }
+
                     +file
                 }
             }
@@ -49,7 +65,7 @@ val cosvFileManagerComponent: FC<Props> = FC { props ->
                 className = ClassName("list-group-item p-0 d-flex bg-light")
                 dragAndDropForm {
                     isMultipleFilesSupported = true
-                    tooltipMessage = "Regular files/Executable files/ZIP Archives"
+                    tooltipMessage = "Only JSON files"
                     onChangeEventHandler = { files ->
                         setFilesForUploading(files!!.asList())
                         uploadCosvFiles()
