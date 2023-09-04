bbc5c334ecb2a837ecfba6bd587136ac0b80969f
Vfrolov <vlad-20-11-94@yandex.ru>
2023-09-04 13:43:19 +0300

    Added select organization in upload COSV files

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/fileuploader/CosvFileManagerComponent.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/basic/fileuploader/CosvFileManagerComponent.kt
@@ -2,10 +2,14 @@
 
 package com.saveourtool.save.frontend.components.basic.fileuploader
 
+import com.saveourtool.save.entities.OrganizationDto
+import com.saveourtool.save.frontend.components.basic.selectFormRequired
+import com.saveourtool.save.frontend.components.inputform.InputTypes
 import com.saveourtool.save.frontend.components.inputform.dragAndDropForm
 import com.saveourtool.save.frontend.externals.fontawesome.faDownload
 import com.saveourtool.save.frontend.utils.*
 import com.saveourtool.save.utils.FILE_PART_NAME
+import com.saveourtool.save.validation.isValidName
 import js.core.asList
 import org.w3c.fetch.Headers
 import react.FC
@@ -19,16 +23,35 @@ import web.cssom.ClassName
 import web.file.File
 import web.http.FormData
 
-val cosvFileManagerComponent: FC<Props> = FC { props ->
+val cosvFileManagerComponent: FC<Props> = FC { _ ->
     useTooltip()
 
-    val (cosvAvailableFiles, setCosvAvailableFiles) = useState<List<String>>(emptyList())
+    @Suppress("GENERIC_VARIABLE_WRONG_DECLARATION")
+    val organizationSelectForm = selectFormRequired<String>()
 
+    val (cosvAvailableFiles, setCosvAvailableFiles) = useState<List<String>>(emptyList())
     val (filesForUploading, setFilesForUploading) = useState<List<File>>(emptyList())
+
+    val (userOrganizations, setUserOrganizations) = useState(emptyList<OrganizationDto>())
+    val (selectOrganizations, setSelectOrganizations) = useState("")
+
+    useRequest {
+        val organizations = get(
+            url = "$apiUrl/organizations/get/list",
+            headers = jsonHeaders,
+            loadingHandler = ::loadingHandler,
+            responseHandler = ::noopResponseHandler,
+        )
+            .unsafeMap {
+                it.decodeFromJsonString<List<OrganizationDto>>()
+            }
+
+        setUserOrganizations(organizations)
+    }
 
     val uploadCosvFiles = useDeferredRequest {
         val uploadedIds: List<String> = post(
-            url = "$apiUrl/cosv/batch-upload",
+            url = "$apiUrl/cosv/batch-upload?organizationName=$selectOrganizations",
             Headers(),
             FormData().apply { filesForUploading.forEach { append(FILE_PART_NAME, it) } },
             loadingHandler = ::loadingHandler,
@@ -38,8 +61,23 @@ val cosvFileManagerComponent: FC<Props> = FC { props ->
     }
 
     div {
+        organizationSelectForm {
+            selectClasses = "custom-select"
+            formType = InputTypes.ORGANIZATION_NAME
+            validInput = selectOrganizations.isNotEmpty() && selectOrganizations.isValidName()
+            classes = "mb-3"
+            formName = "Organization"
+            getData = { userOrganizations.map { it.name } }
+            dataToString = { it }
+            selectedValue = selectOrganizations
+            disabled = false
+            onChangeFun = { value ->
+                setSelectOrganizations(value ?: "")
+            }
+        }
+
         ul {
-            className = ClassName("list-group shadow")
+            className = ClassName("list-group")
             cosvAvailableFiles.map { file ->
                 li {
                     className = ClassName("list-group-item")
