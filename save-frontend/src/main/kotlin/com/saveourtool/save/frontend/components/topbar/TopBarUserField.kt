7bbf41f406193d05c9ce68bbc67f32020593bd05
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2023-09-05 11:20:20 +0300

    Bugfix for editing of vulnerability based on permissions
    ### What's done:
    - permission bugfix
    - added a functionality to show editing projects components only when edit button is pressed

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/topbar/TopBarUserField.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/topbar/TopBarUserField.kt
@@ -2,7 +2,6 @@
 
 package com.saveourtool.save.frontend.components.topbar
 
-import com.saveourtool.save.domain.Role
 import com.saveourtool.save.frontend.components.basic.avatarRenderer
 import com.saveourtool.save.frontend.components.modal.logoutModal
 import com.saveourtool.save.frontend.externals.fontawesome.*
