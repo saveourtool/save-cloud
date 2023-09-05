c4d8e1e560a72ef17078b131218e7e1a1bf7bcba
Vfrolov <vlad-20-11-94@yandex.ru>
2023-09-05 14:22:38 +0300

    Added user permissions

--- save-cloud-common/src/commonMain/kotlin/com/saveourtool/save/info/UserPermissions.kt
+++ save-cloud-common/src/commonMain/kotlin/com/saveourtool/save/info/UserPermissions.kt
@@ -3,9 +3,9 @@ package com.saveourtool.save.info
 import kotlinx.serialization.Serializable
 
 /**
- * @property inInOrganizations user permissions in organizations
+ * @property inOrganizations user permissions in organizations
  */
 @Serializable
 data class UserPermissions(
-    val inInOrganizations: Map<String, UserPermissionsInOrganization> = emptyMap(),
+    val inOrganizations: Map<String, UserPermissionsInOrganization> = emptyMap(),
 )
