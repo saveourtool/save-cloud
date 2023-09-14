b29fe30a64e870bcaad7dfeadcb502594b48ee7d
Nariman Abdullin <nulls.narik@gmail.com>
2023-09-14 17:23:40 +0300

    added a workaround

--- save-cosv/src/main/kotlin/com/saveourtool/save/cosv/storage/CosvKey.kt
+++ save-cosv/src/main/kotlin/com/saveourtool/save/cosv/storage/CosvKey.kt
@@ -5,8 +5,10 @@ import kotlinx.datetime.LocalDateTime
 /**
  * @property id
  * @property modified
+ * @property isValid
  */
 data class CosvKey(
     val id: String,
     val modified: LocalDateTime,
+    val isValid: Boolean = true,
 )
