9c44f489d50c454c39a4a4b3dc9382745bf82ceb
Nariman Abdullin <nulls.narik@gmail.com>
2023-09-14 18:09:42 +0300

    added a link from CosvFile to VulnerabilityMetadata

--- save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/entities/cosv/CosvFile.kt
+++ save-cloud-common/src/jvmMain/kotlin/com/saveourtool/save/entities/cosv/CosvFile.kt
@@ -3,6 +3,8 @@ package com.saveourtool.save.entities.cosv
 import com.saveourtool.save.spring.entity.BaseEntity
 import java.time.LocalDateTime
 import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 
 /**
  * Entity for COSV repository
@@ -13,6 +15,9 @@ import javax.persistence.Entity
 class CosvFile(
     var identifier: String,
     var modified: LocalDateTime,
+    @ManyToOne
+    @JoinColumn(name = "vulnerability_metadata_id")
+    var metadata: VulnerabilityMetadata,
 ) : BaseEntity() {
     override fun toString(): String = "CosvFile(identifier=$identifier, modified=$modified)"
 }
