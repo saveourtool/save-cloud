42056743d6d8e5e5c6a486c423b28633100a4f0b
Vladislav Frolov <50615459+Cheshiriks@users.noreply.github.com>
2023-09-15 17:48:27 +0300

    Fix date listener (#2596)
    Fix date listener

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/externals/progressbar/ReactCircleBuilder.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/externals/progressbar/ReactCircleBuilder.kt
@@ -27,7 +27,7 @@ fun ChildrenBuilder.progressBar(
     ReactCircle::class.react {
         this.size = size
         this.lineWidth = lineWidth
-        this.progress = progress.toString()
+        this.progress = (progress * 10).toString()
         this.progressColor = color
         this.showPercentageSymbol = showPercentageSymbol
         this.textColor = color
