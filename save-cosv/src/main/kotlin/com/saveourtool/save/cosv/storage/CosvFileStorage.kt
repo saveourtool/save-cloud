ace055d94251637e0d4f242e679f3bc85e9557ae
Nariman Abdullin <nulls.narik@gmail.com>
2023-09-18 14:56:58 +0300

    deleted unexpected files

--- save-cosv/src/main/kotlin/com/saveourtool/save/cosv/storage/CosvFileStorage.kt
+++ save-cosv/src/main/kotlin/com/saveourtool/save/cosv/storage/CosvFileStorage.kt
@@ -2,10 +2,13 @@ package com.saveourtool.save.cosv.storage
 
 import com.saveourtool.save.entities.cosv.CosvFile
 import com.saveourtool.save.s3.S3Operations
+import com.saveourtool.save.storage.DefaultStorageProjectReactor
 import com.saveourtool.save.storage.ReactiveStorageWithDatabase
+import com.saveourtool.save.storage.deleteUnexpectedKeys
 import com.saveourtool.save.utils.blockingToFlux
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 /**
  * Storage for COSV files.
@@ -15,12 +18,22 @@ import reactor.core.publisher.Flux
  */
 @Service
 class CosvFileStorage(
-    s3Operations: S3Operations,
+    private val s3Operations: S3Operations,
     s3KeyManager: CosvFileS3KeyManager,
 ) : ReactiveStorageWithDatabase<CosvFile, CosvFile, CosvFileS3KeyManager>(
     s3Operations,
     s3KeyManager,
 ) {
+    /**
+     * Init method to remove deleted (unexpected) ids which are detected in storage, but missed in database
+     */
+    override fun doInit(underlying: DefaultStorageProjectReactor<CosvFile>): Mono<Unit> = Mono.fromFuture {
+        s3Operations.deleteUnexpectedKeys(
+            storageName = "${this::class.simpleName}",
+            s3KeyManager = s3KeyManager,
+        )
+    }.publishOn(s3Operations.scheduler)
+
     /**
      * @param identifier
      * @return [Flux] with all [CosvFile] found by [identifier]
