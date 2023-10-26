f771389ff956909355ca55275d278a35931a9152
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2023-10-26 13:16:27 +0300

    WIP ### What's done: * WIP

--- save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/inputform/InputForms.kt
+++ save-frontend/src/main/kotlin/com/saveourtool/save/frontend/components/inputform/InputForms.kt
@@ -161,7 +161,8 @@ enum class InputTypes(
         placeholder = SEVERITY_VECTOR_PLACEHOLDER,
         tooltip = SEVERITY_VECTOR_TOOLTIP,
     ),
-    VULN_CREDITS("")
+    VULN_CREDIT_NAME("user name", placeholder = "User name"),
+    VULN_CREDIT_CONTACTS("contacts", placeholder = "Contacts"),
     ;
 }
 
