83c51eaa3494478b17c9572e2be5d909a9ef50ed
Cheshiriks <vlad-20-11-94@yandex.ru>
2023-12-08 15:16:33 +0300

    Deleted unused component from common

--- save-frontend-common/src/main/kotlin/com/saveourtool/save/frontend/common/externals/graph/sigma/layouts/LayoutInstance.kt
+++
@@ -1,7 +0,0 @@
-package com.saveourtool.save.frontend.common.externals.graph.sigma.layouts
-
-/**
- * Wrapper for useLayoutRandom returning value to allow unpacking a pair (has [component1] and [component2])
- */
-@JsExport
-external interface LayoutInstance
