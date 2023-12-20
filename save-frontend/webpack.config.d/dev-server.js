93a3c17211ec9fb2d69157f311f702d02a081a1c
Nariman Abdullin <nulls.narik@gmail.com>
2023-12-20 12:16:25 +0300

    reverted changes

--- save-frontend/webpack.config.d/dev-server.js
+++ save-frontend/webpack.config.d/dev-server.js
@@ -2,13 +2,56 @@ config.devServer = Object.assign(
     {},
     config.devServer || {},
     {
+        setupMiddlewares: (middlewares, devServer) => {
+            devServer.app.get("/sec/oauth-providers", (req, res) => { return res.send([]); });
+            return middlewares;
+        },
         proxy: [
             {
-                context: ["/api/**", "/sec/**", "/oauth2/**", "/logout/**", "/login/oauth2/**"],
-                target: 'http://localhost:5300',
+                context: ["/api/sandbox/**"],
+                target: 'http://localhost:5400',
                 logLevel: 'debug',
+                onProxyReq: function (proxyReq, req, res) {
+                    proxyReq.setHeader("X-Authorization-Id", "1");
+                    proxyReq.setHeader("X-Authorization-Name", "admin");
+                    proxyReq.setHeader("X-Authorization-Roles", "ROLE_SUPER_ADMIN");
+                    proxyReq.setHeader("X-Authorization-Status", "ACTIVE");
+                }
+            },
+            {
+                context: ["/api/demo/**"],
+                target: 'http://localhost:5421',
+                logLevel: 'debug',
+                onProxyReq: function (proxyReq, req, res) {
+                    proxyReq.setHeader("X-Authorization-Id", "1");
+                    proxyReq.setHeader("X-Authorization-Name", "admin");
+                    proxyReq.setHeader("X-Authorization-Roles", "ROLE_SUPER_ADMIN");
+                    proxyReq.setHeader("X-Authorization-Status", "ACTIVE");
+                }
+            },
+            {
+                context: ["/api/cpg/**"],
+                target: 'http://localhost:5500',
+                logLevel: 'debug',
+                onProxyReq: function (proxyReq, req, res) {
+                    proxyReq.setHeader("X-Authorization-Id", "1");
+                    proxyReq.setHeader("X-Authorization-Name", "admin");
+                    proxyReq.setHeader("X-Authorization-Roles", "ROLE_SUPER_ADMIN");
+                    proxyReq.setHeader("X-Authorization-Status", "ACTIVE");
+                }
+            },
+            {
+                context: ["/api/**"],
+                target: 'http://localhost:5800',
+                logLevel: 'debug',
+                onProxyReq: function (proxyReq, req, res) {
+                    proxyReq.setHeader("X-Authorization-Id", "1");
+                    proxyReq.setHeader("X-Authorization-Name", "admin");
+                    proxyReq.setHeader("X-Authorization-Roles", "ROLE_SUPER_ADMIN");
+                    proxyReq.setHeader("X-Authorization-Status", "ACTIVE");
+                }
             }
         ],
         historyApiFallback: true
     }
-)
+);
