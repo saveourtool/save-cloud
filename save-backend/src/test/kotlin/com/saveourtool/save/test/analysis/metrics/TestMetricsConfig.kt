46dd8df844a8451029476563b2ef3e98a5f3c8ac
Cheshiriks <vlad-20-11-94@yandex.ru>
2024-04-23 16:23:50 +0300

    Renaming save-cloud-common module to common

--- save-backend/src/test/kotlin/com/saveourtool/save/test/analysis/metrics/TestMetricsConfig.kt
+++ save-backend/src/test/kotlin/com/saveourtool/save/test/analysis/metrics/TestMetricsConfig.kt
@@ -1,5 +1,6 @@
 package com.saveourtool.save.test.analysis.metrics
 
+import com.saveourtool.common.test.analysis.metrics.supportTestMetrics
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
