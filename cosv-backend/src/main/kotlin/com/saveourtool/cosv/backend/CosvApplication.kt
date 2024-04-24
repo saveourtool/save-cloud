ba3adde311b0a92e401a394dc13544fad3d5d6f2
Cheshiriks <vlad-20-11-94@yandex.ru>
2024-04-24 18:34:30 +0300

    Renaming save-cloud-common module to common

--- cosv-backend/src/main/kotlin/com/saveourtool/cosv/backend/CosvApplication.kt
+++ cosv-backend/src/main/kotlin/com/saveourtool/cosv/backend/CosvApplication.kt
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import
  */
 @SpringBootApplication(scanBasePackages = [
     "com.saveourtool.common",
+    "com.saveourtool.save",
     "com.saveourtool.cosv",
 ])
 @EnableConfigurationProperties(ConfigProperties::class)
